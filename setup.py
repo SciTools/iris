b86e636e901f8b0ce84ec8d0ae5df1443088595a
ecamp <drescampbell@gmail.com>
2012-08-09 13:36:49 +0100

    Change to prevent path issues with 'python setup.py build_ext --inplace' on clean systems.

--- setup.py
+++ setup.py
@@ -155,14 +155,20 @@ class PostBuildExtRunner(build_ext.build_ext):
         # Call parent
         build_ext.build_ext.run(self)
 
-        # Add our new build dir to the start of the path to pick up the KE rules
-        sys.path.insert(0, self.build_lib)
+        # Temporarily add our new build dir to the start of the path to 
+        # pick up the KE rules
+        if self.inplace == 1:
+            sys.path.insert(0, 'lib')
+        else:
+            sys.path.insert(0, self.build_lib)
         
         # Compile the pyke rules
         from pyke import knowledge_engine
         import iris.fileformats._pyke_rules
         e = knowledge_engine.engine(iris.fileformats._pyke_rules)
 
+        # Remove temporary addition to path
+        del sys.path[0]
 
 setup(
     name='Iris',
