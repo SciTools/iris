7abf2b97037029b09634d3c32d0a813edff21d20
Richard Hattersley <rhattersley@gmail.com>
2012-09-28 15:15:19 +0100

    Simplify rotated pole example and accept TEC plot

--- docs/iris/example_code/graphics/rotated_pole_mapping.py
+++ docs/iris/example_code/graphics/rotated_pole_mapping.py
@@ -23,15 +23,8 @@ def main():
     fname = iris.sample_data_path('rotated_pole.nc')
     temperature = iris.load_strict(fname)
     
-    # Calculate the lat lon range and buffer it by 10 degrees
-    lon_range, lat_range = iris.analysis.cartography.xy_range(temperature)
-    lat_range = lat_range[0] - 10, lat_range[1] + 10
-    lon_range = lon_range[0] - 10, lon_range[1] + 10
-
-    
     # Plot #1: Point plot showing data values & a colorbar
     plt.figure()
-    iplt.map_setup(cube=temperature, ylim=lat_range, xlim=lon_range)
     points = qplt.points(temperature, c=temperature.data)
     cb = plt.colorbar(points, orientation='horizontal')
     cb.set_label(temperature.units)
@@ -41,7 +34,6 @@ def main():
     
     # Plot #2: Contourf of the point based data
     plt.figure()
-    iplt.map_setup(cube=temperature, ylim=lat_range, xlim=lon_range)
     qplt.contourf(temperature, 15)
     plt.gca().coastlines()
     plt.show()
@@ -49,7 +41,6 @@ def main():
     
     # Plot #3: Contourf overlayed by coloured point data
     plt.figure()
-    iplt.map_setup(cube=temperature, ylim=lat_range, xlim=lon_range)
     qplt.contourf(temperature)
     iplt.points(temperature, c=temperature.data)
     plt.gca().coastlines()
@@ -64,7 +55,6 @@ def main():
     
     # Plot #4: Block plot
     plt.figure()
-    iplt.map_setup(cube=temperature, ylim=lat_range, xlim=lon_range)
     iplt.pcolormesh(temperature)
     plt.gca().bluemarble()
     plt.gca().coastlines()
