8aa3b496276f2ac93c25cf05b18468b92e9ef4fb
Bill Little <bill.james.little@gmail.com>
2012-10-11 11:41:49 +0100

    Refactor for gitwash get and build process.

--- docs/iris/src/developers_guide/gitwash_build.sh
+++ docs/iris/src/developers_guide/gitwash_build.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env sh
 
-# Get the latest gitwash_dumper.py from GitHub.
-echo "Downloading latest gitwash_dumper.py from GitHub ..."
-echo
-curl -O https://raw.github.com/matthew-brett/gitwash/master/gitwash_dumper.py
 
 # Generate the gitwash sub-directory.
 echo
