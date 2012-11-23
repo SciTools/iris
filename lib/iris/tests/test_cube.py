bd2f4f849c217ad9e5bb34e4af3efa8afba27bb0
Carwyn Pelley <cpelley.pub@gmail.com>
2012-11-23 09:52:24 +0000

    new test file for cube

---
+++ lib/iris/tests/test_cube.py
@@ -0,0 +1,39 @@
+# (C) British Crown Copyright 2010 - 2012, Met Office
+#
+# This file is part of Iris.
+#
+# Iris is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Iris is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Iris. If not, see <http://www.gnu.org/licenses/>.
+
+# import iris tests first so that some things can be initialised before
+# importing anything else
+import iris.tests as tests
+
+import numpy
+
+import iris
+
+
+class Test_add_dim_coord(tests.IrisTest):
+    def setUp(self):
+        self.cube = iris.cube.Cube(numpy.arange(4).reshape(2, 2))
+        
+    
+    def test_1d(self):
+        self.assertRaises(TypeError,
+                          self.cube.add_dim_coord,
+                          iris.coords.DimCoord(numpy.arange(2), "latitude"))
+
+
+if __name__ == "__main__":
+    tests.main()
