44bdb773b7418b7643a6be91c1c7a4b177d35bc2
Richard Hattersley <rhattersley@gmail.com>
2012-12-07 15:09:16 +0000

    Update version to 1.1.0-dev

--- setup.py
+++ setup.py
@@ -237,7 +237,7 @@ class BuildPyWithExtras(build_py.build_py):
 
 setup(
     name='Iris',
-    version='1.1.0rc1',
+    version='1.1.0-dev',
     url='http://scitools.github.com/iris',
     author='UK Met Office',
 
