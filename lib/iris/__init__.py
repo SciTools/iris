c1d21d4e2b46f49ebefa2015fb86faaa437a5dd0
Ed Campbell <drescampbell@gmail.com>
2013-01-03 14:06:45 +0000

    Update version in __init__.py for Release v1.1.0

--- lib/iris/__init__.py
+++ lib/iris/__init__.py
@@ -109,7 +109,7 @@ import iris.io
 
 
 # Iris revision.
-__version__ = '1.1.0-dev'
+__version__ = '1.1.0'
 
 # Restrict the names imported when using "from iris import *"
 __all__ = ['load', 'load_cube', 'load_cubes', 'load_raw', 'load_strict', 'save',
