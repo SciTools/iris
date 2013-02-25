34fb301e101a48b4312d4272dc5960cef801a227
Carwyn Pelley <cpelley.pub@gmail.com>
2013-02-25 11:26:34 +0000

    lbmin ppsave bugfix, closes #277

--- lib/iris/fileformats/pp.py
+++ lib/iris/fileformats/pp.py
@@ -1235,6 +1235,7 @@ class PPField2(PPField):
         self.lbmon = dt.month
         self.lbdat = dt.day
         self.lbhr = dt.hour
+        self.lbmin = dt.minute
         self.lbday = int(dt.strftime('%j'))
         if hasattr(self, '_t1'):
             delattr(self, '_t1')
@@ -1279,6 +1280,7 @@ class PPField3(PPField):
         self.lbmon = dt.month
         self.lbdat = dt.day
         self.lbhr = dt.hour
+        self.lbmin = dt.minute
         self.lbsec = dt.second
         if hasattr(self, '_t1'):
             delattr(self, '_t1')
@@ -1605,7 +1607,7 @@ def save(cube, target, append=False, field_coords=None):
         # raise an IndexError)
         # NB watch out for the ordering of the dimensions
         field_coords = (cube.coords(dimensions=n_dims-2)[0], cube.coords(dimensions=n_dims-1)[0])
-        
+
     # Save each named or latlon slice2D in the cube
     for slice2D in cube.slices(field_coords):
         # Start with a blank PPField
