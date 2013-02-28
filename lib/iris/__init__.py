0bc6e1a34bdcec120230eb704c75aae07ddb961c
bblay <byronblay@googlemail.com>
2013-02-28 16:41:27 +0000

    version 1.2.1-dev

--- lib/iris/__init__.py
+++ lib/iris/__init__.py
@@ -109,7 +109,7 @@ import iris.io
 
 
 # Iris revision.
-__version__ = '1.2.0'
+__version__ = '1.2.1-dev'
 
 # Restrict the names imported when using "from iris import *"
 __all__ = ['load', 'load_cube', 'load_cubes', 'load_raw', 'load_strict', 'save',
