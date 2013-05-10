16f2112bda89cdfbd2e4efa18a2b65e32bcedc00
cpelley <cpelley.pub@gmail.com>
2013-05-10 18:00:40 +0200

    Update calculus.py

--- lib/iris/analysis/calculus.py
+++ lib/iris/analysis/calculus.py
@@ -111,12 +111,6 @@ def _construct_midpoint_coord(coord, circular=None):
         mid_point_coord = coord.from_coord(coord).copy(mid_point_points, mid_point_bounds)
     except ValueError:
         mid_point_coord = iris.coords.AuxCoord.from_coord(coord).copy(mid_point_points, mid_point_bounds)
-    
-    # Set all of the coord names to original values as coord_delta will have
-    # modified it to 'change_in_*'.
-    mid_point_coord.standard_name = coord.standard_name
-    mid_point_coord.long_name = coord.long_name
-    mid_point_coord.var_name = coord.var_name
 
     return mid_point_coord
 
