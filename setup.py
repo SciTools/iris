f8dde9fce15f7d79c5f07ea40a580ecda6e348fc
Peter Killick <peter.killick@metoffice.gov.uk>
2013-10-09 15:00:28 +0100

    Version updated to v1.5.2-dev

--- setup.py
+++ setup.py
@@ -214,7 +214,7 @@ class BuildPyWithExtras(build_py.build_py):
 
 setup(
     name='Iris',
-    version='1.5.1',
+    version='1.5.2-dev',
     url='http://scitools.github.com/iris',
     author='UK Met Office',
 
