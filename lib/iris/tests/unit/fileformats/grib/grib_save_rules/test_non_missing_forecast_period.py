72bd3d409eb04fa6bac57362a246fb728fa2def9
bblay <byronblay@googlemail.com>
2013-12-02 16:15:58 +0000

    nicer test

--- lib/iris/tests/unit/fileformats/grib/grib_save_rules/test_non_missing_forecast_period.py
+++ lib/iris/tests/unit/fileformats/grib/grib_save_rules/test_non_missing_forecast_period.py
@@ -30,38 +30,27 @@ import iris.unit
 
 
 class Test(TestGribSimple):
-    def _t_coord(self, bounds=False):
-        t_coord = mock.Mock()
-        t_coord.points = [15]
-        t_coord.units = iris.unit.Unit('hours')
-        if bounds:
-            t_coord.has_bounds = mock.Mock(return_value=True)
-            t_coord.bounds = np.array([[10, 20]])
-        else:
-            t_coord.has_bounds = mock.Mock(return_value=False)
-        return t_coord
-
-    def _mock_cube(self, t_bounds=False):
-        coords = {'time': self._t_coord(t_bounds),
-                  'forecast_period': mock.Mock()}
-        coords['time'].copy = mock.Mock(return_value=self._t_coord(t_bounds))
-        coords['forecast_period'].points = [10]
-        coords['forecast_period'].units = iris.unit.Unit('hours')
-        coords['forecast_period'].has_bounds = mock.Mock(return_value=False)
-
-        cube = mock.Mock()
-        cube.coord = coords.get
+    def _cube(self, t_bounds=False):
+        time_coord = iris.coords.DimCoord(15, standard_name='time',
+                                          units='hours since epoch')
+        if t_bounds:
+            time_coord.bounds = [[8, 12]]
+        fp_coord = iris.coords.DimCoord(10, standard_name='forecast_period',
+                                        units='hours')
+        cube = iris.cube.Cube([23])
+        cube.add_dim_coord(time_coord, 0)
+        cube.add_aux_coord(fp_coord, 0)
         return cube
-
+    
     def test_time_point(self):
-        cube = self._mock_cube()
+        cube = self._cube()
         rt, rt_meaning, fp, fp_meaning = _non_missing_forecast_period(cube)
         self.assertEqual((rt_meaning, fp, fp_meaning), (1, 10, 1))
 
     def test_time_bounds(self):
-        cube = self._mock_cube(t_bounds=True)
+        cube = self._cube(t_bounds=True)
         rt, rt_meaning, fp, fp_meaning = _non_missing_forecast_period(cube)
-        self.assertEqual((rt_meaning, fp, fp_meaning), (1, 5, 1))
+        self.assertEqual((rt_meaning, fp, fp_meaning), (1, 3, 1))
 
 
 if __name__ == "__main__":
