74d3aa91595e2ac559e388644ca35cbde315cd1d
marqh <markh@scitools.org.uk>
2014-01-22 10:33:54 +0000

    central longitude example

--- docs/iris/example_code/graphics/global_map.py
+++ docs/iris/example_code/graphics/global_map.py
@@ -6,6 +6,7 @@ This example demonstrates a contour plot of global air temperature.
 The plot title and the labels for the axes are automatically derived from the metadata.
 
 """
+import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
 
 import iris
@@ -16,10 +17,18 @@ def main():
     fname = iris.sample_data_path('air_temp.pp')
     temperature = iris.load_cube(fname)
 
+    # Plot #1: contourf with axes longitude from -180 to 180
+    plt.figure()
     qplt.contourf(temperature, 15)
     plt.gca().coastlines()
     plt.show()
 
+    # Plot #2: contourf with axes longitude from 0 to 360
+    plt.figure()
+    ax = plt.axes(projection=ccrs.PlateCarree(central_longitude=-180.0))
+    qplt.contourf(temperature, 15)
+    plt.gca().coastlines()
+    plt.show()
 
 if __name__ == '__main__':
     main()
