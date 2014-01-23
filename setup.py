ac24cc2b03b76441e0c91afb1faaf243852e9320
bblay <byronblay@googlemail.com>
2014-01-23 11:46:57 +0000

    1.7.0-dev

--- setup.py
+++ setup.py
@@ -219,7 +219,7 @@ class BuildPyWithExtras(build_py.build_py):
 
 setup(
     name='Iris',
-    version='1.6.0-dev',
+    version='1.7.0-dev',
     url='http://scitools.github.com/iris',
     author='UK Met Office',
 
