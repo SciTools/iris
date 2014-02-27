8acc341a1ddc039c32343b7821c6c4eb20ae999b
marqh <markh@scitools.org.uk>
2014-02-27 08:50:23 +0000

    example ready

---
+++ docs/iris/example_code/graphics/orca_projection.py
@@ -0,0 +1,58 @@
+"""
+Tru-Polar Grid Projected Ploting
+================================
+
+This example demonstrates cell plots of data on the semi-structured ORCA2 model
+grid.
+
+First, the data is projected into the PlateCarree coordinate reference system.
+
+Second four pcolormesh plots are created from this projected dataset,
+using different projections for the output image.
+
+"""
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+import cartopy.crs as ccrs
+import iris
+import iris.analysis.cartography
+import iris.quickplot as qplt
+
+
+def main():
+    # Load data
+    filepath = iris.sample_data_path('orca2_votemper.nc')
+    cube = iris.load_cube(filepath)
+
+    # Choose plot projections
+    projections = {}
+    projections['Mollweide'] = ccrs.Mollweide()
+    projections['PlateCarree'] = ccrs.PlateCarree()
+    projections['NorthPolarStereo'] = ccrs.NorthPolarStereo()
+    projections['Orthographic'] = ccrs.Orthographic(central_longitude=-90,
+                                                    central_latitude=45)
+
+    pcarree = projections['PlateCarree']
+    # Transform cube to target projection
+    new_cube, extent = iris.analysis.cartography.project(cube, pcarree,
+                                                         nx=400, ny=200)
+
+    # Plot data in each projection
+    for name, target_proj in projections.iteritems():
+        fig = plt.figure()
+        fig.suptitle('ORCA2 Data Projected to {}'.format(name))
+        # Set up axes and title
+        ax = plt.subplot(projection=target_proj)
+        # Set limits
+        ax.set_global()
+        # plot with Iris quickplot pcolormesh
+        qplt.pcolormesh(new_cube)
+        # Draw coastlines
+        ax.coastlines()
+
+        plt.show()
+
+if __name__ == '__main__':
+    main()
