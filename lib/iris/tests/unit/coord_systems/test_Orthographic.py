539d7fae098cbea84e4b68dc3b34940e4cd8cee3
Peter Killick <peter.killick@metoffice.gov.uk>
2014-04-14 09:58:23 +0100

    Added orthographic projection

---
+++ lib/iris/tests/unit/coord_systems/test_Orthographic.py
@@ -0,0 +1,55 @@
+# (C) British Crown Copyright 2014, Met Office
+#
+# This file is part of Iris.
+#
+# Iris is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Iris is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Iris.  If not, see <http://www.gnu.org/licenses/>.
+"""Unit tests for the :class:`iris.coord_systems.Orthographic` class."""
+
+# Import iris.tests first so that some things can be initialised before
+# importing anything else.
+import iris.tests as tests
+
+import datetime
+import collections
+
+import mock
+import numpy as np
+
+import cartopy.crs
+from iris.coord_systems import GeogCS, Orthographic
+
+
+class Test_Orthographic(tests.IrisTest):
+    def setUp(self):
+        self.latitude_of_projection_origin = 0.0
+        self.longitude_of_projection_origin = 0.0
+        self.ellipsoid = GeogCS(semi_major_axis=6377563.396,
+                                semi_minor_axis=6356256.909)
+        self.ortho_cs = Orthographic(self.latitude_of_projection_origin,
+                                     self.longitude_of_projection_origin,
+                                     ellipsoid=self.ellipsoid)
+
+    def test_as_cartopy_crs(self):
+        res = self.ortho_cs.as_cartopy_crs()
+        globe = cartopy.crs.Globe(semimajor_axis=6377563.396,
+                                  semiminor_axis=6356256.909, ellipse=None)
+        expected = cartopy.crs.Orthographic(
+            self.latitude_of_projection_origin,
+            self.longitude_of_projection_origin,
+            globe=globe)
+        self.assertEqual(res, expected)
+
+
+if __name__ == '__main__':
+    tests.main()
