25577fccc09fb312b338fcca1bb3fda269a94acc
marqh <markh@scitools.org.uk>
2014-05-08 16:22:22 +0000

    lichead

--- tools/gen_helpers.py
+++ tools/gen_helpers.py
@@ -2,10 +2,10 @@
 #
 # This file is part of Iris.
 #
-# Iris is free software: you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
+# Iris is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
 # Iris is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
