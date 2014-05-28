f447eb0c74c53ec5b9c3fd6eb5beb7a991f4abed
Andreas Hilboll <andreas@hilboll.de>
2014-05-28 18:08:54 +0200

    ENH: speedup geometry_area_weights

--- lib/iris/analysis/geometry.py
+++ lib/iris/analysis/geometry.py
@@ -22,11 +22,119 @@ Various utilities related to geometric operations.
 
 """
 
+import warnings
+
 from shapely.geometry import Polygon
 
 import numpy as np
 
 import iris.exceptions
+
+
+def _extract_relevant_cube_slice(cube, geometry):
+    """
+    Given a shapely geometry object, this helper method returns
+    the tuple
+    (subcube, x_coord_of_subcube, y_coord_of_subcube,
+     (min_x_index, min_y_index, max_x_index, max_y_index))
+
+    If cube and geometry don't overlap, returns None.
+
+    """
+
+    # Validate the input parameters
+    if not cube.coords(axis='x') or not cube.coords(axis='y'):
+        raise ValueError('The cube must contain x and y axes.')
+
+    x_coords = cube.coords(axis='x')
+    y_coords = cube.coords(axis='y')
+    if len(x_coords) != 1 or len(y_coords) != 1:
+        raise ValueError('The cube must contain one, and only one, coordinate '
+                         'for each of the x and y axes.')
+
+    x_coord = x_coords[0]
+    y_coord = y_coords[0]
+    if not (x_coord.has_bounds() and y_coord.has_bounds()):
+        raise ValueError('Both horizontal coordinates must have bounds.')
+
+    if x_coord.ndim != 1:
+        raise iris.exceptions.CoordinateMultiDimError(x_coord)
+    if y_coord.ndim != 1:
+        raise iris.exceptions.CoordinateMultiDimError(y_coord)
+
+    # bounds of cube dimensions
+    x_bounds = x_coord.bounds
+    y_bounds = y_coord.bounds
+
+    # identify ascending/descending coordinate dimensions
+    x_ascending = x_coord.points[1] - x_coord.points[0] > 0.
+    y_ascending = y_coord.points[1] - y_coord.points[0] > 0.
+
+    # identify upper/lower bounds of coordinate dimensions
+    x_bounds_lower = x_bounds[:, 0] if x_ascending else x_bounds[:, 1]
+    y_bounds_lower = y_bounds[:, 0] if y_ascending else y_bounds[:, 1]
+    x_bounds_upper = x_bounds[:, 1] if x_ascending else x_bounds[:, 0]
+    y_bounds_upper = y_bounds[:, 1] if y_ascending else y_bounds[:, 0]
+
+    # find indices of coordinate bounds to fully cover geometry
+    x_min_geom, y_min_geom, x_max_geom, y_max_geom = geometry.bounds
+    try:
+        x_min_ix = np.where(x_bounds_lower <= x_min_geom)[0]
+        x_min_ix = x_min_ix[np.argmax(x_bounds_lower[x_min_ix])]
+    except ValueError:
+        warnings.warn("The geometry exceeds the cube's x dimension at the "
+                      "lower end.", UserWarning)
+        x_min_ix = 0 if x_ascending else x_coord.points.size - 1
+
+    try:
+        x_max_ix = np.where(x_bounds_upper >= x_max_geom)[0]
+        x_max_ix = x_max_ix[np.argmin(x_bounds_upper[x_max_ix])]
+    except ValueError:
+        warnings.warn("The geometry exceeds the cube's x dimension at the "
+                      "upper end.", UserWarning)
+        x_max_ix = x_coord.points.size - 1 if x_ascending else 0
+
+    try:
+        y_min_ix = np.where(y_bounds_lower <= y_min_geom)[0]
+        y_min_ix = y_min_ix[np.argmax(y_bounds_lower[y_min_ix])]
+    except ValueError:
+        warnings.warn("The geometry exceeds the cube's y dimension at the "
+                      "lower end.", UserWarning)
+        y_min_ix = 0 if y_ascending else y_coord.points.size - 1
+
+    try:
+        y_max_ix = np.where(y_bounds_upper >= y_max_geom)[0]
+        y_max_ix = y_max_ix[np.argmin(y_bounds_upper[y_max_ix])]
+    except ValueError:
+        warnings.warn("The geometry exceeds the cube's y dimension at the "
+                      "upper end.", UserWarning)
+        y_max_ix = y_coord.points.size - 1 if y_ascending else 0
+
+    # extract coordinate values at these indices
+    x_min = x_bounds_lower[x_min_ix]
+    x_max = x_bounds_upper[x_max_ix]
+    y_min = y_bounds_lower[y_min_ix]
+    y_max = y_bounds_upper[y_max_ix]
+
+    # switch min and max if necessary, to create slice objects later on
+    if x_min_ix > x_max_ix:
+        x_min_ix, x_max_ix = x_max_ix, x_min_ix
+    if y_min_ix > y_max_ix:
+        y_min_ix, y_max_ix = y_max_ix, y_min_ix
+    bnds_ix = x_min_ix, y_min_ix, x_max_ix, y_max_ix
+
+    # cut the relevant part from the original cube
+    coord_constr = {x_coord.name(): lambda x: x_min <= x.point <= x_max,
+                    y_coord.name(): lambda y: y_min <= y.point <= y_max}
+    constraint = iris.Constraint(coord_values=coord_constr)
+    subcube = cube.extract(constraint)
+
+    if subcube is None:
+        return None
+    else:
+        x_coord = subcube.coord(axis='x')
+        y_coord = subcube.coord(axis='y')
+        return subcube, x_coord, y_coord, bnds_ix
 
 
 def geometry_area_weights(cube, geometry, normalize=False):
@@ -42,6 +150,15 @@ def geometry_area_weights(cube, geometry, normalize=False):
         This routine works in Euclidean space. Area calculations do not
         account for the curvature of the Earth. And care must be taken to
         ensure any longitude values are expressed over a suitable interval.
+
+    .. note::
+        This routine currently does not handle all out-of-bounds cases
+        correctly. In cases where both the coordinate bounds and the
+        geometry's bounds lie outside the physically realistic range
+        (i.e., abs(latitude) > 90., as it is commonly the case when
+        bounds are constructed via guess_bounds()), the weights
+        calculation might be wrong. In this case, a UserWarning will
+        be issued.
 
     Args:
 
@@ -59,46 +176,51 @@ def geometry_area_weights(cube, geometry, normalize=False):
         Default is False.
 
     """
-    # Validate the input parameters
-    if not cube.coords(axis='x') or not cube.coords(axis='y'):
-        raise ValueError('The cube must contain x and y axes.')
 
-    x_coords = cube.coords(axis='x')
-    y_coords = cube.coords(axis='y')
-    if len(x_coords) != 1 or len(y_coords) != 1:
-        raise ValueError('The cube must contain one, and only one, coordinate for each of the x and y axes.')
+    # extract smallest subcube containing geometry
+    shape = cube.shape
+    extraction_results = _extract_relevant_cube_slice(cube, geometry)
 
-    x_coord = x_coords[0]
-    y_coord = y_coords[0]
-    if not (x_coord.has_bounds() and y_coord.has_bounds()):
-        raise ValueError('Both horizontal coordinates must have bounds.')
+    # test if there is overlap between cube and geometry
+    if extraction_results is None:
+        return np.zeros(shape)
 
-    if x_coord.ndim != 1:
-        raise iris.exceptions.CoordinateMultiDimError(x_coord)
-    if y_coord.ndim != 1:
-        raise iris.exceptions.CoordinateMultiDimError(y_coord)
+    subcube, subx_coord, suby_coord, bnds_ix = extraction_results
+    x_min_ix, y_min_ix, x_max_ix, y_max_ix = bnds_ix
 
-    # Figure out the shape of the horizontal dimensions
-    shape = [1] * len(cube.shape)
-    x_dim = cube.coord_dims(x_coord)[0]
-    y_dim = cube.coord_dims(y_coord)[0]
-    shape[x_dim] = x_coord.shape[0]
-    shape[y_dim] = y_coord.shape[0]
-    weights = np.empty(shape, np.float32)
+    # prepare the weights array
+    subshape = list(cube.shape)
+    x_dim = cube.coord_dims(subx_coord)[0]
+    y_dim = cube.coord_dims(suby_coord)[0]
+    subshape[x_dim] = subx_coord.shape[0]
+    subshape[y_dim] = suby_coord.shape[0]
+    subx_bounds = subx_coord.bounds
+    suby_bounds = suby_coord.bounds
+    subweights = np.empty(subshape, np.float32)
 
-    # Calculate the area weights
-    x_bounds = x_coord.bounds
-    y_bounds = y_coord.bounds
-    for nd_index in np.ndindex(weights.shape):
+    # calculate the area weights
+    for nd_index in np.ndindex(subweights.shape):
         xi = nd_index[x_dim]
         yi = nd_index[y_dim]
-        x0, x1 = x_bounds[xi]
-        y0, y1 = y_bounds[yi]
+        x0, x1 = subx_bounds[xi]
+        y0, y1 = suby_bounds[yi]
         polygon = Polygon([(x0, y0), (x0, y1), (x1, y1), (x1, y0)])
+        subweights[nd_index] = polygon.intersection(geometry).area
         if normalize:
-            weights[nd_index] = polygon.intersection(geometry).area / polygon.area
+            subweights[nd_index] /= polygon.area
+
+    # pad the calculated weights with zeros to match original cube shape
+    weights = np.zeros(shape, np.float32)
+    slices = []
+    for i in range(weights.ndim):
+        if i == x_dim:
+            slices.append(slice(x_min_ix, x_max_ix + 1))
+        elif i == y_dim:
+            slices.append(slice(y_min_ix, y_max_ix + 1))
         else:
-            weights[nd_index] = polygon.intersection(geometry).area
+            slices.append(slice(None))
+
+    weights[slices] = subweights
 
     # Fix for the limitation of iris.analysis.MEAN weights handling.
     # Broadcast the array to the full shape of the cube
---
+++ lib/iris/tests/unit/analysis/geometry/test_geometry_area_weights.py
@@ -0,0 +1,164 @@
+# (C) British Crown Copyright 2014, Met Office
+#
+# This file is part of Iris.
+#
+# Iris is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Iris is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Iris.  If not, see <http://www.gnu.org/licenses/>.
+"""
+Unit tests for the :func:`iris.analysis.geometry.geometry_area_weights`
+function.
+
+ """
+
+from __future__ import division
+# Import iris.tests first so that some things can be initialised before
+# importing anything else.
+
+import iris.tests as tests
+import iris.tests.stock
+
+import warnings
+import numpy as np
+import shapely.geometry
+
+import iris
+from iris.analysis.geometry import geometry_area_weights
+from iris.coords import DimCoord
+from iris.cube import Cube
+
+
+class Test(tests.IrisTest):
+
+    def setUp(self):
+        x_coord = DimCoord([1.0, 3.0], 'longitude', bounds=[[0, 2], [2, 4]])
+        y_coord = DimCoord([1.0, 3.0], 'latitude', bounds=[[0, 2], [2, 4]])
+        self.data = np.empty((4, 2, 2))
+        dim_coords_and_dims = [(y_coord, (1,)), (x_coord, (2,))]
+        self.cube = Cube(self.data, dim_coords_and_dims=dim_coords_and_dims)
+        self.geometry = shapely.geometry.Polygon([(3, 3), (3, 50), (50, 50),
+                                                  (50, 3)])
+
+    def test_no_overlap(self):
+        geometry = shapely.geometry.Polygon([(4, 4), (4, 6), (6, 6), (6, 4)])
+        weights = geometry_area_weights(self.cube, geometry)
+        self.assertEqual(np.sum(weights), 0)
+
+    def test_overlap(self):
+        weights = geometry_area_weights(self.cube, self.geometry)
+        expected = np.repeat([[[0., 0.], [0., 1.]]], self.data.shape[0],
+                             axis=0)
+        self.assertArrayEqual(weights, expected)
+
+    def test_overlap_normalize(self):
+        weights = geometry_area_weights(self.cube, self.geometry,
+                                        normalize=True)
+        expected = np.repeat([[[0., 0.], [0., 0.25]]], self.data.shape[0],
+                             axis=0)
+        self.assertArrayEqual(weights, expected)
+
+    @tests.skip_data
+    def test_distinct_xy(self):
+        cube = tests.stock.simple_pp()
+        cube = cube[:4, :4]
+        lon = cube.coord('longitude')
+        lat = cube.coord('latitude')
+        lon.guess_bounds()
+        lat.guess_bounds()
+        from iris.fileformats.rules import regular_step
+        quarter = abs(regular_step(lon) * regular_step(lat) * 0.25)
+        half = abs(regular_step(lon) * regular_step(lat) * 0.5)
+        minx = 3.7499990463256836
+        maxx = 7.499998092651367
+        miny = 84.99998474121094
+        maxy = 89.99998474121094
+        geometry = shapely.geometry.box(minx, miny, maxx, maxy)
+        weights = geometry_area_weights(cube, geometry)
+        target = np.array([
+            [0, quarter, quarter, 0],
+            [0, half, half, 0],
+            [0, quarter, quarter, 0],
+            [0, 0, 0, 0]])
+        self.assertTrue(np.allclose(weights, target))
+
+    @tests.skip_data
+    def test_distinct_xy_bounds(self):
+        # cases where geometry bnds are outside cube bnds correctly handled?
+        cube = tests.stock.simple_pp()
+        cube = cube[:4, :4]
+        lon = cube.coord('longitude')
+        lat = cube.coord('latitude')
+        lon.guess_bounds()
+        lat.guess_bounds()
+        from iris.fileformats.rules import regular_step
+        quarter = abs(regular_step(lon) * regular_step(lat) * 0.25)
+        half = abs(regular_step(lon) * regular_step(lat) * 0.5)
+        full = abs(regular_step(lon) * regular_step(lat))
+        minx = 3.7499990463256836
+        maxx = 13.12499619
+        maxx_overshoot = 15.
+        miny = 84.99998474121094
+        maxy = 89.99998474121094
+        geometry = shapely.geometry.box(minx, miny, maxx, maxy)
+        geometry_overshoot = shapely.geometry.box(minx, miny, maxx_overshoot,
+                                                  maxy)
+        weights = geometry_area_weights(cube, geometry)
+        weights_overshoot = geometry_area_weights(cube, geometry_overshoot)
+        target = np.array([
+            [0, quarter, half, half],
+            [0, half, full, full],
+            [0, quarter, half, half],
+            [0, 0, 0, 0]])
+        self.assertTrue(np.allclose(weights, target))
+        self.assertTrue(np.allclose(weights_overshoot, target))
+
+    @tests.skip_data
+    def test_distinct_xy_bounds_pole(self):
+        # is UserWarning issued for out-of-bounds? results will be unexpected!
+        cube = tests.stock.simple_pp()
+        cube = cube[:4, :4]
+        lon = cube.coord('longitude')
+        lat = cube.coord('latitude')
+        lon.guess_bounds()
+        lat.guess_bounds()
+        from iris.fileformats.rules import regular_step
+        quarter = abs(regular_step(lon) * regular_step(lat) * 0.25)
+        half = abs(regular_step(lon) * regular_step(lat) * 0.5)
+        minx = 3.7499990463256836
+        maxx = 7.499998092651367
+        miny = 84.99998474121094
+        maxy = 99.99998474121094
+        geometry = shapely.geometry.box(minx, miny, maxx, maxy)
+        # see http://stackoverflow.com/a/3892301 to assert warnings
+        with warnings.catch_warnings(record=True) as w:
+            warnings.simplefilter("always")  # always trigger all warnings
+            weights = geometry_area_weights(cube, geometry)
+            self.assertEqual(str(w[-1].message), "The geometry exceeds the "
+                             "cube's y dimension at the upper end.")
+            self.assertTrue(issubclass(w[-1].category, UserWarning))
+        target = np.array([
+            [0, half, half, 0],
+            [0, half, half, 0],
+            [0, quarter, quarter, 0],
+            [0, 0, 0, 0]])
+        self.assertTrue(np.allclose(weights, target))
+
+    def test_shared_xy(self):
+        cube = tests.stock.track_1d()
+        geometry = shapely.geometry.box(1, 4, 3.5, 7)
+        weights = geometry_area_weights(cube, geometry)
+        target = np.array([0, 0, 2, 0.5, 0, 0, 0, 0, 0, 0, 0])
+        self.assertTrue(np.allclose(weights, target))
+
+
+if __name__ == "__main__":
+    tests.main()
