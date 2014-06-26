5ae7168b29f1dc8a06177b087b3027f09008ad88
Richard Hattersley <rhattersley@gmail.com>
2014-06-26 13:36:01 +0100

    Add missing import.

--- lib/iris/tests/experimental/test_raster.py
+++ lib/iris/tests/experimental/test_raster.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Iris.  If not, see <http://www.gnu.org/licenses/>.
 import iris.tests as tests
+import iris
 
 import numpy as np
 import PIL.Image
