8d5c5c1f68bc708271ef7189df654208ec70f9b0
Ed Campbell <drescampbell@gmail.com>
2014-06-26 15:45:59 +0100

    bump version to v1.8.0-dev after v1.7.x branch

--- setup.py
+++ setup.py
@@ -245,7 +245,7 @@ class BuildPyWithExtras(build_py.build_py):
 
 setup(
     name='Iris',
-    version='1.7.0-dev',
+    version='1.8.0-dev',
     url='http://scitools.org.uk/iris/',
     author='UK Met Office',
 
