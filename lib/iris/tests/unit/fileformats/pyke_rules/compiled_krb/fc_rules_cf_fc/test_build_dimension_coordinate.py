d8d98542bd6c33061b7ff1bf505a78c68c9f7116
Carwyn Pelley <cpelley.pub@gmail.com>
2014-08-19 15:35:14 +0100

    BUG: Fixed varname setting in aux con.

--- lib/iris/tests/unit/fileformats/pyke_rules/compiled_krb/fc_rules_cf_fc/test_build_dimension_coordinate.py
+++ lib/iris/tests/unit/fileformats/pyke_rules/compiled_krb/fc_rules_cf_fc/test_build_dimension_coordinate.py
@@ -24,15 +24,17 @@ fc_rules_cf_fc.build_dimension_coordinate`.
 # importing anything else
 import iris.tests as tests
 
+import warnings
+
 import numpy as np
 import mock
 
-from iris.coords import DimCoord
+from iris.coords import AuxCoord, DimCoord
 from iris.fileformats._pyke_rules.compiled_krb.fc_rules_cf_fc import \
     build_dimension_coordinate
 
 
-class TestBoundsVertexDim(tests.IrisTest):
+class _CommonMockTemplate(tests.IrisTest):
     def setUp(self):
         # Create coordinate cf variables and pyke engine.
         points = np.arange(6)
@@ -64,6 +66,75 @@ class TestBoundsVertexDim(tests.IrisTest):
             'iris.fileformats._pyke_rules.compiled_krb.'
             'fc_rules_cf_fc.deferred_load', new=deferred_load)
 
+
+class TestCoordConstruction(_CommonMockTemplate):
+    # Ensure that the coordinates constructed have all the relevant cf
+    # attributes populated.
+    def test_dim_coord_construction(self):
+        bounds = np.arange(12).reshape(6, 2)
+        self.cf_bounds_var = mock.Mock(
+            dimensions=('x', 'nv'),
+            cf_name='wibble_bnds',
+            shape=bounds.shape,
+            __getitem__=lambda self, key: bounds[key])
+
+        expected_coord = DimCoord(
+            self.cf_coord_var[:],
+            long_name=self.cf_coord_var.long_name,
+            var_name=self.cf_coord_var.cf_name,
+            units=self.cf_coord_var.units,
+            bounds=bounds)
+
+        get_cf_bounds_var_patch = mock.patch(
+            'iris.fileformats._pyke_rules.compiled_krb.'
+            'fc_rules_cf_fc.get_cf_bounds_var',
+            return_value=self.cf_bounds_var)
+
+        # Asserts must lie within context manager because of deferred loading.
+        with self.deferred_load_patch, get_cf_bounds_var_patch:
+            build_dimension_coordinate(self.engine, self.cf_coord_var)
+
+            # Test that expected coord is built and added to cube.
+            self.engine.cube.add_dim_coord.assert_called_with(
+                expected_coord, [0])
+
+    def test_aux_coord_construction(self):
+        # Use non monotonically increasing coordinates to force aux coord
+        # construction.
+        points = np.array([1, 3, 2, 4, 6, 5])
+        self.cf_coord_var.__getitem__ = lambda self, key: points[key]
+        bounds = np.arange(12).reshape(6, 2)
+        self.cf_bounds_var = mock.Mock(
+            dimensions=('x', 'nv'),
+            cf_name='wibble_bnds',
+            shape=bounds.shape,
+            __getitem__=lambda self, key: bounds[key])
+
+        expected_coord = AuxCoord(
+            self.cf_coord_var[:],
+            long_name=self.cf_coord_var.long_name,
+            var_name=self.cf_coord_var.cf_name,
+            units=self.cf_coord_var.units,
+            bounds=bounds)
+
+        get_cf_bounds_var_patch = mock.patch(
+            'iris.fileformats._pyke_rules.compiled_krb.'
+            'fc_rules_cf_fc.get_cf_bounds_var',
+            return_value=self.cf_bounds_var)
+        warning_patch = mock.patch('warnings.warn')
+
+        # Asserts must lie within context manager because of deferred loading.
+        with warning_patch, self.deferred_load_patch, get_cf_bounds_var_patch:
+            build_dimension_coordinate(self.engine, self.cf_coord_var)
+
+            # Test that expected coord is built and added to cube.
+            self.engine.cube.add_aux_coord.assert_called_with(
+                expected_coord, [0])
+            self.assertIn("creating 'wibble' auxiliary coordinate instead",
+                          warnings.warn.call_args[0][0])
+
+
+class TestBoundsVertexDim(_CommonMockTemplate):
     def test_slowest_varying_vertex_dim(self):
         # Create the bounds cf variable.
         bounds = np.arange(12).reshape(2, 6)
