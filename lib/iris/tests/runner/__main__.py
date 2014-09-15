1c811518c605c112718336ecf28d4396a0e8a35e
Andrew Dawson <ajdawson@acm.org>
2014-09-15 14:45:12 +0100

    Allow users to choose the number of processors used for running tests.

--- lib/iris/tests/runner/__main__.py
+++ lib/iris/tests/runner/__main__.py
@@ -28,6 +28,7 @@ from _runner import TestRunner
 parser = argparse.ArgumentParser('iris.tests',
                                  description=TestRunner.description)
 for long_opt, short_opt, help_text in TestRunner.user_options:
+    long_opt = long_opt.strip('=')
     if long_opt in TestRunner.boolean_options:
         parser.add_argument('--' + long_opt, '-' + short_opt,
                             action='store_true', help=help_text)
@@ -40,7 +41,7 @@ runner = TestRunner()
 
 runner.initialize_options()
 for long_opt, short_opt, help_text in TestRunner.user_options:
-    arg = long_opt.replace('-', '_')
+    arg = long_opt.replace('-', '_').strip('=')
     setattr(runner, arg, getattr(args, arg))
 runner.finalize_options()
 
