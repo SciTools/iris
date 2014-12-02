85c95980d003a3eb7bc11e71e325b2aa7122fbf2
Ed Campbell <drescampbell@gmail.com>
2014-12-02 13:16:39 +0000

    more grib_set_long to grib_set test changes

--- lib/iris/tests/test_grib_save_rules.py
+++ lib/iris/tests/test_grib_save_rules.py
@@ -32,52 +32,52 @@ import iris.fileformats.grib._save_rules as grib_save_rules
 
 
 class Test_set_fixed_surfaces(tests.IrisTest):
-    @mock.patch.object(gribapi, "grib_set_long")
-    def test_altitude_point(self, mock_set_long):
+    @mock.patch.object(gribapi, "grib_set")
+    def test_altitude_point(self, mock_set):
         grib = None
         cube = iris.cube.Cube([1, 2, 3, 4, 5])
         cube.add_aux_coord(iris.coords.AuxCoord([12345], "altitude", units="m"))
 
         grib_save_rules.set_fixed_surfaces(cube, grib)
 
-        mock_set_long.assert_any_call(grib, "typeOfFirstFixedSurface", 102)
-        mock_set_long.assert_any_call(grib, "scaleFactorOfFirstFixedSurface", 0)
-        mock_set_long.assert_any_call(grib, "scaledValueOfFirstFixedSurface", 12345)
-        mock_set_long.assert_any_call(grib, "typeOfSecondFixedSurface", -1)
-        mock_set_long.assert_any_call(grib, "scaleFactorOfSecondFixedSurface", 255)
-        mock_set_long.assert_any_call(grib, "scaledValueOfSecondFixedSurface", -1)
+        mock_set.assert_any_call(grib, "typeOfFirstFixedSurface", 102)
+        mock_set.assert_any_call(grib, "scaleFactorOfFirstFixedSurface", 0)
+        mock_set.assert_any_call(grib, "scaledValueOfFirstFixedSurface", 12345)
+        mock_set.assert_any_call(grib, "typeOfSecondFixedSurface", -1)
+        mock_set.assert_any_call(grib, "scaleFactorOfSecondFixedSurface", 255)
+        mock_set.assert_any_call(grib, "scaledValueOfSecondFixedSurface", -1)
 
-    @mock.patch.object(gribapi, "grib_set_long")
-    def test_height_point(self, mock_set_long):
+    @mock.patch.object(gribapi, "grib_set")
+    def test_height_point(self, mock_set):
         grib = None
         cube = iris.cube.Cube([1, 2, 3, 4, 5])
         cube.add_aux_coord(iris.coords.AuxCoord([12345], "height", units="m"))
 
         grib_save_rules.set_fixed_surfaces(cube, grib)
 
-        mock_set_long.assert_any_call(grib, "typeOfFirstFixedSurface", 103)
-        mock_set_long.assert_any_call(grib, "scaleFactorOfFirstFixedSurface", 0)
-        mock_set_long.assert_any_call(grib, "scaledValueOfFirstFixedSurface", 12345)
-        mock_set_long.assert_any_call(grib, "typeOfSecondFixedSurface", -1)
-        mock_set_long.assert_any_call(grib, "scaleFactorOfSecondFixedSurface", 255)
-        mock_set_long.assert_any_call(grib, "scaledValueOfSecondFixedSurface", -1)
+        mock_set.assert_any_call(grib, "typeOfFirstFixedSurface", 103)
+        mock_set.assert_any_call(grib, "scaleFactorOfFirstFixedSurface", 0)
+        mock_set.assert_any_call(grib, "scaledValueOfFirstFixedSurface", 12345)
+        mock_set.assert_any_call(grib, "typeOfSecondFixedSurface", -1)
+        mock_set.assert_any_call(grib, "scaleFactorOfSecondFixedSurface", 255)
+        mock_set.assert_any_call(grib, "scaledValueOfSecondFixedSurface", -1)
 
-    @mock.patch.object(gribapi, "grib_set_long")
-    def test_no_vertical(self, mock_set_long):
+    @mock.patch.object(gribapi, "grib_set")
+    def test_no_vertical(self, mock_set):
         grib = None
         cube = iris.cube.Cube([1, 2, 3, 4, 5])
         grib_save_rules.set_fixed_surfaces(cube, grib)
-        mock_set_long.assert_any_call(grib, "typeOfFirstFixedSurface", 1)
-        mock_set_long.assert_any_call(grib, "scaleFactorOfFirstFixedSurface", 0)
-        mock_set_long.assert_any_call(grib, "scaledValueOfFirstFixedSurface", 0)
-        mock_set_long.assert_any_call(grib, "typeOfSecondFixedSurface", -1)
-        mock_set_long.assert_any_call(grib, "scaleFactorOfSecondFixedSurface", 255)
-        mock_set_long.assert_any_call(grib, "scaledValueOfSecondFixedSurface", -1)
+        mock_set.assert_any_call(grib, "typeOfFirstFixedSurface", 1)
+        mock_set.assert_any_call(grib, "scaleFactorOfFirstFixedSurface", 0)
+        mock_set.assert_any_call(grib, "scaledValueOfFirstFixedSurface", 0)
+        mock_set.assert_any_call(grib, "typeOfSecondFixedSurface", -1)
+        mock_set.assert_any_call(grib, "scaleFactorOfSecondFixedSurface", 255)
+        mock_set.assert_any_call(grib, "scaledValueOfSecondFixedSurface", -1)
 
 
 class Test_phenomenon(tests.IrisTest):
-    @mock.patch.object(gribapi, "grib_set_long")
-    def test_phenom_unknown(self, mock_set_long):
+    @mock.patch.object(gribapi, "grib_set")
+    def test_phenom_unknown(self, mock_set):
         grib = None
         cube = iris.cube.Cube(np.array([1.0]))
         # Force reset of warnings registry to avoid suppression of
@@ -93,34 +93,34 @@ class Test_phenomenon(tests.IrisTest):
         grib = None
         cube = iris.cube.Cube(np.array([1.0]))
         grib_save_rules.set_discipline_and_parameter(cube, grib)
-        mock_set_long.assert_any_call(grib, "discipline", 255)
-        mock_set_long.assert_any_call(grib, "parameterCategory", 255)
-        mock_set_long.assert_any_call(grib, "parameterNumber", 255)
+        mock_set.assert_any_call(grib, "discipline", 255)
+        mock_set.assert_any_call(grib, "parameterCategory", 255)
+        mock_set.assert_any_call(grib, "parameterNumber", 255)
 
-    @mock.patch.object(gribapi, "grib_set_long")
-    def test_phenom_known_standard_name(self, mock_set_long):
+    @mock.patch.object(gribapi, "grib_set")
+    def test_phenom_known_standard_name(self, mock_set):
         grib = None
         cube = iris.cube.Cube(np.array([1.0]),
                               standard_name='sea_surface_temperature')
         grib_save_rules.set_discipline_and_parameter(cube, grib)
-        mock_set_long.assert_any_call(grib, "discipline", 10)
-        mock_set_long.assert_any_call(grib, "parameterCategory", 3)
-        mock_set_long.assert_any_call(grib, "parameterNumber", 0)
+        mock_set.assert_any_call(grib, "discipline", 10)
+        mock_set.assert_any_call(grib, "parameterCategory", 3)
+        mock_set.assert_any_call(grib, "parameterNumber", 0)
 
-    @mock.patch.object(gribapi, "grib_set_long")
-    def test_phenom_known_long_name(self, mock_set_long):
+    @mock.patch.object(gribapi, "grib_set")
+    def test_phenom_known_long_name(self, mock_set):
         grib = None
         cube = iris.cube.Cube(np.array([1.0]),
                               long_name='cloud_mixing_ratio')
         grib_save_rules.set_discipline_and_parameter(cube, grib)
-        mock_set_long.assert_any_call(grib, "discipline", 0)
-        mock_set_long.assert_any_call(grib, "parameterCategory", 1)
-        mock_set_long.assert_any_call(grib, "parameterNumber", 22)
+        mock_set.assert_any_call(grib, "discipline", 0)
+        mock_set.assert_any_call(grib, "parameterCategory", 1)
+        mock_set.assert_any_call(grib, "parameterNumber", 22)
 
 
 class Test_type_of_statistical_processing(tests.IrisTest):
-    @mock.patch.object(gribapi, "grib_set_long")
-    def test_stats_type_min(self, mock_set_long):
+    @mock.patch.object(gribapi, "grib_set")
+    def test_stats_type_min(self, mock_set):
         grib = None
         cube = iris.cube.Cube(np.array([1.0]))
         time_unit = iris.unit.Unit('hours since 1970-01-01 00:00:00')
@@ -131,10 +131,10 @@ class Test_type_of_statistical_processing(tests.IrisTest):
         cube.add_aux_coord(time_coord, ())
         cube.add_cell_method(iris.coords.CellMethod('maximum', time_coord))
         grib_save_rules.product_definition_template_8(cube, grib)
-        mock_set_long.assert_any_call(grib, "typeOfStatisticalProcessing", 2)
+        mock_set.assert_any_call(grib, "typeOfStatisticalProcessing", 2)
 
-    @mock.patch.object(gribapi, "grib_set_long")
-    def test_stats_type_max(self, mock_set_long):
+    @mock.patch.object(gribapi, "grib_set")
+    def test_stats_type_max(self, mock_set):
         grib = None
         cube = iris.cube.Cube(np.array([1.0]))
         time_unit = iris.unit.Unit('hours since 1970-01-01 00:00:00')
@@ -145,7 +145,7 @@ class Test_type_of_statistical_processing(tests.IrisTest):
         cube.add_aux_coord(time_coord, ())
         cube.add_cell_method(iris.coords.CellMethod('minimum', time_coord))
         grib_save_rules.product_definition_template_8(cube, grib)
-        mock_set_long.assert_any_call(grib, "typeOfStatisticalProcessing", 3)
+        mock_set.assert_any_call(grib, "typeOfStatisticalProcessing", 3)
 
 
 if __name__ == "__main__":
