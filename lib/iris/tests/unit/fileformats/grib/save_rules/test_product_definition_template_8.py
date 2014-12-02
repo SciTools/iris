a65fec779ce8c88e59053c3f26d2fc11a680053d
Ed Campbell <drescampbell@gmail.com>
2014-12-02 11:18:42 +0000

    move to different mock pattern

--- lib/iris/tests/unit/fileformats/grib/save_rules/test_product_definition_template_8.py
+++ lib/iris/tests/unit/fileformats/grib/save_rules/test_product_definition_template_8.py
@@ -35,9 +35,6 @@ import iris.tests.stock as stock
 from iris.fileformats.grib._save_rules import product_definition_template_8
 
 
-GRIB_API = 'iris.fileformats.grib._save_rules.gribapi'
-
-
 class TestTypeOfStatisticalProcessing(tests.IrisTest):
     def setUp(self):
         self.cube = stock.lat_lon_cube()
@@ -47,55 +44,45 @@ class TestTypeOfStatisticalProcessing(tests.IrisTest):
                          units=Unit('days since epoch', calendar='standard'))
         self.cube.add_aux_coord(coord)
 
-    def test_sum(self):
+    @mock.patch.object(gribapi, 'grib_set_long')
+    def test_sum(self, mock_set):
         cube = self.cube
         cell_method = CellMethod(method='sum', coords=['time'])
         cube.add_cell_method(cell_method)
 
-        grib = mock.Mock()
-        mock_gribapi = mock.Mock(spec=gribapi)
-        with mock.patch(GRIB_API, mock_gribapi):
-            product_definition_template_8(cube, grib)
-        mock_gribapi.assert_has_calls(mock.call.grib_set_long(
-            grib, "typeOfStatisticalProcessing", 1))
+        product_definition_template_8(cube, mock.sentinel.grib)
+        mock_set.assert_any_call(mock.sentinel.grib,
+                                 "typeOfStatisticalProcessing", 1)
 
-    def test_unrecognised(self):
+    @mock.patch.object(gribapi, 'grib_set_long')
+    def test_unrecognised(self, mock_set):
         cube = self.cube
         cell_method = CellMethod(method='95th percentile', coords=['time'])
         cube.add_cell_method(cell_method)
 
-        grib = mock.sentinel.grib_msg_id
-        mock_gribapi = mock.Mock(spec=gribapi)
-        with mock.patch(GRIB_API, mock_gribapi):
-            product_definition_template_8(cube, grib)
-        mock_gribapi.assert_has_calls(mock.call.grib_set_long(
-            grib, "typeOfStatisticalProcessing", 255))
+        product_definition_template_8(cube, mock.sentinel.grib)
+        mock_set.assert_any_call(mock.sentinel.grib,
+                                 "typeOfStatisticalProcessing", 255)
 
-    def test_multiple_cell_method_coords(self):
+    @mock.patch.object(gribapi, 'grib_set_long')
+    def test_multiple_cell_method_coords(self, mock_set):
         cube = self.cube
         cell_method = CellMethod(method='sum',
                                  coords=['time', 'forecast_period'])
         cube.add_cell_method(cell_method)
+        with self.assertRaisesRegexp(ValueError,
+                                     'Cannot handle multiple coordinate name'):
+            product_definition_template_8(cube, mock.sentinel.grib)
 
-        grib = mock.Mock()
-        mock_gribapi = mock.Mock(spec=gribapi)
-        with mock.patch(GRIB_API, mock_gribapi):
-            with self.assertRaisesRegexp(
-                    ValueError, 'Cannot handle multiple coordinate name'):
-                product_definition_template_8(cube, grib)
-
-    def test_cell_method_coord_name_fail(self):
+    @mock.patch.object(gribapi, 'grib_set_long')
+    def test_cell_method_coord_name_fail(self, mock_set):
         cube = self.cube
         cell_method = CellMethod(method='mean', coords=['season'])
         cube.add_cell_method(cell_method)
-
-        grib = mock.Mock()
-        mock_gribapi = mock.Mock(spec=gribapi)
-        with mock.patch(GRIB_API, mock_gribapi):
-            with self.assertRaisesRegexp(
-                    ValueError, "Expected a cell method with a coordinate "
-                    "name of 'time'"):
-                product_definition_template_8(cube, grib)
+        with self.assertRaisesRegexp(
+                ValueError, "Expected a cell method with a coordinate "
+                "name of 'time'"):
+            product_definition_template_8(cube, mock.sentinel.grib)
 
 
 class TestTimeCoordPrerequisites(tests.IrisTest):
@@ -104,44 +91,38 @@ class TestTimeCoordPrerequisites(tests.IrisTest):
         # Rename cube to avoid warning about unknown discipline/parameter.
         self.cube.rename('air_temperature')
 
-    def test_multiple_points(self):
+    @mock.patch.object(gribapi, 'grib_set_long')
+    def test_multiple_points(self, mock_set):
         # Add time coord with multiple points.
         coord = DimCoord([23, 24, 25], 'time',
                          bounds=[[22, 23], [23, 24], [24, 25]],
                          units=Unit('days since epoch', calendar='standard'))
         self.cube.add_aux_coord(coord, 0)
-        grib = mock.Mock()
-        mock_gribapi = mock.Mock(spec=gribapi)
-        with mock.patch(GRIB_API, mock_gribapi):
-            with self.assertRaisesRegexp(
-                    ValueError, 'Expected length one time coordinate'):
-                product_definition_template_8(self.cube, grib)
+        with self.assertRaisesRegexp(
+                ValueError, 'Expected length one time coordinate'):
+            product_definition_template_8(self.cube, mock.sentinel.grib)
 
-    def test_no_bounds(self):
+    @mock.patch.object(gribapi, 'grib_set_long')
+    def test_no_bounds(self, mock_set):
         # Add time coord with no bounds.
         coord = DimCoord(23, 'time',
                          units=Unit('days since epoch', calendar='standard'))
         self.cube.add_aux_coord(coord)
-        grib = mock.Mock()
-        mock_gribapi = mock.Mock(spec=gribapi)
-        with mock.patch(GRIB_API, mock_gribapi):
-            with self.assertRaisesRegexp(
-                    ValueError, 'Expected time coordinate with two bounds, '
-                    'got 0 bounds'):
-                product_definition_template_8(self.cube, grib)
+        with self.assertRaisesRegexp(
+                ValueError, 'Expected time coordinate with two bounds, '
+                'got 0 bounds'):
+            product_definition_template_8(self.cube, mock.sentinel.grib)
 
-    def test_more_than_two_bounds(self):
+    @mock.patch.object(gribapi, 'grib_set_long')
+    def test_more_than_two_bounds(self, mock_set):
         # Add time coord with more than two bounds.
         coord = DimCoord(23, 'time', bounds=[21, 22, 23],
                          units=Unit('days since epoch', calendar='standard'))
         self.cube.add_aux_coord(coord)
-        grib = mock.Mock()
-        mock_gribapi = mock.Mock(spec=gribapi)
-        with mock.patch(GRIB_API, mock_gribapi):
-            with self.assertRaisesRegexp(
-                    ValueError, 'Expected time coordinate with two bounds, '
-                    'got 3 bounds'):
-                product_definition_template_8(self.cube, grib)
+        with self.assertRaisesRegexp(
+                ValueError, 'Expected time coordinate with two bounds, '
+                'got 3 bounds'):
+            product_definition_template_8(self.cube, mock.sentinel.grib)
 
 
 class TestEndOfOverallTimeInterval(tests.IrisTest):
@@ -152,57 +133,58 @@ class TestEndOfOverallTimeInterval(tests.IrisTest):
         cell_method = CellMethod(method='sum', coords=['time'])
         self.cube.add_cell_method(cell_method)
 
-    def test_default_calendar(self):
+    @mock.patch.object(gribapi, 'grib_set_long')
+    def test_default_calendar(self, mock_set):
         cube = self.cube
         # End bound is 1972-04-26 10:27:07.
         coord = DimCoord(23.0, 'time', bounds=[0.0, 20314.452],
                          units=Unit('hours since epoch'))
         cube.add_aux_coord(coord)
 
-        grib = mock.Mock()
-        mock_gribapi = mock.Mock(spec=gribapi)
-        with mock.patch(GRIB_API, mock_gribapi):
-            product_definition_template_8(cube, grib)
-        mock_gribapi.assert_has_calls(mock.call.grib_set_long(
-            grib, "yearOfEndOfOverallTimeInterval", 1972))
-        mock_gribapi.assert_has_calls(mock.call.grib_set_long(
-            grib, "monthOfEndOfOverallTimeInterval", 4))
-        mock_gribapi.assert_has_calls(mock.call.grib_set_long(
-            grib, "dayOfEndOfOverallTimeInterval", 26))
-        mock_gribapi.assert_has_calls(mock.call.grib_set_long(
-            grib, "hourOfEndOfOverallTimeInterval", 10))
-        mock_gribapi.assert_has_calls(mock.call.grib_set_long(
-            grib, "minuteOfEndOfOverallTimeInterval", 27))
-        mock_gribapi.assert_has_calls(mock.call.grib_set_long(
-            grib, "secondOfEndOfOverallTimeInterval", 7))
+        grib = mock.sentinel.grib
+        product_definition_template_8(cube, grib)
 
-    def test_360_day_calendar(self):
+        mock_set.assert_any_call(
+            grib, "yearOfEndOfOverallTimeInterval", 1972)
+        mock_set.assert_any_call(
+            grib, "monthOfEndOfOverallTimeInterval", 4)
+        mock_set.assert_any_call(
+            grib, "dayOfEndOfOverallTimeInterval", 26)
+        mock_set.assert_any_call(
+            grib, "hourOfEndOfOverallTimeInterval", 10)
+        mock_set.assert_any_call(
+            grib, "minuteOfEndOfOverallTimeInterval", 27)
+        mock_set.assert_any_call(
+            grib, "secondOfEndOfOverallTimeInterval", 7)
+
+    @mock.patch.object(gribapi, 'grib_set_long')
+    def test_360_day_calendar(self, mock_set):
         cube = self.cube
         # End bound is 1972-05-07 10:27:07
         coord = DimCoord(23.0, 'time', bounds=[0.0, 20314.452],
                          units=Unit('hours since epoch', calendar='360_day'))
         cube.add_aux_coord(coord)
 
-        grib = mock.Mock()
-        mock_gribapi = mock.Mock(spec=gribapi)
-        with mock.patch(GRIB_API, mock_gribapi):
-            product_definition_template_8(cube, grib)
-        mock_gribapi.assert_has_calls(mock.call.grib_set_long(
-            grib, "yearOfEndOfOverallTimeInterval", 1972))
-        mock_gribapi.assert_has_calls(mock.call.grib_set_long(
-            grib, "monthOfEndOfOverallTimeInterval", 5))
-        mock_gribapi.assert_has_calls(mock.call.grib_set_long(
-            grib, "dayOfEndOfOverallTimeInterval", 7))
-        mock_gribapi.assert_has_calls(mock.call.grib_set_long(
-            grib, "hourOfEndOfOverallTimeInterval", 10))
-        mock_gribapi.assert_has_calls(mock.call.grib_set_long(
-            grib, "minuteOfEndOfOverallTimeInterval", 27))
-        mock_gribapi.assert_has_calls(mock.call.grib_set_long(
-            grib, "secondOfEndOfOverallTimeInterval", 7))
+        grib = mock.sentinel.grib
+        product_definition_template_8(cube, grib)
+
+        mock_set.assert_any_call(
+            grib, "yearOfEndOfOverallTimeInterval", 1972)
+        mock_set.assert_any_call(
+            grib, "monthOfEndOfOverallTimeInterval", 5)
+        mock_set.assert_any_call(
+            grib, "dayOfEndOfOverallTimeInterval", 7)
+        mock_set.assert_any_call(
+            grib, "hourOfEndOfOverallTimeInterval", 10)
+        mock_set.assert_any_call(
+            grib, "minuteOfEndOfOverallTimeInterval", 27)
+        mock_set.assert_any_call(
+            grib, "secondOfEndOfOverallTimeInterval", 7)
 
 
 class TestNumberOfTimeRange(tests.IrisTest):
-    def test_other_cell_methods(self):
+    @mock.patch.object(gribapi, 'grib_set_long')
+    def test_other_cell_methods(self, mock_set):
         cube = stock.lat_lon_cube()
         # Rename cube to avoid warning about unknown discipline/parameter.
         cube.rename('air_temperature')
@@ -215,12 +197,8 @@ class TestNumberOfTimeRange(tests.IrisTest):
         cell_method = CellMethod(method='sum', coords=['time'])
         cube.add_cell_method(cell_method)
 
-        grib = mock.Mock()
-        mock_gribapi = mock.Mock(spec=gribapi)
-        with mock.patch(GRIB_API, mock_gribapi):
-            product_definition_template_8(cube, grib)
-        mock_gribapi.assert_has_calls(mock.call.grib_set_long(
-            grib, 'numberOfTimeRange', 1))
+        product_definition_template_8(cube, mock.sentinel.grib)
+        mock_set.assert_any_call(mock.sentinel.grib, 'numberOfTimeRange', 1)
 
 
 if __name__ == "__main__":
