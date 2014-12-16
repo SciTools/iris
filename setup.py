7d21d9b569cd5a0cbc67577e1e052d7b5171a0b3
Laura Dreyer <laura_dreyer@yahoo.co.uk>
2014-12-16 09:46:21 +0000

    Bump version to v1.7.3

--- setup.py
+++ setup.py
@@ -245,7 +245,7 @@ class BuildPyWithExtras(build_py.build_py):
 
 setup(
     name='Iris',
-    version='1.7.3-rc.1',
+    version='1.7.3',
     url='http://scitools.org.uk/iris/',
     author='UK Met Office',
 
