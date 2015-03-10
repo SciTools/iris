18b9c5d8a2d5a4240fe120456dde1d128f5d9801
Karina Williams <karina.williams@metoffice.gov.uk>
2015-03-10 21:47:17 +0000

    header edits

--- lib/iris/tests/unit/util/test_squeeze.py
+++ lib/iris/tests/unit/util/test_squeeze.py
@@ -1,3 +1,8 @@
+# (C) British Crown Copyright 2010 - 2015, Met Office
+#
+# This file is part of Iris.
+#
+# Iris is free software: you can redistribute it and/or modify it under
 # the terms of the GNU Lesser General Public License as published by the
 # Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
