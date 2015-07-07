971779356305891539d9cba5cc9e46731698a906
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2015-07-06 21:35:23 -0400

    py3k: Correct Unicode usage in merge test.

--- lib/iris/tests/test_merge.py
+++ lib/iris/tests/test_merge.py
@@ -194,7 +194,8 @@ class TestCombination(tests.IrisTest):
                                     long_name='y', units='1'), 0)
 
         for name, value in zip(['a', 'b', 'c', 'd'], [a, b, c, d]):
-            dtype = np.str if isinstance(value, six.string_types) else np.float32
+            dtype = (np.dtype(six.text_type)
+                     if isinstance(value, six.string_types) else np.float32)
             cube.add_aux_coord(AuxCoord(np.array([value], dtype=dtype),
                                         long_name=name, units='1'))
 
