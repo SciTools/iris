d53d3e2f75fccc1776c3d05664074798d5266356
Corinne Bosley <corinne.bosley@metoffice.gov.uk>
2016-02-22 14:07:29 +0000

    Trajectory Interpo0lation Optimisation, working but still with failed tests

--- lib/iris/analysis/trajectory.py
+++ lib/iris/analysis/trajectory.py
@@ -1,4 +1,4 @@
-# (C) British Crown Copyright 2010 - 2015, Met Office
+# (C) British Crown Copyright 2010 - 2016, Met Office
 #
 # This file is part of Iris.
 #
@@ -261,7 +261,7 @@ def interpolate(cube, sample_points, method=None):
                 # cube instead of the value derived from the interpolation scheme, which becomes
                 # incorrect when using the nearest neighbour scheme (you get the nearest neighbour
                 # coordinate value instead of the sample coordinate value).
+                import pdb;pdb.set_trace()
                 new_cube.coord(column_coord.name()).points[i] = point[0][1]
-
 
     return new_cube
