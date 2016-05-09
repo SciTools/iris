d413654d0167dcfd10150c4368c4564e5becd932
Richard Hattersley <rhattersley@gmail.com>
2016-05-09 21:55:51 +0100

    Update tests for iris.analysis.cartography.project

--- lib/iris/tests/unit/analysis/cartography/test_project.py
+++ lib/iris/tests/unit/analysis/cartography/test_project.py
@@ -1,4 +1,4 @@
-# (C) British Crown Copyright 2014 - 2015, Met Office
+# (C) British Crown Copyright 2014 - 2016, Met Office
 #
 # This file is part of Iris.
 #
@@ -14,7 +14,7 @@
 #
 # You should have received a copy of the GNU Lesser General Public License
 # along with Iris.  If not, see <http://www.gnu.org/licenses/>.
-"""Unit tests for the :func:`iris.analysis.cartography.project` function."""
+"""Unit tests for :func:`iris.analysis.cartography.project`."""
 
 from __future__ import (absolute_import, division, print_function)
 from six.moves import (filter, input, map, range, zip)  # noqa
@@ -23,11 +23,26 @@ from six.moves import (filter, input, map, range, zip)  # noqa
 # importing anything else.
 import iris.tests as tests
 
+import cartopy.crs as ccrs
 import numpy as np
 
-import iris
-from iris.coords import DimCoord
+import iris.coord_systems
+import iris.coords
+import iris.cube
+import iris.tests
+import iris.tests.stock
+
 from iris.analysis.cartography import project
+
+
+ROBINSON = ccrs.Robinson()
+
+
+def low_res_4d():
+    cube = iris.tests.stock.realistic_4d_no_derived()
+    cube = cube[0:2, 0:3, ::10, ::10]
+    cube.remove_coord('surface_altitude')
+    return cube
 
 
 class TestAll(tests.IrisTest):
@@ -35,11 +50,11 @@ class TestAll(tests.IrisTest):
         cs = iris.coord_systems.GeogCS(654321)
         self.cube = iris.cube.Cube(np.zeros(25).reshape(5, 5))
         self.cube.add_dim_coord(
-            DimCoord(np.arange(5), standard_name="latitude", units='degrees',
-                     coord_system=cs), 0)
+            iris.coords.DimCoord(np.arange(5), standard_name="latitude",
+                                 units='degrees', coord_system=cs), 0)
         self.cube.add_dim_coord(
-            DimCoord(np.arange(5), standard_name="longitude", units='degrees',
-                     coord_system=cs), 1)
+            iris.coords.DimCoord(np.arange(5), standard_name="longitude",
+                                 units='degrees', coord_system=cs), 1)
 
         self.tcs = iris.coord_systems.GeogCS(600000)
 
@@ -55,6 +70,60 @@ class TestAll(tests.IrisTest):
         self.assertIsNot(res.coord('projection_x_coordinate').coord_system,
                          self.tcs)
 
+    def test_bad_resolution_negative(self):
+        cube = low_res_4d()
+        with self.assertRaises(ValueError):
+            project(cube, ROBINSON, nx=-200, ny=200)
 
-if __name__ == "__main__":
+    def test_bad_resolution_non_numeric(self):
+        cube = low_res_4d()
+        with self.assertRaises(ValueError):
+            project(cube, ROBINSON, nx=200, ny='abc')
+
+    def test_missing_lat(self):
+        cube = low_res_4d()
+        cube.remove_coord('grid_latitude')
+        with self.assertRaises(ValueError):
+            project(cube, ROBINSON)
+
+    def test_missing_lon(self):
+        cube = low_res_4d()
+        cube.remove_coord('grid_longitude')
+        with self.assertRaises(ValueError):
+            project(cube, ROBINSON)
+
+    def test_missing_latlon(self):
+        cube = low_res_4d()
+        cube.remove_coord('grid_longitude')
+        cube.remove_coord('grid_latitude')
+        with self.assertRaises(ValueError):
+            project(cube, ROBINSON)
+
+    def test_default_resolution(self):
+        cube = low_res_4d()
+        new_cube, extent = project(cube, ROBINSON)
+        self.assertEqual(new_cube.shape, cube.shape)
+
+    def test_mismatched_coord_systems(self):
+        cube = low_res_4d()
+        cube.coord('grid_longitude').coord_system = None
+        with self.assertRaises(ValueError):
+            project(cube, ROBINSON)
+
+    @tests.skip_data
+    def test_no_coord_system(self):
+        cube = iris.load_cube(tests.get_data_path(('PP', 'aPPglob1',
+                                                   'global.pp')))
+        cube.coord('longitude').coord_system = None
+        cube.coord('latitude').coord_system = None
+        with iris.tests.mock.patch('warnings.warn') as warn:
+            new_cube, extent = project(cube, ROBINSON)
+        warn.assert_called_once_with('Coordinate system of latitude and '
+                                     'longitude coordinates is not specified. '
+                                     'Assuming WGS84 Geodetic.')
+        self.assertCML(new_cube,
+                       ('analysis', 'project', 'default_source_cs.cml'))
+
+
+if __name__ == '__main__':
     tests.main()
