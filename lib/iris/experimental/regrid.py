Add test for multidimensional operation, fix masking bug.

lib/iris/experimental/regrid.py
@@ -1072,6 +1072,11 @@ def _regrid_weighted_curvilinear_to_rectilinear__prepare(
     # contribution from one or more source points.
     rows = np.nonzero(sum_weights)
 
+    # NOTE: when source points are masked, this 'sum_weights' is possibly
+    # incorrect and needs re-calculating.  Likewise 'rows' may cover target
+    # cells which happen to get no data.  This is dealt with by adjusting as
+    # required in the '__perform' function, below.
+
     regrid_info = (sparse_matrix, sum_weights, rows, grid_cube)
     return regrid_info
 
@@ -1087,15 +1092,35 @@ def _regrid_weighted_curvilinear_to_rectilinear__perform(
     sparse_matrix, sum_weights, rows, grid_cube = regrid_info
 
     # Calculate the numerator of the weighted mean (M, 1).
-    if not ma.isMaskedArray(src_cube.data):
+    is_masked = ma.isMaskedArray(src_cube.data)
+    if not is_masked:
         data = src_cube.data
     else:
         # Use raw data array
         data = src_cube.data.data
-        # Zero any masked source points so they add nothing in output sums.
-        if np.ma.is_masked(src_cube.data):
+        # Check if there are any masked source points to take account of.
+        is_masked = np.ma.is_masked(src_cube.data)
+        if is_masked:
+            # Zero any masked source points so they add nothing in output sums.
             mask = src_cube.data.mask
             data[mask] = 0.0
+            # Calculate a new 'sum_weights' to allow for missing source points.
+            # N.B. it is more efficient to use the original once-calculated
+            # sparse matrix, but in this case we can't.
+            # Hopefully, this post-multiplying by the validities is less costly
+            # than repeating the whole sparse calculation.
+            valid_src_cells = ~mask.flat[:]
+            src_cell_validity_factors = sparse_diags(
+                np.array(valid_src_cells, dtype=int),
+                0)
+            valid_weights = sparse_matrix * src_cell_validity_factors
+            sum_weights = valid_weights.sum(axis=1).getA()
+            # Work out where output cells are missing all contributions.
+            # This allows for where 'rows' contains output cells that have no
+            # data because of missing input points.
+            zero_sums = sum_weights == 0.0
+            # Make sure we can still divide by sum_weights[rows].
+            sum_weights[zero_sums] = 1.0
 
     # Calculate sum in each target cell, over contributions from each source
     # cell.
@@ -1104,19 +1129,15 @@ def _regrid_weighted_curvilinear_to_rectilinear__perform(
     # Create a template for the weighted mean result.
     weighted_mean = ma.masked_all(numerator.shape, dtype=numerator.dtype)
 
-    # Account for any missing input data points.
-    if np.ma.is_masked(src_cube.data):
-        # Calculate a new 'sum_weights' to account for missing source points.
-        valid_src_cells = ~mask.flat[:]
-        src_cell_validity_factors = sparse_diags(
-            np.array(valid_src_cells, dtype=int),
-            0)
-        valid_weights = sparse_matrix * src_cell_validity_factors
-        sum_weights = valid_weights.sum(axis=1)
-        sum_weights = np.maximum(1, sum_weights).getA()
-
     # Calculate final results in all relevant places.
     weighted_mean[rows] = numerator[rows] / sum_weights[rows]
+    if is_masked:
+        # Ensure masked points where relevant source cells were all missing.
+        if np.any(zero_sums):
+            # Make masked if it wasn't.
+            weighted_mean = np.ma.asarray(weighted_mean)
+            # Mask where contributing sums were zero.
+            weighted_mean[zero_sums] = np.ma.masked
 
     # Construct the final regridded weighted mean cube.
     tx = grid_cube.coord(axis='x', dim_coords=True)

lib/iris/tests/unit/experimental/regrid/test__CurvilinearRegridder.py
@@ -25,8 +25,11 @@ import iris.tests as tests
 
 import numpy as np
 
+from iris.analysis.cartography import rotate_pole
 from iris.cube import Cube
-from iris.coords import AuxCoord
+from iris.coords import AuxCoord, DimCoord
+from iris.coord_systems import GeogCS, RotatedGeogCS
+from iris.fileformats.pp import EARTH_RADIUS
 from iris.experimental.regrid import _CurvilinearRegridder as Regridder
 from iris.tests import mock
 from iris.tests.stock import global_pp, lat_lon_cube
@@ -193,6 +196,109 @@ class Test___call____bad_src(tests.IrisTest):
                                      'not defined on the same source grid'):
             self.regridder(self.src_grid[::2, ::2])
 
+
+class Test__call__multidimensional(tests.IrisTest):
+    def test_multidim(self):
+        # Testing with >2D data to demonstrate correct operation over
+        # additional non-XY dimensions (including data masking), which is
+        # handled by the PointInCell wrapper class.
+
+        # Define a simple target grid first, in plain latlon coordinates.
+        plain_latlon_cs = GeogCS(EARTH_RADIUS)
+        grid_x_coord = DimCoord(points=[15.0, 25.0, 35.0],
+                                bounds=[[10.0, 20.0],
+                                        [20.0, 30.0],
+                                        [30.0, 40.0]],
+                                standard_name='longitude',
+                                units='degrees',
+                                coord_system=plain_latlon_cs)
+        grid_y_coord = DimCoord(points=[-30.0, -50.0],
+                                bounds=[[-20.0, -40.0], [-40.0, -60.0]],
+                                standard_name='latitude',
+                                units='degrees',
+                                coord_system=plain_latlon_cs)
+        grid_cube = Cube(np.zeros((2, 3)))
+        grid_cube.add_dim_coord(grid_y_coord, 0)
+        grid_cube.add_dim_coord(grid_x_coord, 1)
+
+        # Define some key points in true-lat/lon thta have known positions
+        # First 3x2 points in the centre of each output cell.
+        x_centres, y_centres = np.meshgrid(grid_x_coord.points,
+                                           grid_y_coord.points)
+        # An extra point also falling in cell 1, 1
+        x_in11, y_in11 = 26.3, -48.2
+        # An extra point completely outside the target grid
+        x_out, y_out = 70.0, -40.0
+
+        # Define a rotated coord system for the source data
+        pole_lon, pole_lat = -125.3, 53.4
+        src_cs = RotatedGeogCS(grid_north_pole_latitude=pole_lat,
+                               grid_north_pole_longitude=pole_lon,
+                               ellipsoid=plain_latlon_cs)
+
+        # Concatenate all the testpoints in a flat array, and find the rotated
+        # equivalents.
+        xx = list(x_centres.flat[:]) + [x_in11, x_out]
+        yy = list(y_centres.flat[:]) + [y_in11, y_out]
+        xx, yy = rotate_pole(lons=np.array(xx),
+                             lats=np.array(yy),
+                             pole_lon=pole_lon,
+                             pole_lat=pole_lat)
+        # Define handy index numbers for all these.
+        i00, i01, i02, i10, i11, i12, i_in, i_out = range(8)
+
+        # Build test data in the shape Z,YX = (3, 8)
+        data = [[1, 2, 3, 11, 12, 13, 7, 99],
+                [1, 2, 3, 11, 12, 13, 7, 99],
+                [7, 6, 5, 51, 52, 53, 12, 1]]
+        mask = [[0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 1, 0, 0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0]]
+        src_data = np.ma.array(data, mask=mask, dtype=float)
+
+        # Make the source cube.
+        src_cube = Cube(src_data)
+        src_x = AuxCoord(xx,
+                         standard_name='grid_longitude',
+                         units='degrees',
+                         coord_system=src_cs)
+        src_y = AuxCoord(yy,
+                         standard_name='grid_latitude',
+                         units='degrees',
+                         coord_system=src_cs)
+        src_z = DimCoord(np.arange(3), long_name='z')
+        src_cube.add_dim_coord(src_z, 0)
+        src_cube.add_aux_coord(src_x, 1)
+        src_cube.add_aux_coord(src_y, 1)
+        # Add in some extra metadata, to ensure it gets copied over.
+        src_cube.add_aux_coord(DimCoord([0], long_name='extra_scalar_coord'))
+        src_cube.attributes['extra_attr'] = 12.3
+
+        # Define what the expected answers should be, shaped (3, 2, 3).
+        expected_result = [
+            [[1.0, 2.0, 3.0],
+             [11.0, 0.5 * (12 + 7), 13.0]],
+            [[1.0, -999, 3.0],
+             [11.0, 12.0, 13.0]],
+            [[7.0, 6.0, 5.0],
+             [51.0, 0.5 * (52 + 12), 53.0]],
+            ]
+        expected_result = np.ma.masked_less(expected_result, 0)
+
+        # Perform the calculation with the regridder.
+        regridder = Regridder(src_cube, grid_cube)
+
+        # Check all is as expected.
+        result = regridder(src_cube)
+        self.assertEqual(result.coord('z'), src_cube.coord('z'))
+        self.assertEqual(result.coord('extra_scalar_coord'),
+                         src_cube.coord('extra_scalar_coord'))
+        self.assertEqual(result.coord('longitude'),
+                         grid_cube.coord('longitude'))
+        self.assertEqual(result.coord('latitude'),
+                         grid_cube.coord('latitude'))
+        self.assertMaskedArrayAlmostEqual(result.data, expected_result)
+        t_dbg = 0
 
 if __name__ == '__main__':
     tests.main()