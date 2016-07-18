78acb390ed14b20e2d42163a95871898c5c24136
lbdreyer <lbdreyer@users.noreply.github.com>
2016-07-18 14:14:34 +0100

    Support plotting non-gregorian calendars (#2067)
    * Use nc-time-axis

--- lib/iris/plot.py
+++ lib/iris/plot.py
@@ -37,6 +37,8 @@ import matplotlib.transforms as mpl_transforms
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mpl_ticker
 from mpl_toolkits.axes_grid.anchored_artists import AnchoredText
+import nc_time_axis
+import netcdftime
 import numpy as np
 import numpy.ma as ma
 
@@ -403,8 +405,16 @@ def _draw_2d_from_points(draw_method_name, arg_func, cube, *args, **kwargs):
 
 def _fixup_dates(coord, values):
     if coord.units.calendar is not None and values.ndim == 1:
-        r = [datetime.datetime(*(coord.units.num2date(val).timetuple()[0:6]))
-             for val in values]
+        # Convert coordinate values into tuples of
+        # (year, month, day, hour, min, sec)
+        dates = [coord.units.num2date(val).timetuple()[0:6]
+                 for val in values]
+        if coord.units.calendar == 'gregorian':
+            r = [datetime.datetime(*date) for date in dates]
+        else:
+            r = [nc_time_axis.CalendarDateTime(
+                 netcdftime.datetime(*date), coord.units.calendar)
+                 for date in dates]
         values = np.empty(len(r), dtype=object)
         values[:] = r
     return values
