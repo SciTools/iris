705404b7d5821fd1bfebb56cb5b668bb2438d28d
Carmona Loaiza Juan Manuel <jcarmona@argo-login2.ictp.it>
2016-08-12 17:09:31 +0200

    performing some minor tests

--- pipistrello/test_pipistrello.py
+++ pipistrello/test_pipistrello.py
@@ -14,16 +14,25 @@
 import pipistrello
 import utils
 import time
+import os
 
-time_a = time.clock()
-database_dir = ('/home/esp-shared-b/RegCM_Data/CAM2')
-all_cubes = pipistrello.database(database_dir,new_catalogue=True)
-time_b = time.clock()
+#time_a = time.clock()
+#database_dir = ('/home/esp-shared-b/RegCM_Data/CAM2')
+#database_dir = ('/home/esp-shared-b/RegCM_Data/regcm3')
+#all_cubes = pipistrello.database(database_dir,new_catalogue=True)
 
-print("total time taken: {} seconds.\n".format(time_b - time_a))
+#cubesA = all_cubes.load_cubes('')
+#for cube in cubesA:
+#	print(cube)
 
+#time_b = time.clock()
+
+#print("total time taken: {} seconds.\n".format(time_b - time_a))
+
+files_to_catalogue = os.listdir(self.location)
 
 
 #This cleans up leftover files (compiled python files)
 #and prints a good-bye message.
-utils.cleanup_and_finish()
+#utils.cleanup_and_finish()
+
