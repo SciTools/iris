9a2d2dfdac4de9b6602031d867eeab4ed300a9f8
Johan Swanljung <johan@Agda.local>
2016-10-14 23:44:38 +0200

    Put test_netcdf.py imports in alphabetical order.

--- lib/iris/tests/integration/test_netcdf.py
+++ lib/iris/tests/integration/test_netcdf.py
@@ -23,8 +23,8 @@ from six.moves import (filter, input, map, range, zip)  # noqa
 # importing anything else.
 import iris.tests as tests
 
-from itertools import repeat
 from contextlib import contextmanager
+from itertools import repeat
 import os.path
 import shutil
 import tempfile
