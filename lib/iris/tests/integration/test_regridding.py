f0a2c19676a410b136331555c3a976c2acd87e7b
Corinne Bosley <corinne.bosley@metoffice.gov.uk>
2016-11-28 13:57:50 +0000

    pep8 correction: reduced line length and hopefully fixed test filepath

--- lib/iris/tests/integration/test_regridding.py
+++ lib/iris/tests/integration/test_regridding.py
@@ -100,7 +100,8 @@ class TestGlobalSubsample(tests.IrisTest):
 
 class TestUnstructured(tests.IrisTest):
     def setUp(self):
-        path = tests.get_data_path('NetCDF/unstructured_grid/theta_nodal_xios.nc')
+        path = tests.get_data_path('NetCDF', 'unstructured_grid',
+                                   'theta_nodal_xios.nc')
         self.src = iris.load_cube(path, 'Potential Temperature')
         self.grid = simple_3d()[0, :, :]
 
