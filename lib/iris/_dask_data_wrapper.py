896f4da3370fe433e35306c7ca205e684694652c
Daniel Kirkham <daniel.kirkham@metoffice.gov.uk>
2017-02-17 14:21:04 +0000

    Modify cube.py to use wrapper

--- lib/iris/_dask_data_wrapper.py
+++ lib/iris/_dask_data_wrapper.py
@@ -1,17 +1,25 @@
-import dask
+import dask.array as da
+import numpy as np
+
 
 def from_proxy(proxy):
-    return DataWrapper(dask.from_array(proxy, np.ones(proxy.ndim))), True)
+    return _DataWrapper(dask.from_array(proxy, 1), True)
+
+
+def from_lazy_array(array):
+    return _DataWrapper(array, True)
+
 
 def from_array(array, lazy=False):
-    return DataWrapper(array, lazy)
+    return _DataWrapper(array, lazy)
+
 
 class _DataWrapper(object):
     def __init__(self, data, lazy):
         if lazy:
             self._real_data = None
             self._lazy_data = data
-        else
+        else:
             self._real_data = data
             self._lazy_data = None
 
@@ -19,8 +27,15 @@ class _DataWrapper(object):
         return self._real_data is None
 
     @property
+    def shape(self):
+        return self.real_or_lazy_data.shape
+
+    @property
+    def dtype(self):
+        return self.real_or_lazy_data.dtype
+
     def lazy_data(self):
-        return da.from_array(self._real_data, np.ones(self._real_data.ndim) \
+        return da.from_array(self._real_data, 1) \
             if self._real_data is not None else self._lazy_data
 
     @property
@@ -32,13 +47,23 @@ class _DataWrapper(object):
     def data(self):
         if self.has_lazy_data():
             self._real_data = self._lazy_data.compute()
-            # TODO: Delete self._lazy_data here?
+            del self._lazy_data
         return self._real_data
 
     def __getitem__(self, indices):
-        return from_array(self.lazy_data[indices], self.has_lazy_data())
+        return from_array(self.real_or_lazy_data[indices],
+                          self.has_lazy_data())
 
     def __add__(self, other):
-        lazy = self.has_lazy_data() or other.has_lazy_data():
+        lazy = self.has_lazy_data() or other.has_lazy_data()
         return from_array(
                 self.real_or_lazy_data + other.real_or_lazy_data, lazy)
+
+    def transpose(self, axis=None):
+        return from_array(self.real_or_lazy_data.transpose(),
+                          self.has_lazy_data())
+
+    def concatenate(self, wrappers, axis=0):
+        arrays = [wrapper.real_or_lazy_data for wrapper in wrappers]
+        lazy = np.any([wrapper.has_lazy_data() for wrapper in wrappers])
+        return from_array(da.concatenate(arrays, axis), lazy)
--- lib/iris/_data_wrapper.py
+++ lib/iris/_data_wrapper.py
@@ -1,1 +1,1 @@
-from _dask_data_wrapper import from_proxy, from_array
+from _dask_data_wrapper import from_proxy, from_array, from_lazy_array
