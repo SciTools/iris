083fecab736f658688215141baba9bee1aefeeed
Daniel Kirkham <djkirkham@users.noreply.github.com>
2017-02-23 17:02:30 +0000

    Refactor AuxCoordFactory unit tests to remove biggus references (#2401)
    * Refactor AuxCoordFactory unit tests to remove biggus references

--- lib/iris/tests/unit/aux_factory/test_AuxCoordFactory.py
+++ lib/iris/tests/unit/aux_factory/test_AuxCoordFactory.py
@@ -1,4 +1,4 @@
-# (C) British Crown Copyright 2015, Met Office
+# (C) British Crown Copyright 2015 - 2017, Met Office
 #
 # This file is part of Iris.
 #
@@ -26,7 +26,8 @@ from six.moves import (filter, input, map, range, zip)  # noqa
 # importing anything else.
 import iris.tests as tests
 
-import biggus
+import iris._lazy_data
+import dask.array as da
 import numpy as np
 
 import iris.coords
@@ -55,27 +56,27 @@ class Test__nd_points(tests.IrisTest):
         expected = points.T[np.newaxis, np.newaxis, ..., np.newaxis]
         self.assertArrayEqual(result, expected)
 
-    def test_biggus_simple(self):
+    def test_lazy_simple(self):
         raw_points = np.arange(12).reshape(4, 3)
-        points = biggus.NumpyArrayAdapter(raw_points)
+        points = da.from_array(raw_points, 1)
         coord = iris.coords.AuxCoord(points)
-        self.assertIsInstance(coord._points, biggus.Array)
+        self.assertTrue(iris._lazy_data.is_lazy_data(coord._points))
         result = AuxCoordFactory._nd_points(coord, (0, 1), 2)
         # Check we haven't triggered the loading of the coordinate values.
-        self.assertIsInstance(coord._points, biggus.Array)
-        self.assertIsInstance(result, biggus.Array)
+        self.assertTrue(iris._lazy_data.is_lazy_data(coord._points))
+        self.assertTrue(iris._lazy_data.is_lazy_data(result))
         expected = raw_points
         self.assertArrayEqual(result, expected)
 
-    def test_biggus_complex(self):
+    def test_lazy_complex(self):
         raw_points = np.arange(12).reshape(4, 3)
-        points = biggus.NumpyArrayAdapter(raw_points)
+        points = da.from_array(raw_points, 1)
         coord = iris.coords.AuxCoord(points)
-        self.assertIsInstance(coord._points, biggus.Array)
+        self.assertTrue(iris._lazy_data.is_lazy_data(coord._points))
         result = AuxCoordFactory._nd_points(coord, (3, 2), 5)
         # Check we haven't triggered the loading of the coordinate values.
-        self.assertIsInstance(coord._points, biggus.Array)
-        self.assertIsInstance(result, biggus.Array)
+        self.assertTrue(iris._lazy_data.is_lazy_data(coord._points))
+        self.assertTrue(iris._lazy_data.is_lazy_data(result))
         expected = raw_points.T[np.newaxis, np.newaxis, ..., np.newaxis]
         self.assertArrayEqual(result, expected)
 
