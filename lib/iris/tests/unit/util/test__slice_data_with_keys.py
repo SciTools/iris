8b51b04a40156a9404d04f9b459d71950011e772
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2017-05-05 12:18:46 +0100

    Added purely results-based tests.

--- lib/iris/tests/unit/util/test__slice_data_with_keys.py
+++ lib/iris/tests/unit/util/test__slice_data_with_keys.py
@@ -34,6 +34,7 @@ import iris.tests as tests
 import numpy as np
 
 from iris.util import _slice_data_with_keys
+from iris._lazy_data import as_lazy_data, as_concrete_data
 
 
 class DummyArray(object):
@@ -251,6 +252,158 @@ class Test_dimensions_mapping(MixinIndexingTest, tests.IrisTest):
         # Cutting out the middle dim.
         self.check((3, 4, 2), Index[:, 2],
                    expect_map={None: None, 0: 0, 1: None, 2: 1})
+
+
+class TestResults(tests.IrisTest):
+    # Integration-style test, exercising (mostly) the same cases as above,
+    # but checking actual results, for both real and lazy array inputs.
+
+    def check(self, real_data, keys, expect_result, expect_map):
+        real_data = np.array(real_data)
+        lazy_data = as_lazy_data(real_data, real_data.shape)
+        real_dim_map, real_result = _slice_data_with_keys(real_data, keys)
+        lazy_dim_map, lazy_result = _slice_data_with_keys(lazy_data, keys)
+        lazy_result = as_concrete_data(lazy_result)
+        self.assertArrayEqual(real_result, expect_result)
+        self.assertArrayEqual(lazy_result, expect_result)
+        self.assertEqual(real_dim_map, expect_map)
+        self.assertEqual(lazy_dim_map, expect_map)
+
+    def test_1d_int(self):
+        self.check([1, 2, 3, 4], Index[2],
+                   [3],
+                   {None: None, 0: None})
+
+    def test_1d_all(self):
+        self.check([1, 2, 3], Index[:],
+                   [1, 2, 3],
+                   {None: None, 0: 0})
+
+    def test_1d_tuple(self):
+        self.check([1, 2, 3], Index[(2, 0, 1, 0), ],
+                   [3, 1, 2, 1],
+                   {None: None, 0: 0})
+
+    def test_fail_1d_2keys(self):
+        msg = 'More slices .* than dimensions'
+        with self.assertRaisesRegexp(IndexError, msg):
+            self.check([1, 2, 3], Index[1, 2], None, None)
+
+    def test_fail_empty_slice(self):
+        msg = 'Cannot index with zero length slice'
+        with self.assertRaisesRegexp(IndexError, msg):
+            self.check([1, 2, 3], Index[1:1], None, None)
+
+    def test_2d_tuple(self):
+        self.check([[11, 12], [21, 22], [31, 32]],
+                   Index[(2, 0, 1), ],
+                   [[31, 32], [11, 12], [21, 22]],
+                   {None: None, 0: 0, 1: 1})
+
+    def test_2d_two_tuples(self):
+        # Could be treated as fancy indexing, but must not be !
+        # Two separate 2-D indexing operations.
+        self.check([[11, 12, 13], [21, 22, 23], [31, 32, 33]],
+                   Index[(2, 0), (0, 1, 0, 1)],
+                   [[31, 32, 31, 32], [11, 12, 11, 12]],
+                   {None: None, 0: 0, 1: 1})
+
+    def test_2d_tuple_and_value(self):
+        # The two keys are applied in separate operations, and in the reverse
+        # order (?) :  The second op is then slicing a 1-D array, not 2-D.
+        self.check([[11, 12, 13, 14], [21, 22, 23, 24], [31, 32, 33, 34]],
+                   Index[(2, 0, 1), 3],
+                   [34, 14, 24],
+                   {None: None, 0: 0, 1: None})
+
+    def test_2d_single_int(self):
+        self.check([[11, 12, 13], [21, 22, 23], [31, 32, 33]],
+                   Index[1],
+                   [21, 22, 23],
+                   {None: None, 0: None, 1: 0})
+
+    def test_2d_int_slice(self):
+        self.check([[11, 12, 13], [21, 22, 23], [31, 32, 33]],
+                   Index[2, 1:3],
+                   [32, 33],
+                   {None: None, 0: None, 1: 0})
+
+    def test_3d_1int(self):
+        self.check([[[111, 112, 113], [121, 122, 123]],
+                    [[211, 212, 213], [221, 222, 223]],
+                    [[311, 312, 313], [321, 322, 323]]],
+                   Index[1],
+                   [[211, 212, 213], [221, 222, 223]],
+                   {None: None, 0: None, 1: 0, 2: 1})
+
+    def test_3d_2int(self):
+        self.check([[[111, 112, 113], [121, 122, 123], [131, 132, 133]],
+                    [[211, 212, 213], [221, 222, 223], [231, 232, 233]]],
+                   Index[1, 2],
+                   [231, 232, 233],
+                   {None: None, 0: None, 1: None, 2: 0})
+
+    def test_3d_tuple_and_value(self):
+        # The two keys are applied in separate operations, and in the reverse
+        # order (?) : The second op is slicing a 2-D array, not 3-D.
+        self.check([[[111, 112, 113, 114], [121, 122, 123, 124]],
+                    [[211, 212, 213, 214], [221, 222, 223, 224]],
+                    [[311, 312, 313, 314], [321, 322, 323, 324]]],
+                   Index[(2, 0, 1), 1],
+                   [[321, 322, 323, 324],
+                    [121, 122, 123, 124],
+                    [221, 222, 223, 224]],
+                   {None: None, 0: 0, 1: None, 2: 1})
+
+    def test_3d_ellipsis_last(self):
+        self.check([[[111, 112, 113], [121, 122, 123]],
+                    [[211, 212, 213], [221, 222, 223]],
+                    [[311, 312, 313], [321, 322, 323]]],
+                   Index[2, ...],
+                   [[311, 312, 313], [321, 322, 323]],
+                   {None: None, 0: None, 1: 0, 2: 1})
+
+    def test_3d_ellipsis_first_1int(self):
+        self.check([[[111, 112, 113, 114], [121, 122, 123, 124]],
+                    [[211, 212, 213, 214], [221, 222, 223, 224]],
+                    [[311, 312, 313, 314], [321, 322, 323, 324]]],
+                   Index[..., 2],
+                   [[113, 123],
+                    [213, 223],
+                    [313, 323]],
+                   {None: None, 0: 0, 1: 1, 2: None})
+
+    def test_3d_ellipsis_mid_1int(self):
+        self.check([[[111, 112, 113], [121, 122, 123]],
+                    [[211, 212, 213], [221, 222, 223]],
+                    [[311, 312, 313], [321, 322, 323]]],
+                   Index[..., 1, ...],
+                   [[121, 122, 123],
+                    [221, 222, 223],
+                    [321, 322, 323]],
+                   {None: None, 0: 0, 1: None, 2: 1})
+
+    def test_3d_ellipsis_first_2int(self):
+        self.check([[[111, 112, 113], [121, 122, 123]],
+                    [[211, 212, 213], [221, 222, 223]],
+                    [[311, 312, 313], [321, 322, 323]]],
+                   Index[..., 1, 2],
+                   [123, 223, 323],
+                   {None: None, 0: 0, 1: None, 2: None})
+
+    def test_3d_multiple_tuples(self):
+        # Where there are TWO or more tuple keys, this could be misinterpreted
+        # as 'fancy' indexing :  It should resolve into multiple calls.
+        self.check([[[111, 112, 113, 114], [121, 122, 123, 124]],
+                    [[211, 212, 213, 214], [221, 222, 223, 224]],
+                    [[311, 312, 313, 314], [321, 322, 323, 324]]],
+                   Index[(1, 2, 1), :, (2, 2, 3)],
+                   [[[213, 213, 214], [223, 223, 224]],
+                    [[313, 313, 314], [323, 323, 324]],
+                    [[213, 213, 214], [223, 223, 224]]],
+                   {None: None, 0: 0, 1: 1, 2: 2})
+        # NOTE: there seem to be an extra initial [:, :, :].
+        # That's just what it does at present.
 
 
 if __name__ == '__main__':
