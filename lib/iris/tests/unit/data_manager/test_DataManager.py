b8aa8852eb640519311a57cfdb9684c14eeb3588
Bill Little <bill.little@metoffice.gov.uk>
2017-05-15 10:09:44 +0100

    Dask data manager propagate (#2548)
    * Propogate masked fill-value to dm.
    
    * Fix existing CML.
    
    * __init__ fill-value 'none' and None
    
    * Fix broken DataManager tests.
    
    * Add propogate test coverage + minor refactor
    
    * Fix typo
    
    * Review actions.

--- lib/iris/tests/unit/data_manager/test_DataManager.py
+++ lib/iris/tests/unit/data_manager/test_DataManager.py
@@ -471,6 +471,39 @@ class Test__deepcopy(tests.IrisTest):
             dm._deepcopy(self.memo, fill_value=1e+20)
 
 
+class Test__propagate_masked_data_fill_value(tests.IrisTest):
+    def setUp(self):
+        self.real_array = np.array(0)
+        self.mask_array = ma.array(0)
+        np_fill_value = ma.array(0, dtype=self.mask_array.dtype).fill_value
+        assert np_fill_value == self.mask_array.fill_value
+        self.np_fill_value = np_fill_value
+        self.dm = DataManager(self.mask_array)
+        self.dm.fill_value = None
+
+    def test_nop__with_real_array(self):
+        fill_value = 1234
+        dm = DataManager(self.real_array)
+        dm.fill_value = 1234
+        self.assertEqual(dm.fill_value, fill_value)
+        dm._propagate_masked_data_fill_value()
+        self.assertEqual(dm.fill_value, fill_value)
+
+    def test_clear_fill_value__with_np_default(self):
+        fill_value = 1234
+        self.dm.fill_value = fill_value
+        self.assertEqual(self.dm.fill_value, fill_value)
+        self.dm._propagate_masked_data_fill_value()
+        self.assertIsNone(self.dm.fill_value)
+
+    def test_set_fill_value(self):
+        fill_value = 1234
+        self.assertIsNone(self.dm.fill_value)
+        self.dm._real_array.fill_value = fill_value
+        self.dm._propagate_masked_data_fill_value()
+        self.assertEqual(self.dm.fill_value, fill_value)
+
+
 class Test__realised_dtype_setter(tests.IrisTest):
     def setUp(self):
         self.real_array = np.array(0.0)
@@ -566,8 +599,16 @@ class Test_data__getter(tests.IrisTest):
         self.assertArrayEqual(result, self.real_array)
 
     def test_with_real_mask_array__default_fill_value(self):
-        self.mask_array.fill_value = 1234
+        fill_value = 1234
+        self.mask_array.fill_value = fill_value
         dm = DataManager(self.mask_array)
+        self.assertEqual(dm.fill_value, fill_value)
+        self.assertEqual(dm.data.fill_value, fill_value)
+
+    def test_with_real_mask_array__with_fill_value_None(self):
+        fill_value = 1234
+        self.mask_array.fill_value = fill_value
+        dm = DataManager(self.mask_array, fill_value=None)
         self.assertIsNone(dm.fill_value)
         np_fill_value = ma.array(0, dtype=dm.dtype).fill_value
         self.assertEqual(dm.data.fill_value, np_fill_value)
@@ -580,6 +621,14 @@ class Test_data__getter(tests.IrisTest):
 
     def test_with_lazy_mask_array__masked_default_fill_value(self):
         dm = DataManager(self.lazy_mask_array_masked,
+                         realised_dtype=self.realised_dtype)
+        self.assertIsNone(dm.fill_value)
+        np_fill_value = ma.array(0, dtype=dm.dtype).fill_value
+        self.assertEqual(dm.data.fill_value, np_fill_value)
+
+    def test_with_lazy_mask_array__masked_fill_value_None(self):
+        dm = DataManager(self.lazy_mask_array_masked,
+                         fill_value=None,
                          realised_dtype=self.realised_dtype)
         self.assertIsNone(dm.fill_value)
         np_fill_value = ma.array(0, dtype=dm.dtype).fill_value
@@ -839,7 +888,7 @@ class Test_data__setter(tests.IrisTest):
         self.assertIsNone(dm.fill_value)
         data = ma.array(1)
         dm.data = data
-        self.assertEqual(dm.fill_value, data.fill_value)
+        self.assertIsNone(dm.fill_value)
 
     def test_mask_array__with_fill_value(self):
         dm = DataManager(np.array(0))
