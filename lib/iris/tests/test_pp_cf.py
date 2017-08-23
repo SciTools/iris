8f9b64c94db868d828bed62b036880742d520c68
Daniel Kirkham <daniel.kirkham@metoffice.gov.uk>
2017-08-23 17:58:39 +0100

    Update cml for iris.tests.test_pp_cf.TestAll.test_integer_b_pp

--- lib/iris/tests/test_pp_cf.py
+++ lib/iris/tests/test_pp_cf.py
@@ -80,7 +80,6 @@ def callback_aaxzc_n10r13xy_b_pp(cube, field, filename):
     cube.add_aux_coord(height_coord)
 
 
-@tests.skip_dask_mask
 @tests.skip_data
 class TestAll(tests.IrisTest, pp.PPTest):
     _ref_dir = ('usecases', 'pp_to_cf_conversion')
