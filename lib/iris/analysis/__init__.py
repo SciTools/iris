49a930e10fcaa6815de09bcc8b7ac54966b7324f
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2017-10-12 15:33:24 +0100

    Dask masked-data stats (#2743)
    * Fix lazy stats; enhance mdtol test.
    
    * Tidy lazy mdtol code; use dask.core.elemwise.
    
    * pep8 fixes.
    
    * Improved use of dask-mask code.
    
    * Simplified with latest dask.
    
    * Add lazy-stats tests for all 'axis' options.
    
    * Efficient points count in _build_dask_mdtol_function.
    
    * Simplify points-count calculation.
    
    * Simpler lazy stats calculations for mdtol>=1.0
    
    * Remove+fix final skip_dask_mask in analysis tests.

--- lib/iris/analysis/__init__.py
+++ lib/iris/analysis/__init__.py
@@ -1419,36 +1419,41 @@ This aggregator handles masked data.
 """
 
 
-def _build_dask_mdtol_function(dask_nanfunction):
+def _build_dask_mdtol_function(dask_stats_function):
     """
     Make a wrapped dask statistic function that supports the 'mdtol' keyword.
 
-    'dask_nanfunction' must be a statistical function that treats NaNs as
-    missing data, and which has the basic call signature
-    "dask_nanfunction(data, axis, **kwargs)".
+    'dask_function' must be a dask statistical function, compatible with the
+    call signature : "dask_stats_function(data, axis, **kwargs)".
+    It must be masked-data tolerant, i.e. it ignores masked input points and
+    performs a calculation on only the unmasked points.
+    For example, mean([1, --, 2]) = (1 + 2) / 2 = 1.5.
 
     The returned value is a new function operating on dask arrays.
-    It has the call signature "stat(data, axis=-1, mdtol=None, *kwargs)".
+    It has the call signature `stat(data, axis=-1, mdtol=None, **kwargs)`.
 
     """
-    @wraps(dask_nanfunction)
+    @wraps(dask_stats_function)
     def inner_stat(array, axis=-1, mdtol=None, **kwargs):
-        dask_result = dask_nanfunction(array, axis=axis, **kwargs)
-        if mdtol is None:
+        # Call the statistic to get the basic result (missing-data tolerant).
+        dask_result = dask_stats_function(array, axis=axis, **kwargs)
+        if mdtol is None or mdtol >= 1.0:
             result = dask_result
         else:
-            point_counts = np.sum(np.ones(array.shape), axis=axis)
-            point_mask_counts = da.sum(da.isnan(array), axis=axis)
-            masked_point_fractions = (point_mask_counts + 0.0) / point_counts
-            # Note: the +0.0 forces a floating-point divide.
+            # Build a lazy computation to compare the fraction of missing
+            # input points at each output point to the 'mdtol' threshold.
+            point_mask_counts = da.sum(da.ma.getmaskarray(array), axis=axis)
+            points_per_calc = array.size / dask_result.size
+            masked_point_fractions = point_mask_counts / points_per_calc
             boolean_mask = masked_point_fractions > mdtol
-            result = da.where(boolean_mask, np.nan, dask_result)
+            # Return an mdtol-masked version of the basic result.
+            result = da.ma.masked_array(da.ma.getdata(dask_result),
+                                        boolean_mask)
         return result
     return inner_stat
 
-
 MEAN = WeightedAggregator('mean', ma.average,
-                          lazy_func=_build_dask_mdtol_function(da.nanmean))
+                          lazy_func=_build_dask_mdtol_function(da.mean))
 """
 An :class:`~iris.analysis.Aggregator` instance that calculates
 the mean over a :class:`~iris.cube.Cube`, as computed by
@@ -1647,7 +1652,7 @@ This aggregator handles masked data.
 
 
 STD_DEV = Aggregator('standard_deviation', ma.std, ddof=1,
-                     lazy_func=_build_dask_mdtol_function(da.nanstd))
+                     lazy_func=_build_dask_mdtol_function(da.std))
 """
 An :class:`~iris.analysis.Aggregator` instance that calculates
 the standard deviation over a :class:`~iris.cube.Cube`, as
@@ -1714,7 +1719,7 @@ This aggregator handles masked data.
 VARIANCE = Aggregator('variance',
                       ma.var,
                       units_func=lambda units: units * units,
-                      lazy_func=_build_dask_mdtol_function(da.nanvar),
+                      lazy_func=_build_dask_mdtol_function(da.var),
                       ddof=1)
 """
 An :class:`~iris.analysis.Aggregator` instance that calculates
