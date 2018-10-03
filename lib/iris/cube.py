a41db2d786e1b0fc4dcfc5d1648f14ad934a86ee
hdyson <hdyson@gmail.com>
2018-10-03 15:59:15 +0100

    Improved Aux factory error handling: better message, delivered earlier (#3182)
    * Tests for expected behaviour
    
    * Raise error if aux factory relies on a coordinate external to cube
    
    * Include coordinate name in error message
    
    * Don't check whether dependencies that are "None" are in the coords.
    
    * Review changes: check cube name in error and variable rename
    
    * Truncated variable for flake8

--- lib/iris/cube.py
+++ lib/iris/cube.py
@@ -994,6 +994,12 @@ bound=(1994-12-01 00:00:00, 1998-12-01 00:00:00)
         if not isinstance(aux_factory, iris.aux_factory.AuxCoordFactory):
             raise TypeError('Factory must be a subclass of '
                             'iris.aux_factory.AuxCoordFactory.')
+        cube_coords = self.coords()
+        for dependency in aux_factory.dependencies:
+            ref_coord = aux_factory.dependencies[dependency]
+            if ref_coord is not None and ref_coord not in cube_coords:
+                msg = "{} coordinate for factory is not present on cube {}"
+                raise ValueError(msg.format(ref_coord.name(), self.name()))
         self._aux_factories.append(aux_factory)
 
     def add_cell_measure(self, cell_measure, data_dims=None):
