6ee8aee542eef29238ae481abad04494dd0aed7d
Corinne Bosley <corinne.e.bosley@gmail.com>
2018-10-08 12:37:24 +0100

    tests updated

--- lib/iris/tests/unit/util/test_mask_discontiguities.py
+++ lib/iris/tests/unit/util/test_mask_discontiguities.py
@@ -26,7 +26,7 @@ import iris.tests as tests
 import numpy as np
 import numpy.ma as ma
 
-from iris.util import mask_data_at_discontiguities
+from iris.util import mask_discontiguities
 from iris.tests.stock import simple_3d
 from iris.tests.stock import sample_2d_latlons
 from iris.tests.stock import make_bounds_discontiguous_at_point
@@ -45,48 +45,20 @@ class Test(tests.IrisTest):
         self.cube_2d = full2d_global()
         make_bounds_discontiguous_at_point(self.cube_2d, 3, 3)
 
-        # Construct a 3d cube with a different masked discontiguity to test
-        # masking of multidimensional cubes
-        self.cube_3d = simple_3d()
-        x_coord = self.cube_3d.coord('longitude')
-        x_coord.guess_bounds()
-        y_coord = self.cube_3d.coord('latitude')
-        y_coord.guess_bounds()
-        x_coord_2d, y_coord_2d = grid_coords_2d_from_1d(x_coord, y_coord)
-        # Remove the old grid coords.
-        for coord in (x_coord, y_coord):
-            self.cube_3d.remove_coord(coord)
-        # Add the new grid coords.
-        for coord in (x_coord_2d, y_coord_2d):
-            self.cube_3d.add_aux_coord(coord, (1, 2))
-        # Add a discontiguity and mask the data point
-        make_bounds_discontiguous_at_point(self.cube_3d, 0, 0)
-
     def test_mask_discontiguities_2d(self):
         # This tests the masking of a 2d data array
         cube = self.cube_2d
-        coord = cube.coord('longitude')
+        discontiguity_array = ma.getmaskarray(cube.data).copy()
         expected = cube.copy()
 
         # Remove mask so that we can pass an unmasked data set to
         # mask_discontiguities, and check that it masks the correct point by
         # comparing with masked data
         cube.data.mask = ma.nomask
-        returned = mask_data_at_discontiguities(cube, coord)
+        returned = mask_discontiguities(cube, discontiguity_array)
         self.assertTrue(np.all(expected.data.mask == returned.data.mask))
 
-    def test_mask_discontiguities_3d(self):
-        # This tests the broadcasting of the mask array from 2d to 3d
-        cube = self.cube_3d
-        coord = cube.coord('longitude')
-        expected = cube.copy()
-
-        # Remove mask so that we can pass an unmasked data set to
-        # mask_discontiguities, and check that it masks the correct point by
-        # comparing with masked data
-        cube.data.mask = ma.nomask
-        returned = mask_data_at_discontiguities(cube, coord)
-        self.assertTrue(np.all(expected.data.mask == returned.data.mask))
+    # TODO: Add more tests here (e.g. y-direction discontiguity masking)
 
 
 if __name__ == '__main__':
