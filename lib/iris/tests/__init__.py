c421e71d73747c7d8cdd4c6aa8fb81fb11720bba
David Brown <david.brown@metoffice.gov.uk>
2019-02-25 15:30:51 +0000

    Added a 1 second 'timeout' to requests.get call

--- lib/iris/tests/__init__.py
+++ lib/iris/tests/__init__.py
@@ -115,7 +115,8 @@ except ImportError:
     NC_TIME_AXIS_AVAILABLE = False
 
 try:
-    requests.get('https://github.com/SciTools/iris')
+    # Added 1 second timeout to stop call hanging on the HPC
+    requests.get('https://github.com/SciTools/iris', timeout=1.0)
     INET_AVAILABLE = True
 except requests.exceptions.ConnectionError:
     INET_AVAILABLE = False
