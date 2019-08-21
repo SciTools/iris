936a7e74b838e39023cd79534fd3902ef158f062
stephen.worsley <stephen.worsley@metoffice.gov.uk>
2019-08-21 16:15:45 +0100

    Added a test

--- lib/iris/tests/integration/plot/test_vector_plots.py
+++ lib/iris/tests/integration/plot/test_vector_plots.py
@@ -167,6 +167,26 @@ class MixinVectorPlotCases(object):
             self.plot('2d_rotated', u_cube, v_cube,
                       coords=('longitude', 'latitude'))
 
+    def test_circular_longitude(self):
+        # Test circular longitude does not cause a crash.
+        res = 5
+        lat = DimCoord(np.arange(-90, 91, res), 'latitude',
+                                   units='degrees_north')
+        lon = DimCoord(np.arange(0, 360, res), 'longitude',
+                                   units='degrees_east')
+        nlat = len(lat.points)
+        nlon = len(lon.points)
+        u_arr = np.ones((nlat, nlon))
+        v_arr = np.ones((nlat, nlon))
+        u_cube = Cube(u_arr, dim_coords_and_dims=[(lat, 0), (lon, 1)],
+                           standard_name='eastward_wind')
+        v_cube = Cube(v_arr, dim_coords_and_dims=[(lat, 0), (lon, 1)],
+                           standard_name='northward_wind')
+        u_cube.coord('longitude').circular = True
+
+        self.plot('circular', u_cube, v_cube,
+                  coords=('longitude', 'latitude'))
+
 
 class TestQuiver(MixinVectorPlotCases, tests.GraphicsTest):
     def setUp(self):
