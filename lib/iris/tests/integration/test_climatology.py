2d7235ae28f086518faae34cc7eed72479b360ca
Martin Yeo <martin.yeo@metoffice.gov.uk>
2019-08-30 16:50:39 +0100

    Refactored to use a temporary directory

--- lib/iris/tests/integration/test_climatology.py
+++ lib/iris/tests/integration/test_climatology.py
@@ -24,6 +24,10 @@ from six.moves import (filter, input, map, range, zip)  # noqa
 import iris.tests as tests
 
 from datetime import datetime
+from os.path import join as path_join
+import shutil
+from subprocess import check_call
+import tempfile
 
 import netCDF4 as nc
 import numpy as np
@@ -34,13 +38,15 @@ from iris.cube import Cube
 
 
 class TestClimatology(iris.tests.IrisTest):
+    reference_cdl_path = (
+        '../results/integration/climatology/TestClimatology/'
+        'reference_simpledata.cdl')
+
     @staticmethod
     def _simple_data_cube():
-
         def jan_offset(day, year):
             dt = (datetime(year, 1, day) - datetime(1970, 1, 1))
             return dt.total_seconds() / (24. * 3600)
-
 
         days = range(10, 15)
         years = [[year, year + 10] for year in [2001] * 4]
@@ -72,10 +78,21 @@ class TestClimatology(iris.tests.IrisTest):
 
         return cube
 
+    @classmethod
+    def _simple_cdl_string(cls):
+        with open(cls.reference_cdl_path, 'r') as f:
+            cdl_content = f.read()
+        # Add the expected CDL first line since this is removed from the
+        # stored results file.
+        cdl_content = 'netcdf {\n' + cdl_content
+
+        return cdl_content
+
     @staticmethod
-    def _bounds_to_climatology(filepath):
+    def _bounds_to_climatology(file_path):
         # Hack file until Iris does it right ..
-        ds = nc.Dataset(filepath, 'r+')
+        # TODO: remove this and any references to it.
+        ds = nc.Dataset(file_path, 'r+')
         ds.variables['time'].renameAttribute('bounds', 'climatology')
         old_name = 'time_bnds'
         new_name = 'time_climatology'
@@ -84,25 +101,74 @@ class TestClimatology(iris.tests.IrisTest):
         ds.renameVariable(old_name, new_name)
         ds.close()
 
+    @staticmethod
+    def _load_sanitised_cube(filepath):
+        cube = iris.load_cube(filepath)
+        # Remove attributes convention, if any.
+        cube.attributes.pop('Conventions', None)
+        # Remove any var-names.
+        for coord in cube.coords():
+            coord.var_name = None
+        cube.var_name = None
+        return cube
+
+    @classmethod
+    def setUpClass(cls):
+        # Create a temp directory for temp files.
+        cls.temp_dir = tempfile.mkdtemp()
+        cls.path_ref_cdl = path_join(cls.temp_dir, 'standard.cdl')
+        cls.path_ref_nc = path_join(cls.temp_dir, 'standard.nc')
+        # Create reference CDL file.
+        with open(cls.path_ref_cdl, 'w') as f_out:
+            f_out.write(cls._simple_cdl_string())
+        # Create reference netCDF file from reference CDL.
+        command = 'ncgen -o {} {}'.format(
+            cls.path_ref_nc, cls.path_ref_cdl)
+        check_call(command, shell=True)
+        cls.path_temp_nc = path_join(cls.temp_dir, 'tmp.nc')
+
+        # Create reference cube.
+        cls.cube_ref = cls._simple_data_cube()
+
+    @classmethod
+    def tearDownClass(cls):
+        # Destroy a temp directory for temp files.
+        shutil.rmtree(cls.temp_dir)
+
+###############################################################################
+
     def test_save_simpledata(self):
-        cube = self._simple_data_cube()
-        # Write Cube to netCDF file.
-        with self.temp_filename(suffix='.nc') as filepath_out:
-            iris.save(cube, filepath_out)
-            self._bounds_to_climatology(filepath_out)
-            # Check CDL of saved result.
-            self.assertCDL(filepath_out, flags='')
+        # Create file from cube, test against reference CDL.
+        cube = self.cube_ref
+        iris.save(cube, self.path_temp_nc)
+        self._bounds_to_climatology(self.path_temp_nc)
+        self.assertCDL(
+            self.path_temp_nc,
+            reference_filename=self.reference_cdl_path,
+            flags='')
+
+    def test_load_simpledata(self):
+        # Create cube from file, test against reference cube.
+        cube = self._load_sanitised_cube(self.path_ref_nc)
+        self.assertEqual(cube, self.cube_ref)
 
     def test_cube_to_cube(self):
-        cube_out = self._simple_data_cube()
-        bounds_out = cube_out.coord('time').bounds
-        # Write Cube to netCDF file.
-        with self.temp_filename(suffix='.nc') as filepath_out:
-            iris.save(cube_out, filepath_out)
-            self._bounds_to_climatology(filepath_out)
-            cube_in = iris.load_cube(filepath_out)
-            bounds_in = cube_in.coord('time').bounds
-            self.assertArrayEqual(bounds_in, bounds_out)
+        # Save reference cube to file, load cube from same file, test against
+        # reference cube.
+        iris.save(self.cube_ref, self.path_temp_nc)
+        cube = self._load_sanitised_cube(self.path_temp_nc)
+        self.assertEqual(cube, self.cube_ref)
+
+    def test_file_to_file(self):
+        # Load cube from reference file, save same cube to file, test against
+        # reference CDL.
+        cube = iris.load_cube(self.path_ref_nc)
+        iris.save(cube, self.path_temp_nc)
+        self._bounds_to_climatology(self.path_temp_nc)
+        self.assertCDL(
+            self.path_temp_nc,
+            reference_filename=self.reference_cdl_path,
+            flags='')
 
 
 if __name__ == "__main__":
