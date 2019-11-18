3edb2acd9bfebf1b712706cb30dbbf0a0197698b
Bill Little <bill.james.little@gmail.com>
2019-11-18 11:56:43 +0000

    Make _DimensionalMetadata abstract (#3531)

---
+++ lib/iris/tests/unit/coords/test__DimensionalMetadata.py
@@ -0,0 +1,26 @@
+# Copyright Iris contributors
+#
+# This file is part of Iris and is released under the LGPL license.
+# See COPYING and COPYING.LESSER in the root of the repository for full
+# licensing details.
+"""Unit tests for the :class:`iris.coords._DimensionalMetadata` class."""
+
+# Import iris.tests first so that some things can be initialised before
+# importing anything else.
+import iris.tests as tests
+
+from iris.coords import _DimensionalMetadata
+
+
+class Test___init____abstractmethod(tests.IrisTest):
+    def test(self):
+        emsg = (
+            "Can't instantiate abstract class _DimensionalMetadata with "
+            "abstract methods __init__"
+        )
+        with self.assertRaisesRegex(TypeError, emsg):
+            _ = _DimensionalMetadata(0)
+
+
+if __name__ == "__main__":
+    tests.main()
