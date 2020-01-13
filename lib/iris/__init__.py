b1f9b8457f59caeb1250c9f017e6e17d28c079ee
Martin Yeo <martin.yeo@metoffice.gov.uk>
2020-01-13 13:09:02 +0000

    Bump version to 2.4.0rc0.

--- lib/iris/__init__.py
+++ lib/iris/__init__.py
@@ -121,7 +121,7 @@ except ImportError:
 
 
 # Iris revision.
-__version__ = '2.3.0'
+__version__ = '2.4.0rc0'
 
 # Restrict the names imported when using "from iris import *"
 __all__ = ['load', 'load_cube', 'load_cubes', 'load_raw',
