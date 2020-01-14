48b7f5f4ae1735d3523a42877a80d815f5e2b6eb
Martin Yeo <martin.yeo@metoffice.gov.uk>
2020-01-14 10:29:15 +0000

    Bracketed six.moves and __future__ imports.

--- lib/iris/tests/unit/constraints/test_NameConstraint.py
+++ lib/iris/tests/unit/constraints/test_NameConstraint.py
@@ -5,8 +5,8 @@
 # licensing details.
 """Unit tests for the `iris._constraints.NameConstraint` class."""
 
-from __future__ import absolute_import, division, print_function
-from six.moves import filter, input, map, range, zip  # noqa
+from __future__ import (absolute_import, division, print_function)
+from six.moves import (filter, input, map, range, zip)  # noqa
 
 # Import iris.tests first so that some things can be initialised before
 # importing anything else.
