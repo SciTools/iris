186cc6ff8c3975592ed3dc75a642aac814c81ba5
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2020-02-24 14:15:14 +0000

    Removed grib-specific test to iris-grib.

--- lib/iris/tests/integration/test_pickle.py
+++ lib/iris/tests/integration/test_pickle.py
@@ -10,12 +10,8 @@
 import iris.tests as tests
 
 import pickle
-import unittest
 
 import iris
-
-if tests.GRIB_AVAILABLE:
-    from iris_grib.message import GribMessage
 
 
 class Common:
@@ -37,44 +33,6 @@ class Common:
 
     def test_protocol_2(self):
         self.pickle_cube(2)
-
-
-@tests.skip_data
-@tests.skip_grib
-class TestGribMessage(Common, tests.IrisTest):
-    def setUp(self):
-        self.path = tests.get_data_path(("GRIB", "fp_units", "hours.grib2"))
-
-    def pickle_obj(self, obj):
-        with self.temp_filename(".pkl") as filename:
-            with open(filename, "wb") as f:
-                pickle.dump(obj, f)
-
-    # These probably "ought" to work, but currently fail.
-    # see https://github.com/SciTools/iris/pull/2608
-    @unittest.expectedFailure
-    def test_protocol_0(self):
-        super().test_protocol_0()
-
-    @unittest.expectedFailure
-    def test_protocol_1(self):
-        super().test_protocol_1()
-
-    @unittest.expectedFailure
-    def test_protocol_2(self):
-        super().test_protocol_2()
-
-    def test(self):
-        # Check that a GribMessage pickles without errors.
-        messages = GribMessage.messages_from_filename(self.path)
-        obj = next(messages)
-        self.pickle_obj(obj)
-
-    def test_data(self):
-        # Check that GribMessage.data pickles without errors.
-        messages = GribMessage.messages_from_filename(self.path)
-        obj = next(messages).data
-        self.pickle_obj(obj)
 
 
 @tests.skip_data
