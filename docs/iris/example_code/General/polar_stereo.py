8e1c5b163a1a1f01dd6caad178e19348cd252b41
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2020-06-18 12:09:18 +0100

    Replace grib file with netcdf in polar_stereo example.

--- docs/iris/example_code/General/polar_stereo.py
+++ docs/iris/example_code/General/polar_stereo.py
@@ -15,7 +15,7 @@ import iris.quickplot as qplt
 
 
 def main():
-    file_path = iris.sample_data_path("polar_stereo.grib2")
+    file_path = iris.sample_data_path("toa_brightness_stereographic.nc")
     cube = iris.load_cube(file_path)
     qplt.contourf(cube)
     ax = plt.gca()
