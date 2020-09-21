d04d6545b700bd4b45a7cd7df5c324ca4b4227b2
Tremain Knight <2108488+tkknight@users.noreply.github.com>
2020-09-21 15:12:13 +0100

    Updated napolean config

--- docs/iris/src/conf.py
+++ docs/iris/src/conf.py
@@ -132,13 +132,15 @@ napoleon_google_docstring = True
 napoleon_numpy_docstring = True
 napoleon_include_init_with_doc = False
 napoleon_include_private_with_doc = False
-napoleon_include_special_with_doc = True
+napoleon_include_special_with_doc = True  # includes dunders in api doc
 napoleon_use_admonition_for_examples = False
 napoleon_use_admonition_for_notes = False
 napoleon_use_admonition_for_references = False
 napoleon_use_ivar = False
 napoleon_use_param = True
 napoleon_use_rtype = True
+napoleon_use_keyword = True
+napoleon_custom_sections = None
 
 # -- spellingextension --------------------------------------------------------
 # See https://sphinxcontrib-spelling.readthedocs.io/en/latest/customize.html
