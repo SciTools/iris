0c9688485d0488c64e35b4d93b0eae5828f8acbe
Bill Little <bill.james.little@gmail.com>
2021-01-21 08:25:05 +0000

    added docs URL linkcheck skip

--- docs/iris/src/conf.py
+++ docs/iris/src/conf.py
@@ -270,6 +270,7 @@ linkcheck_ignore = [
     "http://scitools.github.com/cartopy",
     "http://www.wmo.int/pages/prog/www/DPFS/documents/485_Vol_I_en_colour.pdf",
     "https://software.ac.uk/how-cite-software",
+    "http://www.esrl.noaa.gov/psd/data/gridded/conventions/cdc_netcdf_standard.shtml",
 ]
 
 # list of sources to exclude from the build.
