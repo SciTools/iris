106eb7c092421c39ffc5d96caf06701025cab5a3
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2021-01-25 15:37:41 +0000

    Slight tidy + more comments.

--- lib/iris/_representation/cube_printout.py
+++ lib/iris/_representation/cube_printout.py
@@ -60,11 +60,11 @@ class CubePrinter:
         # Construct a beautifultable to represent the cube-summary info.
         #
         # NOTE: although beautifultable is useful and provides a flexible output
-        # form, but its formatting features are not yet adequate to produce our
+        # form, its formatting features are not yet adequate to produce our
         # desired "standard cube summary" appearance :  For that, we still need
-        # column spanning.
+        # column spanning (at least).
         # So a 'normal' cube summary is produced by "CubePrinter.to_string()",
-        # which relies on information *not* stored in the table.
+        # which must also use information *not* stored in the table.
         extra_indent = " " * n_indent_extra
         sect_indent = " " * n_indent_section
         item_indent = sect_indent + " " * n_indent_item
@@ -81,16 +81,12 @@ class CubePrinter:
         n_dims = len(dim_names)
         assert len(cube_shape) == n_dims
 
-        # NOTE: this is of no interest, as it "decided" to make a list
-        # of "{dim-name}: {length}", but we don't actually use that.
-        # dimheader.contents
-
         tb = bt.BeautifulTable(max_width=self.max_width)
 
         # First setup the columns
-        #   - 3 initially for the column-1 content
-        #   - 2 (name, length) for each dimension
-        #   -
+        #   - x1 @0 column-1 content : main title; headings; elements-names
+        #   - x1 @1 "value" content (for scalar items)
+        #   - x2n @2 (name, length) for each of n dimensions
         column_texts = [nameunits_string, ""]
         for dim_name, length in zip(dim_names, cube_shape):
             column_texts.append(f"{dim_name}:")
@@ -117,9 +113,13 @@ class CubePrinter:
                         column_texts = [""] * len(column_texts)
                         column_texts[1] = extra_indent + extra_string
 
-        # Similar for scalar sections : different ones handle differently
+        # Record where the 'scalar' part starts.
+        self.i_first_scalar_row = len(tb.rows)
+
+        # Similar for scalar sections
         for sect in summ.scalar_sections.values():
             if sect.contents:
+                # Add a row for the "section title" text.
                 sect_name = sect.title
                 column_texts = [sect_indent + sect_name, ""]
                 column_texts += [""] * (2 * n_dims)
@@ -131,6 +131,8 @@ class CubePrinter:
                     column_texts += [""] * (2 * n_dims)
                     tb.rows.append(column_texts)
 
+                # Add a row for each item
+                # NOTE: different section types handle differently
                 if "scalar coordinate" in title:
                     for item in sect.contents:
                         add_scalar(item.name, item.content)
@@ -138,7 +140,7 @@ class CubePrinter:
                     for title, value in zip(sect.names, sect.values):
                         add_scalar(title, value)
                 elif "scalar cell measure" in title or "cell method" in title:
-                    # These ones are "just strings".
+                    # These are just strings: nothing in the 'value' column.
                     for name in sect.contents:
                         add_scalar(name, "")
                 else:
@@ -146,6 +148,71 @@ class CubePrinter:
                     raise ValueError(msg)
 
         return tb
+
+    def _set_table_style(self, tb):
+        # Fix all the column widths and alignments
+        tb.maxwidth = 9999  # don't curtail or wrap *anything* (initially)
+        tb.columns.alignment[0] = bt.ALIGN_LEFT
+        tb.columns.alignment[1] = bt.ALIGN_LEFT
+        for i_col in range(2, len(tb.columns) - 1, 2):
+            tb.columns.alignment[i_col] = bt.ALIGN_RIGHT
+            tb.columns.padding_left[i_col] = 2
+            tb.columns.padding_right[i_col] = 0
+            tb.columns.padding_left[i_col + 1] = 0
+        tb.set_style(bt.STYLE_NONE)
+
+    def _oneline_printout(self):
+        # Make a copy of the table, with no spacing and doctored columns.
+        tb = bt.BeautifulTable()  # use new table, as copy() is deprecated
+
+        # Add column headers, with extra text modifications.
+        column_headers = self.table.column_headers[:]
+        column_headers[0] = "<iris 'Cube' of " + column_headers[0]
+        column_headers[2] = "(" + column_headers[2]
+        column_headers[-1] = column_headers[-1] + ")>"
+        # Add semicolons as column spacers
+        for i_col in range(3, len(column_headers) - 1, 2):
+            column_headers[i_col] += ";"
+            # NOTE: it would be "nice" use `table.columns.separator` to do
+            # this, but bt doesn't currently support that :  Setting it
+            # affects the header-underscore/separator line instead.
+        tb.column_headers = column_headers
+        # Add a single row matching the header (or nothing will print)
+        # ( as used inside bt.BeatifulTable._get_string() ).
+        tb.rows.append(tb.columns.header)
+
+        # Set all the normal style options
+        self._set_table_style(tb)
+
+        # Adjust the column paddings for minimal spacing.
+        tb.columns.padding_left = 0
+        tb.columns.padding_right = 1
+
+        # Return only the top (header) line.
+        result = next(tb._get_string())
+        return result
+
+    def _full_multiline_summary(self, max_width):
+        # pre-render with no width limitation whatsoever.
+        tb = self.table
+        tb.maxwidth = 9999
+        str(tb)
+
+        # Force wraps in the 'value column' (== column #1)
+        widths = tb.columns.width[:]
+        widths[1] = 0
+        widths[1] = max_width - sum(widths)
+        tb.columns.width = widths
+        tb.columns.width_exceed_policy = bt.WEP_WRAP
+        # Also must re-establish the style.
+        # Hmmm, none of this is that obvious, is it ??
+        tb.set_style(bt.STYLE_NONE)
+
+        # Finally, use _get_string to reprint *without* recalulting widths.
+        summary_lines = list(tb._get_string(recalculate_width=False))
+        result = "\n".join(summary_lines)
+
+        return result
 
     def to_string(self, oneline=False, max_width=None):
         """
@@ -165,51 +232,12 @@ class CubePrinter:
         """
         if max_width is None:
             max_width = self.max_width
-        tb = self.table
-        tb.set_style(bt.STYLE_COMPACT)
-        # Fix all the column widths and alignments
-        tb.maxwidth = 9999  # don't curtail or wrap *anything*
-        tb.columns.alignment[0] = bt.ALIGN_LEFT
-        tb.columns.alignment[1] = bt.ALIGN_LEFT
-        for i_col in range(2, len(tb.columns) - 1, 2):
-            tb.columns.alignment[i_col] = bt.ALIGN_RIGHT
-            tb.columns.padding_left[i_col] = 2
-            tb.columns.padding_right[i_col] = 0
-            tb.columns.padding_left[i_col + 1] = 0
 
         if oneline:
-            # Render a copy of the table, with no spacing and doctored columns.
-            tb2 = bt.BeautifulTable()
-            column_headers = tb.column_headers[:]
-            column_headers[0] = "<iris 'Cube' of " + column_headers[0]
-            column_headers[2] = "(" + column_headers[2]
-            for i_col in range(3, len(column_headers) - 1, 2):
-                column_headers[i_col] += ";"
-            column_headers[-1] = column_headers[-1] + ")>"
-            tb2.column_headers = column_headers
-            tb2.rows.append(tb2.columns.header)
-            # tb2.set_style(bt.STYLE_COMPACT)
-            tb2.set_style(bt.STYLE_NONE)
-            tb2.maxwidth = 9999
-            tb2.columns.alignment = tb.columns.alignment
-            tb2.columns.padding_left = 0
-            tb2.columns.padding_right = 0
-            result = next(tb2._get_string())
+            result = self._oneline_printout()
         else:
-            # pre-render with no width limitation
-            tb.maxwidth = 9999
-            str(tb)
-            # Force wraps in the 'value column' (== column #1)
-            widths = tb.columns.width[:]
-            widths[1] = 0
-            widths[1] = max_width - sum(widths)
-            tb.columns.width = widths
-            tb.columns.width_exceed_policy = bt.WEP_WRAP
-            # Also must re-establish the style.
-            # Hmmm, none of this is that obvious, is it ??
-            tb.set_style(bt.STYLE_NONE)
-            summary_lines = list(tb._get_string(recalculate_width=False))
-            result = "\n".join(summary_lines)
+            result = self._full_multiline_summary(max_width)
+
         return result
 
     def __str__(self):
