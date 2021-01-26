43ee0cd9f849d6104c824825753c5fe4e2f3b0b8
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2021-01-26 10:24:55 +0000

    Expand test cube to test all features (except scalar cube).

--- lib/iris/tests/unit/representation/test_cube_printout.py
+++ lib/iris/tests/unit/representation/test_cube_printout.py
@@ -9,45 +9,39 @@ import iris.tests as tests
 
 import numpy as np
 import iris
-from iris.cube import Cube
 from iris.coords import AuxCoord, DimCoord
 from iris._representation import cube_summary as icr
 import iris.tests.stock as istk
-
+from iris.util import new_axis
 
 from iris._representation.cube_printout import CubePrinter
 
 
 def test_cube(n_extra_dims=0):
-    cube = istk.realistic_3d()
+    # cube = istk.realistic_3d()
+    cube = istk.realistic_4d()  # this one has a derived coord
 
-    # Add multiple extra dimensions to test the width controls
+    # Optionally : add multiple extra dimensions to test the width controls
     if n_extra_dims > 0:
-        new_dims = cube.shape + (1,) * n_extra_dims
-        new_data = cube.data.reshape(new_dims)
-        new_cube = Cube(new_data)
-        for i_dim in range(new_cube.ndim):
-            if i_dim < cube.ndim:
-                dimco = cube.coord(dimensions=i_dim, dim_coords=True)
-            else:
-                dim_name = "long_name_dim_{}".format(i_dim)
-                dimco = DimCoord([0], long_name=dim_name)
-            new_cube.add_dim_coord(dimco, i_dim)
 
-        # Copy all aux coords too
-        for co, dims in cube._aux_coords_and_dims:
-            new_cube.add_aux_coord(co, dims)
+        new_cube = cube.copy()
+        # Add n extra scalar *1 coords
+        for i_dim in range(n_extra_dims):
+            dim_name = "long_name_dim_{}".format(i_dim + cube.ndim)
+            dimco = DimCoord([0], long_name=dim_name)
+            new_cube.add_aux_coord(dimco)
+            # Promote to dim coord
+            new_cube = new_axis(new_cube, dim_name)
 
-        # Copy attributes
-        new_cube.attributes = cube.attributes
+        # Put them all at the back
+        dim_order = list(range(new_cube.ndim))
+        dim_order = dim_order[n_extra_dims:] + dim_order[:n_extra_dims]
+        new_cube.transpose(dim_order)  # dontcha hate this inplace way ??
 
-        # Nothing else to copy ?
-        assert not cube.ancillary_variables()
-        assert not cube.cell_measures()  # Includes scalar ones
-        assert not cube.aux_factories
-
+        # Replace the original test cube
         cube = new_cube
 
+    # Add extra things to test all aspects
     rotlats_1d, rotlons_1d = (
         cube.coord("grid_latitude").points,
         cube.coord("grid_longitude").points,
@@ -61,23 +55,88 @@ def test_cube(n_extra_dims=0):
         cs.grid_north_pole_longitude,
         cs.grid_north_pole_latitude,
     )
+    co_lat, co_lon = cube.coord(axis="y"), cube.coord(axis="x")
+    latlon_dims = cube.coord_dims(co_lat) + cube.coord_dims(co_lon)
     cube.add_aux_coord(
-        AuxCoord(trulons, standard_name="longitude", units="degrees"), (1, 2)
+        AuxCoord(trulons, standard_name="longitude", units="degrees"),
+        latlon_dims,
     )
     cube.add_aux_coord(
-        AuxCoord(trulats, standard_name="latitude", units="degrees"), (1, 2)
+        AuxCoord(trulats, standard_name="latitude", units="degrees"),
+        latlon_dims,
     )
 
     cube.attributes[
         "history"
     ] = "Exceedingly and annoying long message with many sentences.  And more and more.  And more and more."
 
+    cube.add_cell_method(iris.coords.CellMethod("mean", ["time"]))
+    cube.add_cell_method(
+        iris.coords.CellMethod(
+            "max", ["latitude"], intervals="3 hour", comments="remark"
+        )
+    )
+    latlons_shape = [cube.shape[i_dim] for i_dim in latlon_dims]
+    cube.add_cell_measure(
+        iris.coords.CellMeasure(
+            np.zeros(latlons_shape), long_name="cell-timings", units="s"
+        ),
+        latlon_dims,
+    )
+    cube.add_cell_measure(
+        iris.coords.CellMeasure(
+            [4.3], long_name="whole_cell_factor", units="m^2"
+        ),
+        (),
+    )  # a SCALAR cell-measure
+
+    time_dim = cube.coord_dims(cube.coord(axis="t"))
+    cube.add_ancillary_variable(
+        iris.coords.AncillaryVariable(
+            np.zeros(cube.shape[0]), long_name="time_scalings", units="ppm"
+        ),
+        time_dim,
+    )
+    cube.add_ancillary_variable(
+        iris.coords.AncillaryVariable(
+            [3.2], long_name="whole_cube_area_factor", units="m^2"
+        ),
+        (),
+    )  # a SCALAR ancillary
+
+    # Add some duplicate-named coords (not possible for dim-coords)
+    vector_duplicate_name = "level_height"
+    co_orig = cube.coord(vector_duplicate_name)
+    dim_orig = cube.coord_dims(co_orig)
+    co_new = co_orig.copy()
+    co_new.attributes.update(dict(extra_distinguishing="this", a=1, b=2))
+    cube.add_aux_coord(co_new, dim_orig)
+
+    vector_different_name = "sigma"
+    co_orig = cube.coord(vector_different_name)
+    co_orig.attributes["setting"] = "a"
+    dim_orig = cube.coord_dims(co_orig)
+    co_new = co_orig.copy()
+    co_new.attributes["setting"] = "B"
+    cube.add_aux_coord(co_new, dim_orig)
+
+    # Also need to test this with a SCALAR coord
+    scalar_duplicate_name = "forecast_period"
+    co_orig = cube.coord(scalar_duplicate_name)
+    co_new = co_orig.copy()
+    co_new.points = co_new.points + 2.3
+    co_new.attributes["different"] = "True"
+    cube.add_aux_coord(co_new)
+
     return cube
 
 
 class TestCubePrintout(tests.IrisTest):
     def test_basic(self):
-        cube = test_cube(n_extra_dims=4)
+        cube = test_cube(
+            n_extra_dims=4
+        )  # NB does not yet work with factories.
+        # cube = test_cube()
         summ = icr.CubeSummary(cube)
         printer = CubePrinter(summ, max_width=110)
         print("EXISTING full :")
@@ -92,3 +151,4 @@ class TestCubePrintout(tests.IrisTest):
         print("")
         print("original table form:")
         print(printer.table)
+        print("")
