6327609b975b910939f59430f7f1135e5aa047aa
Martin Yeo <martin.yeo@metoffice.gov.uk>
2021-03-17 12:53:28 +0000

    More basic UGRID loading integration tests.

--- lib/iris/tests/integration/experimental/test_ugrid_load.py
+++ lib/iris/tests/integration/experimental/test_ugrid_load.py
@@ -48,16 +48,56 @@ class TestBasic(tests.IrisTest):
             cube, ("experimental", "ugrid", "2D_1t_face_half_levels.cml")
         )
 
-    # def test_3D_1t_face_half_levels(self):
-    #     pass
-    #
-    # def test_3D_1t_face_full_levels(self):
-    #     pass
-    #
-    # def test_multiple_time_values(self):
-    #     pass
-    #
-    # etc ...
+    def test_3D_1t_face_half_levels(self):
+        # TODO: remove constraint once file no longer has orphan connectivities.
+        theta_in_w3 = NameConstraint(var_name="theta_in_w3")
+        cube = ugrid_load_cube(
+            tests.get_data_path(
+                [
+                    "NetCDF",
+                    "unstructured_grid",
+                    "lfric_ngvat_3D_1t_half_level_face_grid_derived_theta_in_w3.nc",
+                ]
+            ),
+            constraint=theta_in_w3,
+        )
+        self.assertCML(
+            cube, ("experimental", "ugrid", "3D_1t_face_half_levels.cml")
+        )
+
+    def test_3D_1t_face_full_levels(self):
+        # TODO: remove constraint once file no longer has orphan connectivities.
+        area_fraction = NameConstraint(var_name="area_fraction")
+        cube = ugrid_load_cube(
+            tests.get_data_path(
+                [
+                    "NetCDF",
+                    "unstructured_grid",
+                    "lfric_ngvat_3D_1t_full_level_face_grid_main_area_fraction_unit1.nc",
+                ]
+            ),
+            constraint=area_fraction,
+        )
+        self.assertCML(
+            cube, ("experimental", "ugrid", "3D_1t_face_full_levels.cml")
+        )
+
+    def test_2D_72t_face_half_levels(self):
+        # TODO: remove constraint once file no longer has orphan connectivities.
+        conv_rain = NameConstraint(var_name="conv_rain")
+        cube = ugrid_load_cube(
+            tests.get_data_path(
+                [
+                    "NetCDF",
+                    "unstructured_grid",
+                    "lfric_ngvat_2D_72t_face_half_levels_main_conv_rain.nc",
+                ]
+            ),
+            constraint=conv_rain,
+        )
+        self.assertCML(
+            cube, ("experimental", "ugrid", "2D_72t_face_half_levels.cml")
+        )
 
 
 # class TestPseudoLevels(tests.IrisTest):
