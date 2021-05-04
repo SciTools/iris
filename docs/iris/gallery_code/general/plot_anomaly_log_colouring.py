707bda7fc34c3cfbec6c961e6eb680cea25cc8d3
Martin Yeo <40734014+trexfeathers@users.noreply.github.com>
2021-05-04 16:29:56 +0100

    Corrected plot_anomaly_log_colouring for new Matplotlib linscale rules. (#4115)

--- docs/iris/gallery_code/general/plot_anomaly_log_colouring.py
+++ docs/iris/gallery_code/general/plot_anomaly_log_colouring.py
@@ -68,13 +68,13 @@ def main():
     # Create a 'logarithmic' data normalization.
     anom_norm = mcols.SymLogNorm(
         linthresh=minimum_log_level,
-        linscale=1,
+        linscale=0.01,
         vmin=-maximum_scale_level,
         vmax=maximum_scale_level,
     )
     # Setting "linthresh=minimum_log_level" makes its non-logarithmic
     # data range equal to our 'zero band'.
-    # Setting "linscale=1" maps the whole zero band to the middle colour value
+    # Setting "linscale=0.01" maps the whole zero band to the middle colour value
     # (i.e., 0.5), which is the neutral point of a "diverging" style colormap.
 
     # Create an Axes, specifying the map projection.
