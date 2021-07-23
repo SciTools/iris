81e51f89fc6c73e0833b732315b9a200a22b1eb4
James Penn <james@jamespenn.co.uk>
2021-07-23 10:26:23 +0000

    Added license headers to benchmark files

--- benchmarks/benchmarks/import_iris.py
+++ benchmarks/benchmarks/import_iris.py
@@ -1,3 +1,8 @@
+# Copyright Iris contributors
+#
+# This file is part of Iris and is released under the LGPL license.
+# See COPYING and COPYING.LESSER in the root of the repository for full
+# licensing details.
 import sys
 
 
@@ -16,7 +21,7 @@ class Iris:
             sys.modules.pop(module)
 
     def time_iris(self):
-        import iris 
+        import iris
 
     def time__concatenate(self):
         import iris._concatenate
