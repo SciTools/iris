f53135805db7ac3d9b66c21b3166e5a3cbd9c2cd
James Penn <james@jamespenn.co.uk>
2021-10-29 16:32:44 +0100

    License header on image_test_output.py

--- docs/src/sphinxext/image_test_output.py
+++ docs/src/sphinxext/image_test_output.py
@@ -1,3 +1,9 @@
+# Copyright Iris contributors
+#
+# This file is part of Iris and is released under the LGPL license.
+# See COPYING and COPYING.LESSER in the root of the repository for full
+# licensing details.
+
 import json
 from typing import Dict, List
 import re
