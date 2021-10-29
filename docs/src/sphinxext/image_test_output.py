a8550fad1a7e89c5afb37c5d0edeaafec0c0e50b
James Penn <james@jamespenn.co.uk>
2021-10-29 15:09:35 +0000

    Added imagehash index to toc trees etc

--- docs/src/sphinxext/image_test_output.py
+++ docs/src/sphinxext/image_test_output.py
@@ -1,55 +1,51 @@
 import json
-from pathlib import Path
-from typing import TYPE_CHECKING, Dict, List
+from typing import Dict, List
+import re
 
 from docutils import nodes
-from docutils.parsers.rst import Directive
 from sphinx.util.docutils import SphinxDirective
-from sphinx.util.nodes import make_refnode
 from sphinx.application import Sphinx
 
 ImageRepo = Dict[str, List[str]]
+
+HASH_MATCH = re.compile(r"([^\/]+)\.png$")
+def hash_from_url(url: str) -> str:
+    match = HASH_MATCH.search(url)
+    if not match:
+        raise ValueError(f"url {url} does not match form `http...hash.png`")
+    else:
+        return match.groups()[0]
 
 class ImageTestDirective(SphinxDirective):
     def run(self):
         with open(self.config['image_test_json'], 'r') as fh:
             imagerepo = json.load(fh)
+        enum_list = nodes.enumerated_list()
         nodelist = []
-        for test in imagerepo:
-            paragraph_node = nodes.raw(f'<li><a src="/image_test/{test} />{test}</a></li>')
-            nodelist.append(paragraph_node)
+        nodelist.append(enum_list)
+        for test in sorted(imagerepo):
+            link_node = nodes.raw('', f'<a href="{self.config["html_baseurl"]}/generated/image_test/{test}.html" />{test}</a>', format='html')
+            li_node =  nodes.list_item('')
+            li_node += link_node
+            enum_list += li_node
         return nodelist
 
-def load_imagerepo_json(app: Sphinx, env, added, changed, removed):
-    with open(app.config['image_test_json'], 'r') as fh:
-        env.imagerepo = json.load(fh)
-    return []
-
 def collect_imagehash_pages(app: Sphinx):
+    """Generate pages for each entry in the imagerepo.json"""
     with open(app.config['image_test_json'], 'r') as fh:
         imagerepo: ImageRepo = json.load(fh)
     pages = []
     for test, hashfiles in imagerepo.items():
-        hashstrs = []
-        pages.append((f'image_test/{test}', {'test': test, 'hashfiles': hashfiles}, 'imagehash.html'))
+        hashstrs = [hash_from_url(h) for h in hashfiles]
+        pages.append((f'generated/image_test/{test}', {'test': test, 'hashfiles': zip(hashstrs, hashfiles)}, 'imagehash.html'))
     return pages
 
 
 def setup(app: Sphinx):
     app.add_config_value('image_test_json', '../../lib/iris/tests/results/imagerepo.json', 'html')
 
-    # app.add_node(todolist)
-    # app.add_node(todo,
-    #              html=(visit_todo_node, depart_todo_node),
-    #              latex=(visit_todo_node, depart_todo_node),
-    #              text=(visit_todo_node, depart_todo_node))
-
     app.add_directive('imagetest-list', ImageTestDirective)
-    # app.add_directive('todolist', TodolistDirective)
-    app.connect('env-get-outdated', load_imagerepo_json)
     app.connect('html-collect-pages', collect_imagehash_pages)
-    # app.connect('env-merge-info', merge_todos)
-
 
     return {
         'version': '0.1',
