474f0831695dcf4498b35541a6141a3cc4ac5828
stephen.worsley <stephen.worsley@metoffice.gov.uk>
2021-12-10 16:38:25 +0000

    fix array_equal behaviour for masked arrays

--- lib/iris/util.py
+++ lib/iris/util.py
@@ -363,7 +363,8 @@ def array_equal(array1, array2, withnans=False):
 
     def normalise_array(array):
         if not is_lazy_data(array):
-            array = np.asarray(array)
+            if not ma.isMaskedArray(array):
+                array = np.asarray(array)
         return array
 
     array1, array2 = normalise_array(array1), normalise_array(array2)
