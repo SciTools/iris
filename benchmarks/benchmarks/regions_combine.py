6261c5e8d071057a96d0e6df4cc0a5ec2596596a
Martin Yeo <martin.yeo@metoffice.gov.uk>
2022-02-02 14:48:17 +0000

    Regionds combine benchmark licence header.

--- benchmarks/benchmarks/regions_combine.py
+++ benchmarks/benchmarks/regions_combine.py
@@ -1,3 +1,8 @@
+# Copyright Iris contributors
+#
+# This file is part of Iris and is released under the LGPL license.
+# See COPYING and COPYING.LESSER in the root of the repository for full
+# licensing details.
 """
 Benchmarks stages of operation of the function
 :func:`iris.experimental.ugrid.utils.recombine_submeshes`.
