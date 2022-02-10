585f2669d474bb2d22a478693a41a436e24d1081
Martin Yeo <martin.yeo@metoffice.gov.uk>
2022-02-10 16:42:08 +0000

    Revert "[pre-commit.ci] pre-commit autoupdate (#4560)"
    This reverts commit d503ce62c41e9d827b91121e4f6395d1fcb6b644.

--- lib/iris/analysis/maths.py
+++ lib/iris/analysis/maths.py
@@ -540,7 +540,7 @@ def exponentiate(cube, exponent, in_place=False):
     return _math_op_common(
         cube,
         power,
-        cube.units**exponent,
+        cube.units ** exponent,
         new_dtype=new_dtype,
         in_place=in_place,
     )
