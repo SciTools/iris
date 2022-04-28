d31d6f5b6c1af5353b217da2b59409f11179d90b
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2022-04-28 15:17:57 +0100

    Addaux speedup (#4718)
    * Attempt faster add-aux-fact
    
    * Added whatsnew draft.
    
    * Put PR ref into whatsnew.
    
    * Review changes.

--- lib/iris/cube.py
+++ lib/iris/cube.py
@@ -1188,7 +1188,16 @@ class Cube(CFVariableMixin):
                 "Factory must be a subclass of "
                 "iris.aux_factory.AuxCoordFactory."
             )
-        cube_coords = self.coords()
+
+        # Get all 'real' coords (i.e. not derived ones) : use private data
+        # rather than cube.coords(), as that is quite slow.
+        def coordsonly(coords_and_dims):
+            return [coord for coord, dims in coords_and_dims]
+
+        cube_coords = coordsonly(self._dim_coords_and_dims) + coordsonly(
+            self._aux_coords_and_dims
+        )
+
         for dependency in aux_factory.dependencies:
             ref_coord = aux_factory.dependencies[dependency]
             if ref_coord is not None and ref_coord not in cube_coords:
