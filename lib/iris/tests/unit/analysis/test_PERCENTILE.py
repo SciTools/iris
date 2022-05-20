Bigger arrays to avoid np quirk, bug fix, check masking in 2d_multi

lib/iris/tests/unit/analysis/test_PERCENTILE.py
@@ -32,7 +32,7 @@ class AggregateMixin:
         if self.lazy:
             data = as_lazy_data(data)
 
-        expected = np.array(expected)
+        expected = ma.array(expected)
 
         actual = self.agg_method(
             data,
@@ -131,7 +131,7 @@ class ScipyAggregateMixin:
     def test_masked_2d_multi(self):
         shape = (3, 10)
         data = ma.arange(np.prod(shape)).reshape(shape)
-        data[1] = ma.masked
+        data[1, ::2] = ma.masked
         percent = np.array([10, 50, 70, 80])
         axis = 0
         mdtol = 0.1
@@ -140,10 +140,11 @@ class ScipyAggregateMixin:
         # linear interpolation.
         expected = percent / 100 * 20
         # Other columns are first column plus column number.
-        expected = (
+        expected = ma.array(
             np.broadcast_to(expected, (shape[-1], percent.size))
             + np.arange(shape[-1])[:, np.newaxis]
         )
+        expected[::2] = ma.masked
 
         self.check_percentile_calc(
             data, axis, percent, expected, mdtol=mdtol, approx=True
@@ -218,13 +219,13 @@ class Test_fast_aggregate(tests.IrisTest, AggregateMixin):
             )
 
     def test_masked_mdtol_0(self):
-        shape = (2, 11)
+        shape = (3, 11)
         axis = 0
         percent = 50
         data = ma.arange(np.prod(shape)).reshape(shape)
         data[0, ::2] = ma.masked
-        expected = ma.arange(shape[-1]) + 5.5
-        expected[0] = ma.masked
+        expected = ma.arange(shape[-1]) + 11
+        expected[::2] = ma.masked
         self.check_percentile_calc(data, axis, percent, expected, mdtol=0)
 
     @mock.patch("numpy.percentile")
@@ -307,14 +308,14 @@ class Test_lazy_fast_aggregate(tests.IrisTest, AggregateMixin, MultiAxisMixin):
             as_concrete_data(actual)
 
     def test_masked_mdtol_0(self):
-        shape = (2, 11)
+        shape = (3, 11)
         axis = 0
         percent = 50
         data = ma.arange(np.prod(shape)).reshape(shape)
         data[0, ::2] = ma.masked
         data = as_lazy_data(data)
-        expected = ma.arange(shape[-1]) + 5.5
-        expected[0] = ma.masked
+        expected = ma.arange(shape[-1]) + 11
+        expected[::2] = ma.masked
         self.check_percentile_calc(data, axis, percent, expected, mdtol=0)
 
     @mock.patch("numpy.percentile", return_value=np.array([2, 4]))