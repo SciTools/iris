918df4e092f205dc0829ffb1c6a094f021b35321
Ruth Comer <ruth.comer@metoffice.gov.uk>
2022-05-24 16:20:11 +0100

    collections again

--- lib/iris/tests/unit/cube/test_CubeList.py
+++ lib/iris/tests/unit/cube/test_CubeList.py
@@ -565,7 +565,9 @@ class Test_iteration(tests.IrisTest):
                 self.scalar_cubes.append(Cube(i, long_name=letter))
 
     def test_iterable(self):
-        self.assertTrue(isinstance(self.scalar_cubes, collections.Iterable))
+        self.assertTrue(
+            isinstance(self.scalar_cubes, collections.abc.Iterable)
+        )
 
     def test_iteration(self):
         letters = "abcd" * 5
