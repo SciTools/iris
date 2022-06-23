1e3ccd60f80446d1ff7e5eb37dcae05b67946351
Bill Little <bill.james.little@gmail.com>
2022-06-23 08:47:49 +0100

    linkcheck ignore metarelate.net

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -358,6 +358,7 @@ linkcheck_ignore = [
     "http://www.esrl.noaa.gov/psd/data/gridded/conventions/cdc_netcdf_standard.shtml",
     "http://www.nationalarchives.gov.uk/doc/open-government-licence",
     "https://www.metoffice.gov.uk/",
+    "http://www.metarelate.net/",
 ]
 
 # list of sources to exclude from the build.
