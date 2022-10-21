0c060f8cab5918bcce9c4bd5356f62e9b158fc42
Elias Sadek <elias.sadek@metoffice.gov.uk>
2022-10-21 15:18:32 +0100

    updated tests to include fixtures

--- lib/iris/tests/unit/analysis/cartography/test__get_lon_lat_coords.py
+++ lib/iris/tests/unit/analysis/cartography/test__get_lon_lat_coords.py
@@ -7,68 +7,88 @@
 
 import pytest
 from iris.tests.stock import lat_lon_cube
-from iris.analysis.cartography import _get_lon_lat_coords as g_lat_lon
+from iris.analysis.cartography import _get_lon_lat_coords as g_lon_lat
 from iris.coords import AuxCoord
 
-dim_only_cube = lat_lon_cube()
-lon_dim = dim_only_cube.coord("longitude")
-lat_dim = dim_only_cube.coord("latitude")
+
+@pytest.fixture
+def dim_only_cube():
+    return lat_lon_cube()
 
 
-def test_dim_only():
-    lon, lat = g_lat_lon(dim_only_cube)
-    assert lon == lon_dim
-    assert lat == lat_dim
+def test_dim_only(dim_only_cube):
+    lon, lat = g_lon_lat(dim_only_cube)
+    t_lat, t_lon = dim_only_cube.dim_coords
+    assert lon == t_lon
+    assert lat == t_lat
 
 
-dim_aux_cube = lat_lon_cube()
-lat_aux = AuxCoord.from_coord(lat_dim)
-lat_aux.standard_name = "grid_latitude"
-lon_aux = AuxCoord.from_coord(lon_dim)
-lon_aux.standard_name = "grid_longitude"
-dim_aux_cube.add_aux_coord(lat_aux, 0)
-dim_aux_cube.add_aux_coord(lon_aux, 1)
+@pytest.fixture
+def dim_aux_cube(dim_only_cube):
+    dim_aux_cube = dim_only_cube
+    lat_dim, lon_dim = dim_only_cube.dim_coords
+    lat_aux = AuxCoord.from_coord(lat_dim)
+    lat_aux.standard_name = "grid_latitude"
+    lon_aux = AuxCoord.from_coord(lon_dim)
+    lon_aux.standard_name = "grid_longitude"
+    dim_aux_cube.add_aux_coord(lat_aux, 0)
+    dim_aux_cube.add_aux_coord(lon_aux, 1)
+    return dim_aux_cube
 
 
-def test_dim_and_aux():
-    lon, lat = g_lat_lon(dim_aux_cube)
-    assert lon == lon_dim
-    assert lat == lat_dim
+def test_dim_aux(dim_aux_cube):
+    lon, lat = g_lon_lat(dim_aux_cube)
+    t_lat_dim, t_lon_dim = dim_aux_cube.dim_coords
+    assert lon == t_lon_dim
+    assert lat == t_lat_dim
 
 
-aux_only_cube = dim_aux_cube.copy(dim_aux_cube.data)
-aux_only_cube.remove_coord(lon_dim)
-aux_only_cube.remove_coord(lat_dim)
+@pytest.fixture
+def aux_only_cube(dim_aux_cube):
+    lon_dim, lat_dim = dim_aux_cube.dim_coords
+    dim_aux_cube.remove_coord(lon_dim)
+    dim_aux_cube.remove_coord(lat_dim)
+    return dim_aux_cube
 
 
-def test_aux_only():
-    lon, lat = g_lat_lon(aux_only_cube)
-    assert lon == lon_aux
-    assert lat == lat_aux
+def test_aux_only(aux_only_cube):
+    lon, lat = g_lon_lat(aux_only_cube)
+    aux_lat, aux_lon = aux_only_cube.aux_coords
+    assert lon == aux_lon
+    assert lat == aux_lat
 
 
-double_dim_cube = lat_lon_cube()
-double_dim_cube.coord("latitude").standard_name = "grid_longitude"
+@pytest.fixture
+def double_dim_cube(dim_only_cube):
+    double_dim_cube = dim_only_cube
+    double_dim_cube.coord("latitude").standard_name = "grid_longitude"
+    return double_dim_cube
 
 
-def test_double_dim():
+def test_double_dim(double_dim_cube):
     with pytest.raises(ValueError, match="with multiple.*is currently disallowed"):
-        _ = g_lat_lon(double_dim_cube)
+        _ = g_lon_lat(double_dim_cube)
 
 
-double_aux_cube = aux_only_cube.copy(aux_only_cube.data)
-double_aux_cube.coord("grid_latitude").standard_name = "longitude"
+@pytest.fixture
+def double_aux_cube(aux_only_cube):
+    double_aux_cube = aux_only_cube
+    double_aux_cube.coord("grid_latitude").standard_name = "longitude"
+    return double_aux_cube
 
 
-def test_double_aux():
+def test_double_aux(double_aux_cube):
     with pytest.raises(ValueError, match="with multiple.*is currently disallowed"):
-        _ = g_lat_lon(double_aux_cube)
+        _ = g_lon_lat(double_aux_cube)
 
 
-missing_lat_cube = lat_lon_cube()
-missing_lat_cube.remove_coord("latitude")
+@pytest.fixture
+def missing_lat_cube(dim_only_cube):
+    missing_lat_cube = dim_only_cube
+    missing_lat_cube.remove_coord("latitude")
+    return missing_lat_cube
 
 
-def test_missing_coord():
+def test_missing_coord(missing_lat_cube):
     with pytest.raises(IndexError):
-        _ = g_lat_lon(missing_lat_cube)
+        _ = g_lon_lat(missing_lat_cube)
