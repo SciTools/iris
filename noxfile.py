7766a8da3751da7a08ea6a7dca158e1093b84f1a
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2022-10-26 14:23:26 +0100

    Re-enable parallel testing.

--- noxfile.py
+++ noxfile.py
@@ -189,7 +189,6 @@ def tests(session: nox.sessions.Session):
         "python",
         "-m",
         "iris.tests.runner",
-        "--num-processors=1",
         "--default-tests",
         "--system-tests",
     )
