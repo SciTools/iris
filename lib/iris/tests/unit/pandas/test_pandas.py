c8b99a7081d34b8d3e73603808951b960ed99404
Martin Yeo <40734014+trexfeathers@users.noreply.github.com>
2022-11-16 18:33:15 +0000

    Move test_pandas into unit tests. (#5071)

--- lib/iris/tests/unit/pandas/test_pandas.py
+++ lib/iris/tests/unit/pandas/test_pandas.py
@@ -3,6 +3,7 @@
 # This file is part of Iris and is released under the LGPL license.
 # See COPYING and COPYING.LESSER in the root of the repository for full
 # licensing details.
+"""All unit tests for the :mod:`iris.pandas` module."""
 
 # import iris tests first so that some things can be initialised before
 # importing anything else
