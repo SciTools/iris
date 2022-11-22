51035457aca38a6056d7fe3aae05386810700f9e
Martin Yeo <martin.yeo@metoffice.gov.uk>
2022-11-22 17:05:19 +0000

    Re-instate test_load_laea_grid.

--- lib/iris/tests/integration/test_netcdf.py
+++ lib/iris/tests/integration/test_netcdf.py
@@ -507,12 +507,7 @@ class TestCoordSystem(tests.IrisTest):
                 ("NetCDF", "lambert_azimuthal_equal_area", "euro_air_temp.nc")
             )
         )
-        #
-        # FOR NOW:
-        # reference, but don't bother properly checking.
-        # - this specific load generates warnings, with no further action
-        assert cube is not None
-        # self.assertCML(cube, ("netcdf", "netcdf_laea.cml"))
+        self.assertCML(cube, ("netcdf", "netcdf_laea.cml"))
 
     datum_cf_var_cdl = """
         netcdf output {
