897a7cbb9547d76aedb5b60deef7fbc8a61f9240
Elias <110238618+ESadek-MO@users.noreply.github.com>
2023-02-28 10:06:47 +0000

    removed cell measure mask check and error (#5181)
    * removed cell measure mask check and error
    
    * updated whatsnew
    
    * updated tests
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * remove redundant test class in test_Saver.py, and its reference in test_Saver__lazy.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- lib/iris/tests/unit/fileformats/netcdf/test_Saver.py
+++ lib/iris/tests/unit/fileformats/netcdf/test_Saver.py
@@ -1051,38 +1051,5 @@ class Test__create_cf_grid_mapping(tests.IrisTest):
         self._test(coord_system, expected)
 
 
-class Test__create_cf_cell_measure_variable(tests.IrisTest):
-    # Saving of masked data is disallowed.
-
-    # Attribute is substituted in test_Saver__lazy.
-    array_lib = np
-
-    def setUp(self):
-        self.cube = stock.lat_lon_cube()
-        self.names_map = ["latitude", "longitude"]
-        masked_array = self.array_lib.ma.masked_array(
-            [0, 1, 2], mask=[True, False, True]
-        )
-        self.cm = iris.coords.CellMeasure(masked_array, var_name="cell_area")
-        self.cube.add_cell_measure(self.cm, data_dims=0)
-        self.exp_emsg = "Cell measures with missing data are not supported."
-
-    def test_masked_data__insitu(self):
-        # Test that the error is raised in the right place.
-        with self.temp_filename(".nc") as nc_path:
-            saver = Saver(nc_path, "NETCDF4")
-            with self.assertRaisesRegex(ValueError, self.exp_emsg):
-                saver._create_generic_cf_array_var(
-                    self.cube, self.names_map, self.cm
-                )
-
-    def test_masked_data__save_pipeline(self):
-        # Test that the right error is raised by the saver pipeline.
-        with self.temp_filename(".nc") as nc_path:
-            with Saver(nc_path, "NETCDF4") as saver:
-                with self.assertRaisesRegex(ValueError, self.exp_emsg):
-                    saver.write(self.cube)
-
-
 if __name__ == "__main__":
     tests.main()
