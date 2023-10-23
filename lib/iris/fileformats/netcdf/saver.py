6a1afae5cd0f8c89f2c34812cdd6e18365bb1f3b
Martin Yeo <martin.yeo@metoffice.gov.uk>
2023-10-23 17:11:19 +0100

    Use RotatedMercator inheritance for isinstance() check.

--- lib/iris/fileformats/netcdf/saver.py
+++ lib/iris/fileformats/netcdf/saver.py
@@ -2214,10 +2214,7 @@ class Saver:
                     cf_var_grid.sweep_angle_axis = cs.sweep_angle_axis
 
                 # oblique mercator (and rotated variant)
-                # Slightly odd isinstance() check to allow successful mocking.
-                elif isinstance(
-                    cs, iris.coord_systems.ObliqueMercator
-                ) or isinstance(cs, iris.coord_systems.RotatedMercator):
+                elif isinstance(cs, iris.coord_systems.ObliqueMercator):
                     # RotatedMercator subclasses ObliqueMercator, and RM
                     #  instances are implicitly saved as OM due to inherited
                     #  properties. This is correct because CF 1.11 is removing
