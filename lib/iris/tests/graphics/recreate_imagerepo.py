8e5977562e1dbfbf3183d92f8076081aee15a0f6
Bill Little <bill.james.little@gmail.com>
2023-12-18 11:14:24 +0000

    ruff EXE rule compliance

--- lib/iris/tests/graphics/recreate_imagerepo.py
+++ lib/iris/tests/graphics/recreate_imagerepo.py
@@ -1,8 +1,8 @@
+#!/usr/bin/env python
 # Copyright Iris contributors
 #
 # This file is part of Iris and is released under the BSD license.
 # See LICENSE in the root of the repository for full licensing details.
-# !/usr/bin/env python
 """Updates imagerepo.json based on the baseline images
 
 """
