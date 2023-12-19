203d6d20a26400231f8dd8130ab48fc1bb9c7708
tkknight <2108488+tkknight@users.noreply.github.com>
2023-12-19 16:51:33 +0000

    ruff rule D208 compliance. (#5656)

--- lib/iris/tests/unit/analysis/geometry/test_geometry_area_weights.py
+++ lib/iris/tests/unit/analysis/geometry/test_geometry_area_weights.py
@@ -2,10 +2,7 @@
 #
 # This file is part of Iris and is released under the BSD license.
 # See LICENSE in the root of the repository for full licensing details.
-"""Unit tests for the :func:`iris.analysis.geometry.geometry_area_weights`
-function.
-
- """
+"""Unit tests for the :func:`iris.analysis.geometry.geometry_area_weights` function."""
 
 # Import iris.tests first so that some things can be initialised before
 # importing anything else.
