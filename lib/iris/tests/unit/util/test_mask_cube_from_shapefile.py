440f0493bcbf34e06a56fde27ba9c588a539fee1
alex chamberlain-clay <a.chamberlain-clay@metoffice.gov.uk>
2024-02-07 11:38:58 +0000

    added test

--- lib/iris/tests/unit/util/test_mask_cube_from_shapefile.py
+++ lib/iris/tests/unit/util/test_mask_cube_from_shapefile.py
@@ -2,6 +2,7 @@ import numpy as np
 import pytest
 import shapely
 
+from iris.coord_systems import RotatedGeogCS
 from iris.coords import DimCoord
 import iris.cube
 from iris.exceptions import IrisUserWarning
@@ -10,7 +11,7 @@ from iris.util import mask_cube_from_shapefile
 
 
 class TestBasicCubeMasking(tests.IrisTest):
-    """Unit tests for mask_cube_from_shapefile function"""
+    """Unit tests for mask_cube_from_shapefile function."""
 
     def setUp(self):
         basic_data = np.array([[1, 2, 3], [4, 8, 12]])
@@ -76,6 +77,17 @@ class TestBasicCubeMasking(tests.IrisTest):
         assert (
             np.sum(masked_cube.data) == 24
         ), f"basic cube masking against line failed test - expected 24 got {np.sum(masked_cube.data)}"
+
+    def test_cube_coord_mismatch_warning(self):
+        shape = shapely.geometry.box(0.6, 0.6, 0.9, 0.9)
+        cube = self.basic_cube
+        cube.coord("projection_x_coordinate").points = [180, 360, 540]
+        cube.coord("projection_x_coordinate").coord_system = RotatedGeogCS(30, 30)
+        with pytest.warns(IrisUserWarning, match="masking"):
+            mask_cube_from_shapefile(
+                cube,
+                shape,
+            )
 
     def test_shape_invalid(self):
         shape = [5, 6, 7, 8]  # random array
