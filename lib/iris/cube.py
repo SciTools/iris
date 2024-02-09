fe43e097db44fd88ee990651ebc8f379a37fcc8b
Hamish Steptoe <hamish.steptoe@metoffice.gov.uk>
2024-02-09 15:53:30 +0000

    Add doctest: SKIP
    Adding `# doctest: SKIP` statements to multiline prints

--- lib/iris/cube.py
+++ lib/iris/cube.py
@@ -3396,34 +3396,32 @@ class Cube(CFVariableMixin):
         >>> fname = iris.sample_data_path('GloSea4', 'ensemble_01[01].pp')
         >>> cube = iris.load_cube(fname, 'surface_temperature')
         >>> print(cube)
-        surface_temperature / (K)           \
-        (realization: 2; time: 6; latitude: 145; longitude: 192)
+        surface_temperature / (K)           (realization: 2; time: 6; latitude: 145; longitude: 192)
         Dimension coordinates:
-        realization                             \
-        x        -            -               -
-        time                                    \
-        -        x            -               -
-        latitude                                \
-        -        -            x               -
-        longitude                               \
-        -        -            -               x
+            realization                             x        -            -               -
+            time                                    -        x            -               -
+            latitude                                -        -            x               -
+            longitude                               -        -            -               x
         Auxiliary coordinates:
-        forecast_period                         \
-        -        x            -               -
+            forecast_period                         -        x            -               -
         Scalar coordinates:
-        forecast_reference_time     2011-07-23 00:00:00
+            forecast_reference_time     2011-07-23 00:00:00
         Cell methods:
-        0                           time: mean (interval: 1 hour)
+            0                           time: mean (interval: 1 hour)
+        Attributes:
+            STASH                       m01s00i024
+            source                      'Data from Met Office Unified Model'
+            um_version                  '7.6'
 
         To get all 12x2D longitude/latitude subcubes:
 
         >>> for sub_cube in cube.slices_over(['realization', 'time']):
-        ...     print(sub_cube)
+        ...     print(sub_cube)  # doctest: SKIP
 
         To get realizations as 2x3D separate subcubes, using dimension indices:
 
         >>> for sub_cube in cube.slices_over(0):
-        ...     print(sub_cube)
+        ...     print(sub_cube)  # doctest: SKIP
 
         Notes
         -----
@@ -3495,34 +3493,32 @@ class Cube(CFVariableMixin):
         >>> fname = iris.sample_data_path('GloSea4', 'ensemble_01[01].pp')
         >>> cube = iris.load_cube(fname, 'surface_temperature')
         >>> print(cube)
-        surface_temperature / (K)           \
-        (realization: 2; time: 6; latitude: 145; longitude: 192)
+        surface_temperature / (K)           (realization: 2; time: 6; latitude: 145; longitude: 192)
         Dimension coordinates:
-        realization                             \
-        x        -            -               -
-        time                                    \
-        -        x            -               -
-        latitude                                \
-        -        -            x               -
-        longitude                               \
-        -        -            -               x
+            realization                             x        -            -               -
+            time                                    -        x            -               -
+            latitude                                -        -            x               -
+            longitude                               -        -            -               x
         Auxiliary coordinates:
-        forecast_period                         \
-        -        x            -               -
+            forecast_period                         -        x            -               -
         Scalar coordinates:
-        forecast_reference_time     2011-07-23 00:00:00
+            forecast_reference_time     2011-07-23 00:00:00
         Cell methods:
-        0                           time: mean (interval: 1 hour)
+            0                           time: mean (interval: 1 hour)
+        Attributes:
+            STASH                       m01s00i024
+            source                      'Data from Met Office Unified Model'
+            um_version                  '7.6'
 
         To get all 12x2D longitude/latitude subcubes:
 
         >>> for sub_cube in cube.slices(['longitude', 'latitude']):
-        ...     print(sub_cube)
+        ...     print(sub_cube)  # doctest: SKIP
 
         To get all realizations as 2x3D separate subcubes, using dimension indices:
 
         >>> for sub_cube in cube.slices([1, 2, 3]):
-        ...     print(sub_cube)
+        ...     print(sub_cube)  # doctest: SKIP
 
         See Also
         --------
