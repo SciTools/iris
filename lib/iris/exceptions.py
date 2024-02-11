a503041070b79af991d7cc83cf10928dc03929e7
stephen.worsley <stephen.worsley@metoffice.gov.uk>
2024-02-11 22:56:14 +0000

    fix warnings

--- lib/iris/exceptions.py
+++ lib/iris/exceptions.py
@@ -350,16 +350,16 @@ class IrisSaverFillValueWarning(IrisMaskValueMatchWarning, IrisSaveWarning):
 
 
 @lru_cache(None)
-def warn_once(msg, type, stacklevel, frame, **kwargs):
+def warn_once(msg, stacklevel, frame, **kwargs):
     """Raise a warning only if a similar one has not been raised before."""
-    warnings.warn(msg, type, stacklevel=stacklevel, **kwargs)
+    warnings.warn(msg, stacklevel=stacklevel, **kwargs)
 
 
-def warn_once_at_level(msg, type=None, stacklevel=0, **kwargs):
+def warn_once_at_level(msg, stacklevel=0, **kwargs):
     """
     Raise a warning only if a similar one hasn't been raised from the same line
     for a given stack level.
     """
     stacklevel += 1
     frame = traceback.format_stack()[-stacklevel]
-    warn_once(msg, type, stacklevel + 1, frame, **kwargs)
+    warn_once(msg, stacklevel + 1, frame, **kwargs)
