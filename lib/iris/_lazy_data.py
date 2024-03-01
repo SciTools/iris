d91c7c6d0dd032b78234b1c8cb257a9088bbdf20
Martin Yeo <martin.yeo@metoffice.gov.uk>
2024-03-01 16:31:49 +0000

    Typo.

--- lib/iris/_lazy_data.py
+++ lib/iris/_lazy_data.py
@@ -493,7 +493,7 @@ def map_complete_blocks(src, func, dims, out_sizes, *args, **kwargs):
     else:
         data = src.lazy_data()
 
-    if result is None and data is None:
+    if result is None and data is not None:
         # Ensure dims are not chunked
         in_chunks = list(data.chunks)
         for dim in dims:
