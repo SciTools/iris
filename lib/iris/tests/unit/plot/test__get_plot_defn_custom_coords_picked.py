de5f0278d736e46b2e64c6458f43484e819483f1
Martin Yeo <40734014+trexfeathers@users.noreply.github.com>
2024-03-20 15:36:17 +0000

    Convert `unit.plot` and `.quickplot` to PyTest (#5866)
    * Convert tests.unit.plot to PyTest.
    
    * Convert tests.unit.quickplot to PyTest.

--- lib/iris/tests/unit/plot/test__get_plot_defn_custom_coords_picked.py
+++ lib/iris/tests/unit/plot/test__get_plot_defn_custom_coords_picked.py
@@ -5,70 +5,64 @@
 """Unit tests for the `iris.plot._get_plot_defn_custom_coords_picked`
 function.
 """
-
-# Import iris.tests first so that some things can be initialised before
-# importing anything else.
-import iris.tests as tests  # isort:skip
+import pytest
 
 from iris.coords import BOUND_MODE, POINT_MODE
+from iris.tests import _shared_utils
 from iris.tests.stock import hybrid_height, simple_2d, simple_2d_w_multidim_coords
 
-if tests.MPL_AVAILABLE:
+if _shared_utils.MPL_AVAILABLE:
     import iris.plot as iplt
 
 
-@tests.skip_plot
-class Test_get_plot_defn_custom_coords_picked(tests.IrisTest):
+@_shared_utils.skip_plot
+class Test_get_plot_defn_custom_coords_picked:
     def test_1d_coords(self):
         cube = simple_2d()
         defn = iplt._get_plot_defn_custom_coords_picked(
             cube, ("foo", "bar"), POINT_MODE
         )
-        self.assertEqual([coord.name() for coord in defn.coords], ["bar", "foo"])
-        self.assertFalse(defn.transpose)
+        assert [coord.name() for coord in defn.coords] == ["bar", "foo"]
+        assert not defn.transpose
 
     def test_1d_coords_swapped(self):
         cube = simple_2d()
         defn = iplt._get_plot_defn_custom_coords_picked(
             cube, ("bar", "foo"), POINT_MODE
         )
-        self.assertEqual([coord.name() for coord in defn.coords], ["foo", "bar"])
-        self.assertTrue(defn.transpose)
+        assert [coord.name() for coord in defn.coords] == ["foo", "bar"]
+        assert defn.transpose
 
     def test_1d_coords_as_integers(self):
         cube = simple_2d()
         defn = iplt._get_plot_defn_custom_coords_picked(cube, (1, 0), POINT_MODE)
-        self.assertEqual([coord for coord in defn.coords], [0, 1])
-        self.assertFalse(defn.transpose)
+        assert [coord for coord in defn.coords] == [0, 1]
+        assert not defn.transpose
 
     def test_1d_coords_as_integers_swapped(self):
         cube = simple_2d()
         defn = iplt._get_plot_defn_custom_coords_picked(cube, (0, 1), POINT_MODE)
-        self.assertEqual([coord for coord in defn.coords], [1, 0])
-        self.assertTrue(defn.transpose)
+        assert [coord for coord in defn.coords] == [1, 0]
+        assert defn.transpose
 
     def test_2d_coords(self):
         cube = simple_2d_w_multidim_coords()
         defn = iplt._get_plot_defn_custom_coords_picked(
             cube, ("foo", "bar"), BOUND_MODE
         )
-        self.assertEqual([coord.name() for coord in defn.coords], ["bar", "foo"])
-        self.assertFalse(defn.transpose)
+        assert [coord.name() for coord in defn.coords] == ["bar", "foo"]
+        assert not defn.transpose
 
     def test_2d_coords_as_integers(self):
         cube = simple_2d_w_multidim_coords()
         defn = iplt._get_plot_defn_custom_coords_picked(cube, (0, 1), BOUND_MODE)
-        self.assertEqual([coord for coord in defn.coords], [1, 0])
-        self.assertTrue(defn.transpose)
+        assert [coord for coord in defn.coords] == [1, 0]
+        assert defn.transpose
 
     def test_span_check(self):
         cube = hybrid_height()
         emsg = "don't span the 2 data dimensions"
-        with self.assertRaisesRegex(ValueError, emsg):
+        with pytest.raises(ValueError, match=emsg):
             iplt._get_plot_defn_custom_coords_picked(
                 cube, ("sigma", "level_height"), POINT_MODE
             )
-
-
-if __name__ == "__main__":
-    tests.main()
--- lib/iris/tests/unit/plot/test_pcolormesh.py
+++ lib/iris/tests/unit/plot/test_pcolormesh.py
@@ -3,11 +3,7 @@
 # This file is part of Iris and is released under the BSD license.
 # See LICENSE in the root of the repository for full licensing details.
 """Unit tests for the `iris.plot.pcolormesh` function."""
-
-# Import iris.tests first so that some things can be initialised before
-# importing anything else.
-import iris.tests as tests  # isort:skip
-
+from iris.tests import _shared_utils
 from iris.tests.unit.plot import TestGraphicStringCoord
 from iris.tests.unit.plot._blockplot_common import (
     Mixin2dCoordsContigTol,
@@ -15,35 +11,28 @@ from iris.tests.unit.plot._blockplot_common import (
     MixinStringCoordPlot,
 )
 
-if tests.MPL_AVAILABLE:
+if _shared_utils.MPL_AVAILABLE:
     import iris.plot as iplt
 
     PLOT_FUNCTION_TO_TEST = iplt.pcolormesh
 
 
-@tests.skip_plot
+@_shared_utils.skip_plot
 class TestStringCoordPlot(MixinStringCoordPlot, TestGraphicStringCoord):
     def blockplot_func(self):
         return PLOT_FUNCTION_TO_TEST
 
 
-@tests.skip_plot
-class Test2dCoords(tests.IrisTest, Mixin2dCoordsPlot):
-    def setUp(self):
-        self.blockplot_setup()
-
+@_shared_utils.skip_plot
+class Test2dCoords(Mixin2dCoordsPlot):
     def blockplot_func(self):
         return PLOT_FUNCTION_TO_TEST
 
 
-@tests.skip_plot
-class Test2dContigTol(tests.IrisTest, Mixin2dCoordsContigTol):
+@_shared_utils.skip_plot
+class Test2dContigTol(Mixin2dCoordsContigTol):
     # Extra call kwargs expected -- unlike 'pcolor', there are none.
     additional_kwargs = {}
 
     def blockplot_func(self):
         return PLOT_FUNCTION_TO_TEST
-
-
-if __name__ == "__main__":
-    tests.main()
