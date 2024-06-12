a4dad3aeb0428c954fe255133f017f0680d16495
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2024-06-12 18:21:57 +0100

    Add tests for more intelligible errors.

--- lib/iris/experimental/ugrid/load.py
+++ lib/iris/experimental/ugrid/load.py
@@ -490,7 +490,7 @@ def _build_mesh_coords(mesh, cf_var):
         # We should probably issue warnings and recover, but that is too much
         # work.  Raising a more intelligible error is easy to do though.
         msg = (
-            f"mesh data variable {cf_var.name!s} has an invalid "
+            f"mesh data variable {cf_var.name!r} has an invalid "
             f"location={location!r}."
         )
         raise ValueError(msg)
