8d26eab42b64c3954b57122420a74fc9750cd143
Martin Yeo <martin.yeo@metoffice.gov.uk>
2024-07-05 11:42:37 +0100

    Remove testing.

--- lib/iris/tests/stock/__init__.py
+++ lib/iris/tests/stock/__init__.py
@@ -268,7 +268,6 @@ def simple_3d():
 
     """
     cube = Cube(np.arange(24, dtype=np.int32).reshape((2, 3, 4)))
-    more = [cube.copy() for _ in range(10)]
     cube.long_name = "thingness"
     cube.units = "1"
     wibble_coord = DimCoord(
