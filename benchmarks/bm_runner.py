9dffa1bec548d8a73379ca62ab5b76f2e4fb3c2e
Martin Yeo <martin.yeo@metoffice.gov.uk>
2024-07-08 13:49:23 +0100

    Temporary quick demo.

--- benchmarks/bm_runner.py
+++ benchmarks/bm_runner.py
@@ -27,7 +27,10 @@ ROOT_DIR = BENCHMARKS_DIR.parent
 GH_REPORT_DIR = ROOT_DIR.joinpath(".github", "workflows", "benchmark_reports")
 
 # Common ASV arguments for all run_types except `custom`.
-ASV_HARNESS = "run {posargs} --attribute rounds=4 --interleave-rounds --show-stderr"
+ASV_HARNESS = (
+    "run {posargs} --attribute rounds=4 --interleave-rounds --show-stderr "
+    "--bench tracemalloc_merge"
+)
 
 
 def echo(echo_string: str):
