2d614b822a0732fed98126077d04587e5fc17853
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2024-07-25 11:27:51 +0100

    Import annotations in new files.

--- lib/iris/tests/unit/ugrid/utils/__init__.py
+++ lib/iris/tests/unit/ugrid/utils/__init__.py
@@ -3,3 +3,5 @@
 # This file is part of Iris and is released under the BSD license.
 # See LICENSE in the root of the repository for full licensing details.
 """Unit tests for the :mod:`iris.ugrid.utils` package."""
+
+from __future__ import annotations
