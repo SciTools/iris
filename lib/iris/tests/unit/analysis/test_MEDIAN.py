cf988b6a4b0c28672e653a904ae3541a63881c34
Francesco Nattino <f.nattino@esciencecenter.nl>
2025-01-17 09:18:56 +0100

    move tests to pytest

--- lib/iris/tests/unit/analysis/test_MEDIAN.py
+++ lib/iris/tests/unit/analysis/test_MEDIAN.py
@@ -4,10 +4,6 @@
 # See LICENSE in the root of the repository for full licensing details.
 """Unit tests for the :data:`iris.analysis.MEDIAN` aggregator."""
 
-# Import iris.tests first so that some things can be initialised before
-# importing anything else.
-import iris.tests as tests  # isort:skip
-
 import numpy as np
 import numpy.ma as ma
 
@@ -18,6 +14,7 @@ from iris._lazy_data import (
     is_lazy_masked_data,
 )
 from iris.analysis import MEDIAN
+from iris.tests._shared_utils import assert_array_almost_equal, assert_array_equal
 
 
 def _get_data(lazy=False, masked=False):
@@ -30,25 +27,25 @@ def _get_data(lazy=False, masked=False):
     return data
 
 
-class Test_basics(tests.IrisTest):
-    def setUp(self):
+class Test_basics:
+    def setup_method(self):
         self.data = _get_data()
 
     def test_name(self):
-        self.assertEqual(MEDIAN.name(), "median")
+        assert MEDIAN.name() == "median"
 
     def test_collapse(self):
         data = MEDIAN.aggregate(self.data, axis=(0, 1))
-        self.assertArrayEqual(data, [7.5])
+        assert_array_equal(data, [7.5])
 
 
-class Test_masked(tests.IrisTest):
-    def setUp(self):
+class Test_masked:
+    def setup_method(self):
         self.data = _get_data(masked=True)
 
     def test_output_is_masked(self):
         result = MEDIAN.aggregate(self.data, axis=1)
-        self.assertTrue(ma.isMaskedArray(result))
+        assert ma.isMaskedArray(result)
 
     def test_median_is_mask_aware(self):
         # the median computed along one axis differs if the array is masked
@@ -56,42 +53,38 @@ class Test_masked(tests.IrisTest):
         result = MEDIAN.aggregate(self.data, axis=axis)
         data_no_mask = _get_data()
         result_no_mask = MEDIAN.aggregate(data_no_mask, axis=axis)
-        self.assertFalse(np.allclose(result, result_no_mask))
+        assert not np.allclose(result, result_no_mask)
 
 
-class Test_lazy(tests.IrisTest):
-    def setUp(self):
+class Test_lazy:
+    def setup_method(self):
         self.data = _get_data(lazy=True)
 
     def test_output_is_lazy(self):
         result = MEDIAN.lazy_aggregate(self.data, axis=(0, 1))
-        self.assertTrue(is_lazy_data(result))
+        assert is_lazy_data(result)
 
     def test_shape(self):
         result = MEDIAN.lazy_aggregate(self.data, axis=1)
-        self.assertTupleEqual(result.shape, (4,))
+        assert result.shape == (4,)
 
     def test_result_values(self):
         axis = 1
         result = MEDIAN.lazy_aggregate(self.data, axis=axis)
         expected = np.median(as_concrete_data(self.data), axis=axis)
-        self.assertArrayAlmostEqual(result, expected)
+        assert_array_almost_equal(result, expected)
 
 
-class Test_lazy_masked(tests.IrisTest):
-    def setUp(self):
+class Test_lazy_masked:
+    def setup_method(self):
         self.data = _get_data(lazy=True, masked=True)
 
     def test_output_is_lazy_and_masked(self):
         result = MEDIAN.lazy_aggregate(self.data, axis=1)
-        self.assertTrue(is_lazy_masked_data(result))
+        assert is_lazy_masked_data(result)
 
     def test_result_values(self):
         axis = 1
         result = MEDIAN.lazy_aggregate(self.data, axis=axis)
         expected = ma.median(as_concrete_data(self.data), axis=axis)
-        self.assertArrayAlmostEqual(result, expected)
-
-
-if __name__ == "__main__":
-    tests.main()
+        assert_array_almost_equal(result, expected)
