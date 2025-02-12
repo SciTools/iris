71c665c4cbf277c3b296d7a7adebd56538a2860e
Bouwe Andela <b.andela@esciencecenter.nl>
2025-02-12 22:11:32 +0100

    Add license header

--- lib/iris/tests/unit/lazy_data/test_lrucache.py
+++ lib/iris/tests/unit/lazy_data/test_lrucache.py
@@ -1,3 +1,9 @@
+# Copyright Iris contributors
+#
+# This file is part of Iris and is released under the BSD license.
+# See LICENSE in the root of the repository for full licensing details.
+"""Test function :func:`iris._lazy data.LRUCache`."""
+
 from iris._lazy_data import LRUCache
 
 
