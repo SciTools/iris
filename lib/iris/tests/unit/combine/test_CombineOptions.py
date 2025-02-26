ea9d82f36fa43187fc1156c39071b02d26c4d897
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2025-02-26 15:52:55 +0000

    Recombine LoadPolicy into CombineOptions.

--- lib/iris/tests/unit/combine/test_CombineOptions.py
+++ lib/iris/tests/unit/combine/test_CombineOptions.py
@@ -2,27 +2,27 @@
 #
 # This file is part of Iris and is released under the BSD license.
 # See LICENSE in the root of the repository for full licensing details.
-"""Unit tests for the :mod:`iris.io.loading.LoadPolicy` package."""
+"""Unit tests for the :mod:`iris._combine.CombineOptions` class."""
 
 from unittest import mock
 
 import pytest
 
-from iris import LoadPolicy
+from iris import CombineOptions
 
 
 class TestInit:
     def test_init_empty(self):
         # Check how a bare init works
-        options = LoadPolicy()
-        assert options.settings() == LoadPolicy.SETTINGS["default"]
+        options = CombineOptions()
+        assert options.settings() == CombineOptions.SETTINGS["default"]
 
     def test_init_args_kwargs(self):
         # Check that init with args, kwargs equates to a pair of set() calls.
-        with mock.patch("iris.LoadPolicy.set") as mock_set:
+        with mock.patch("iris.CombineOptions.set") as mock_set:
             test_option = mock.sentinel.option
             test_kwargs = {"junk": "invalid"}
-            LoadPolicy(options=test_option, **test_kwargs)
+            CombineOptions(options=test_option, **test_kwargs)
         assert mock_set.call_args_list == [
             mock.call("default"),
             mock.call(test_option, **test_kwargs),
@@ -33,11 +33,11 @@ class Test_settings:
     """The .settings() returns a dict full of the settings."""
 
     def test_settings(self):
-        options = LoadPolicy()
+        options = CombineOptions()
         settings = options.settings()
         assert isinstance(settings, dict)
-        assert list(settings.keys()) == LoadPolicy.OPTION_KEYS
-        for key in LoadPolicy.OPTION_KEYS:
+        assert list(settings.keys()) == CombineOptions.OPTION_KEYS
+        for key in CombineOptions.OPTION_KEYS:
             assert settings[key] == getattr(options, key)
 
 
@@ -45,13 +45,13 @@ class Test_set:
     """Check the .set(arg, **kwargs) behaviour."""
 
     def test_empty(self):
-        options = LoadPolicy()
+        options = CombineOptions()
         orig_settings = options.settings()
         options.set()
         assert options.settings() == orig_settings
 
     def test_arg_dict(self):
-        options = LoadPolicy()
+        options = CombineOptions()
         assert options.settings()["merge_concat_sequence"] == "m"
         assert options.settings()["repeat_until_unchanged"] is False
         options.set({"merge_concat_sequence": "c", "repeat_until_unchanged": True})
@@ -59,7 +59,7 @@ class Test_set:
         assert options.settings()["repeat_until_unchanged"] is True
 
     def test_arg_string(self):
-        options = LoadPolicy()
+        options = CombineOptions()
         assert options.settings()["merge_concat_sequence"] == "m"
         assert options.settings()["repeat_until_unchanged"] is False
         options.set("comprehensive")
@@ -67,13 +67,13 @@ class Test_set:
         assert options.settings()["repeat_until_unchanged"] is True
 
     def test_arg_bad_dict(self):
-        options = LoadPolicy()
+        options = CombineOptions()
         expected = "Unknown options.*'junk'.* : valid options are"
         with pytest.raises(ValueError, match=expected):
             options.set({"junk": "invalid"})
 
     def test_arg_bad_string(self):
-        options = LoadPolicy()
+        options = CombineOptions()
         expected = (
             r"arg 'options'='oddthing'.*not a valid setting.*expected one of.* "
             "['legacy', 'default', 'recommended', 'comprehensive']"
@@ -82,7 +82,7 @@ class Test_set:
             options.set("oddthing")
 
     def test_kwargs(self):
-        options = LoadPolicy()
+        options = CombineOptions()
         assert options.settings()["merge_concat_sequence"] == "m"
         assert options.settings()["repeat_until_unchanged"] is False
         options.set(merge_concat_sequence="c", repeat_until_unchanged=True)
@@ -91,7 +91,7 @@ class Test_set:
 
     def test_arg_kwargs(self):
         # Show that kwargs override arg
-        options = LoadPolicy(
+        options = CombineOptions(
             support_multiple_references=False,
             merge_concat_sequence="",
             repeat_until_unchanged=False,
@@ -104,7 +104,7 @@ class Test_set:
         assert options.repeat_until_unchanged is True
 
     def test_bad_kwarg(self):
-        options = LoadPolicy()
+        options = CombineOptions()
         expected = "Unknown options.*'junk'.* : valid options are"
         with pytest.raises(ValueError, match=expected):
             options.set({"junk": "invalid"})
@@ -114,28 +114,28 @@ class Test_AttributeAccess:
     """Check operation of direct property access (with ".")."""
 
     def test_getattr(self):
-        options = LoadPolicy(merge_concat_sequence="m")
+        options = CombineOptions(merge_concat_sequence="m")
         assert options.merge_concat_sequence == "m"
 
     def test_getattr_badname(self):
-        options = LoadPolicy()
-        expected = "'LoadPolicy' object has no attribute 'unknown'"
+        options = CombineOptions()
+        expected = "'CombineOptions' object has no attribute 'unknown'"
         with pytest.raises(AttributeError, match=expected):
             options.unknown
 
     def test_setattr(self):
-        options = LoadPolicy(merge_concat_sequence="m")
+        options = CombineOptions(merge_concat_sequence="m")
         options.merge_concat_sequence = "mc"
         assert options.merge_concat_sequence == "mc"
 
     def test_setattr_badname(self):
-        options = LoadPolicy()
-        expected = "LoadPolicy object has no property 'anyold_property'"
+        options = CombineOptions()
+        expected = "CombineOptions object has no property 'anyold_property'"
         with pytest.raises(KeyError, match=expected):
             options.anyold_property = "x"
 
     def test_setattr_badvalue(self):
-        options = LoadPolicy()
+        options = CombineOptions()
         expected = "'mcm' is not a valid.*merge_concat_sequence : must be one of"
         with pytest.raises(ValueError, match=expected):
             options.merge_concat_sequence = "mcm"
