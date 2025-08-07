d31b58e9a34e0823024a3548cdb0a255b9fa0044
Henry Wright <henrywright@sky.com>
2025-08-07 14:41:48 +0100

    add test_Linear

--- lib/iris/tests/unit/analysis/test_Linear.py
+++ lib/iris/tests/unit/analysis/test_Linear.py
@@ -4,11 +4,7 @@
 # See LICENSE in the root of the repository for full licensing details.
 """Unit tests for :class:`iris.analysis.Linear`."""
 
-# Import iris.tests first so that some things can be initialised before
-# importing anything else.
-import iris.tests as tests  # isort:skip
-
-from unittest import mock
+import pytest
 
 from iris.analysis import Linear
 
@@ -20,14 +16,14 @@ def create_scheme(mode=None):
     return Linear(**kwargs)
 
 
-class Test_extrapolation_mode(tests.IrisTest):
+class Test_extrapolation_mode:
     def check_mode(self, mode):
         linear = create_scheme(mode)
-        self.assertEqual(linear.extrapolation_mode, mode)
+        assert linear.extrapolation_mode == mode
 
     def test_default(self):
         linear = Linear()
-        self.assertEqual(linear.extrapolation_mode, "linear")
+        assert linear.extrapolation_mode == "linear"
 
     def test_extrapolate(self):
         self.check_mode("extrapolate")
@@ -48,95 +44,91 @@ class Test_extrapolation_mode(tests.IrisTest):
         self.check_mode("nanmask")
 
     def test_invalid(self):
-        with self.assertRaisesRegex(ValueError, "Extrapolation mode"):
+        with pytest.raises(ValueError, match="Extrapolation mode"):
             Linear("bogus")
 
 
-class Test_interpolator(tests.IrisTest):
-    def check_mode(self, mode=None):
+class Test_interpolator:
+    def check_mode(self, mocker, mode=None):
         linear = create_scheme(mode)
 
         # Check that calling `linear.interpolator(...)` returns an
         # instance of RectilinearInterpolator which has been created
         # using the correct arguments.
-        with mock.patch(
+        ri = mocker.patch(
             "iris.analysis.RectilinearInterpolator",
-            return_value=mock.sentinel.interpolator,
-        ) as ri:
-            interpolator = linear.interpolator(mock.sentinel.cube, mock.sentinel.coords)
+            return_value=mocker.sentinel.interpolator,
+        )
+        interpolator = linear.interpolator(mocker.sentinel.cube, mocker.sentinel.coords)
         if mode is None or mode == "linear":
             expected_mode = "extrapolate"
         else:
             expected_mode = mode
         ri.assert_called_once_with(
-            mock.sentinel.cube, mock.sentinel.coords, "linear", expected_mode
+            mocker.sentinel.cube, mocker.sentinel.coords, "linear", expected_mode
         )
-        self.assertIs(interpolator, mock.sentinel.interpolator)
+        assert interpolator is mocker.sentinel.interpolator
 
-    def test_default(self):
-        self.check_mode()
+    def test_default(self, mocker):
+        self.check_mode(mocker)
 
-    def test_extrapolate(self):
-        self.check_mode("extrapolate")
+    def test_extrapolate(self, mocker):
+        self.check_mode(mocker, "extrapolate")
 
-    def test_linear(self):
-        self.check_mode("linear")
+    def test_linear(self, mocker):
+        self.check_mode(mocker, "linear")
 
-    def test_nan(self):
-        self.check_mode("nan")
+    def test_nan(self, mocker):
+        self.check_mode(mocker, "nan")
 
-    def test_error(self):
-        self.check_mode("error")
+    def test_error(self, mocker):
+        self.check_mode(mocker, "error")
 
-    def test_mask(self):
-        self.check_mode("mask")
+    def test_mask(self, mocker):
+        self.check_mode(mocker, "mask")
 
-    def test_nanmask(self):
-        self.check_mode("nanmask")
+    def test_nanmask(self, mocker):
+        self.check_mode(mocker, "nanmask")
 
 
-class Test_regridder(tests.IrisTest):
-    def check_mode(self, mode=None):
+class Test_regridder:
+    def check_mode(self, mocker, mode=None):
         linear = create_scheme(mode)
 
         # Check that calling `linear.regridder(...)` returns an instance
         # of RectilinearRegridder which has been created using the correct
         # arguments.
-        with mock.patch(
+        lr = mocker.patch(
             "iris.analysis.RectilinearRegridder",
-            return_value=mock.sentinel.regridder,
-        ) as lr:
-            regridder = linear.regridder(mock.sentinel.src, mock.sentinel.target)
+            return_value=mocker.sentinel.regridder,
+        )
+        regridder = linear.regridder(mocker.sentinel.src, mocker.sentinel.target)
         if mode is None or mode == "linear":
             expected_mode = "extrapolate"
         else:
             expected_mode = mode
         lr.assert_called_once_with(
-            mock.sentinel.src, mock.sentinel.target, "linear", expected_mode
+            mocker.sentinel.src, mocker.sentinel.target, "linear", expected_mode
         )
-        self.assertIs(regridder, mock.sentinel.regridder)
+        assert regridder is mocker.sentinel.regridder
 
-    def test_default(self):
-        self.check_mode()
+    def test_default(self, mocker):
+        self.check_mode(mocker)
 
-    def test_extrapolate(self):
-        self.check_mode("extrapolate")
+    def test_extrapolate(self, mocker):
+        self.check_mode(mocker, "extrapolate")
 
-    def test_linear(self):
-        self.check_mode("linear")
+    def test_linear(self, mocker):
+        self.check_mode(mocker, "linear")
 
-    def test_nan(self):
-        self.check_mode("nan")
+    def test_nan(self, mocker):
+        self.check_mode(mocker, "nan")
 
-    def test_error(self):
-        self.check_mode("error")
+    def test_error(self, mocker):
+        self.check_mode(mocker, "error")
 
-    def test_mask(self):
-        self.check_mode("mask")
+    def test_mask(self, mocker):
+        self.check_mode(mocker, "mask")
 
-    def test_nanmask(self):
-        self.check_mode("nanmask")
-
-
-if __name__ == "__main__":
-    tests.main()
+    def test_nanmask(self, mocker):
+        self.check_mode(mocker, "nanmask")
