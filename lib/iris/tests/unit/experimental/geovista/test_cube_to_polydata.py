dac6fa9fa8d14b694caef96eddb83f0623252409
Martin Yeo <40734014+trexfeathers@users.noreply.github.com>
2025-08-12 15:07:37 +0100

    Convert `tests/unit/experimental` to PyTest (#6630)
    * Convert test_export_geotiff.py to PyTest.
    
    * Convert test_regrid_area_weighted to PyTest.
    
    * Convert test_regrid_weighted_curvilinear to PyTest.
    
    * Convert test_CubeListRepresentation to PyTest.
    
    * Convert test_CubeRepresentation to PyTest.
    
    * Convert test_relevel to PyTest.
    
    * Convert test_cube_to_polydata to PyTest.
    
    * Convert test_extract_unstructured_region to PyTest.
    
    * Missed a np.testing call.

--- lib/iris/tests/unit/experimental/geovista/test_cube_to_polydata.py
+++ lib/iris/tests/unit/experimental/geovista/test_cube_to_polydata.py
@@ -5,7 +5,6 @@
 """Unit tests for the `iris.experimental.geovista.cube_to_polydata` function."""
 
 from typing import ClassVar
-from unittest.mock import Mock
 
 from geovista import Transform
 import numpy as np
@@ -14,16 +13,17 @@ import pytest
 import iris.analysis.cartography
 import iris.coord_systems
 from iris.experimental.geovista import cube_to_polydata
+from iris.tests import _shared_utils
 from iris.tests.stock import lat_lon_cube, sample_2d_latlons
 from iris.tests.stock.mesh import sample_mesh_cube
 
 
-@pytest.fixture()
+@pytest.fixture
 def cube_mesh():
     return sample_mesh_cube()
 
 
-@pytest.fixture()
+@pytest.fixture
 def cube_1d():
     sample_1d_cube = lat_lon_cube()
     for coord in sample_1d_cube.dim_coords:
@@ -31,12 +31,12 @@ def cube_1d():
     return sample_1d_cube
 
 
-@pytest.fixture()
+@pytest.fixture
 def cube_2d():
     return sample_2d_latlons()
 
 
-@pytest.fixture()
+@pytest.fixture
 def default_cs():
     return iris.coord_systems.GeogCS(
         iris.analysis.cartography.DEFAULT_SPHERICAL_EARTH_RADIUS
@@ -46,28 +46,28 @@ def default_cs():
 class ParentClass:
     MOCKED_OPERATION: ClassVar[str]
 
-    @pytest.fixture()
+    @pytest.fixture
     def expected(self):
         pass
 
-    @pytest.fixture()
+    @pytest.fixture
     def operation(self):
         pass
 
-    @pytest.fixture()
+    @pytest.fixture
     def cube(self):
         pass
 
-    @pytest.fixture()
+    @pytest.fixture
     def cube_with_crs(self, default_cs, cube):
         cube_crs = cube.copy()
         for coord in cube_crs.coords():
             coord.coord_system = default_cs
         return cube_crs
 
-    @pytest.fixture()
-    def mocked_operation(self):
-        mocking = Mock()
+    @pytest.fixture
+    def mocked_operation(self, mocker):
+        mocking = mocker.Mock()
         setattr(Transform, self.MOCKED_OPERATION, mocking)
         return mocking
 
@@ -77,7 +77,7 @@ class ParentClass:
         actual = mocked_operation.call_args.kwargs
         for key, expected_value in expected.items():
             if hasattr(expected_value, "shape"):
-                np.testing.assert_array_equal(actual[key], expected_value)
+                _shared_utils.assert_array_equal(actual[key], expected_value)
             else:
                 assert actual[key] == expected_value
 
@@ -98,7 +98,7 @@ class ParentClass:
 class Test2dToPoly(ParentClass):
     MOCKED_OPERATION = "from_2d"
 
-    @pytest.fixture()
+    @pytest.fixture
     def expected(self, cube_2d):
         return {
             "xs": cube_2d.coord(axis="X").contiguous_bounds(),
@@ -107,7 +107,7 @@ class Test2dToPoly(ParentClass):
             "name": f"{cube_2d.name()} / ({cube_2d.units})",
         }
 
-    @pytest.fixture()
+    @pytest.fixture
     def cube(self, cube_2d):
         return cube_2d
 
@@ -115,7 +115,7 @@ class Test2dToPoly(ParentClass):
 class Test1dToPoly(ParentClass):
     MOCKED_OPERATION = "from_1d"
 
-    @pytest.fixture()
+    @pytest.fixture
     def expected(self, cube_1d):
         return {
             "xs": cube_1d.coord(axis="X").contiguous_bounds(),
@@ -124,7 +124,7 @@ class Test1dToPoly(ParentClass):
             "name": f"{cube_1d.name()} / ({cube_1d.units})",
         }
 
-    @pytest.fixture()
+    @pytest.fixture
     def cube(self, cube_1d):
         return cube_1d
 
@@ -132,7 +132,7 @@ class Test1dToPoly(ParentClass):
 class TestMeshToPoly(ParentClass):
     MOCKED_OPERATION = "from_unstructured"
 
-    @pytest.fixture()
+    @pytest.fixture
     def expected(self, cube_mesh):
         return {
             "xs": cube_mesh.mesh.node_coords[0].points,
@@ -150,11 +150,11 @@ class TestMeshToPoly(ParentClass):
         ):
             cube_to_polydata(cube_mesh)
 
-    @pytest.fixture()
+    @pytest.fixture
     def operation(self):
         return "from_unstructured"
 
-    @pytest.fixture()
+    @pytest.fixture
     def cube(self, cube_mesh):
         return cube_mesh[0]
 
@@ -164,7 +164,7 @@ class TestMeshToPoly(ParentClass):
 
 
 class TestExtras:
-    @pytest.fixture()
+    @pytest.fixture
     def cube_1d_2d(self, cube_2d):
         my_cube = cube_2d.copy()
         lat_coord = my_cube.aux_coords[0]
