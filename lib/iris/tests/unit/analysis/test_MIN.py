3f523e8526adf2a78debaf7d7ae1287b7fa115a0
Henry Wright <henrywright@sky.com>
2025-08-13 12:25:08 +0100

    test_MIN.py

--- lib/iris/tests/unit/analysis/test_MIN.py
+++ lib/iris/tests/unit/analysis/test_MIN.py
@@ -4,21 +4,20 @@
 # See LICENSE in the root of the repository for full licensing details.
 """Unit tests for the :data:`iris.analysis.MIN` aggregator."""
 
-# Import iris.tests first so that some things can be initialised before
-# importing anything else.
-import iris.tests as tests  # isort:skip
-
 import numpy as np
 import numpy.ma as ma
+import pytest
 
 from iris._lazy_data import as_lazy_data, is_lazy_data
 from iris.analysis import MIN
 from iris.coords import DimCoord
 from iris.cube import Cube
+from iris.tests import _shared_utils
 
 
-class Test_basics(tests.IrisTest):
-    def setUp(self):
+class Test_basics:
+    @pytest.fixture(autouse=True)
+    def _setup(self):
         data = np.array([1, 2, 3, 4, 5])
         coord = DimCoord([6, 7, 8, 9, 10], long_name="foo")
         self.cube = Cube(data)
@@ -27,51 +26,49 @@ class Test_basics(tests.IrisTest):
         self.lazy_cube.add_dim_coord(coord, 0)
 
     def test_name(self):
-        self.assertEqual(MIN.name(), "minimum")
+        assert MIN.name() == "minimum"
 
     def test_collapse(self):
         data = MIN.aggregate(self.cube.data, axis=0)
-        self.assertArrayEqual(data, [1])
+        _shared_utils.assert_array_equal(data, [1])
 
     def test_lazy(self):
         lazy_data = MIN.lazy_aggregate(self.lazy_cube.lazy_data(), axis=0)
-        self.assertTrue(is_lazy_data(lazy_data))
+        assert is_lazy_data(lazy_data)
 
     def test_lazy_collapse(self):
         lazy_data = MIN.lazy_aggregate(self.lazy_cube.lazy_data(), axis=0)
-        self.assertArrayEqual(lazy_data.compute(), [1])
+        _shared_utils.assert_array_equal(lazy_data.compute(), [1])
 
 
-class Test_masked(tests.IrisTest):
-    def setUp(self):
+class Test_masked:
+    @pytest.fixture(autouse=True)
+    def _setup(self):
         self.cube = Cube(ma.masked_less([1, 2, 3, 4, 5], 3))
         self.cube.add_dim_coord(DimCoord([6, 7, 8, 9, 10], long_name="foo"), 0)
 
     def test_ma(self):
         data = MIN.aggregate(self.cube.data, axis=0)
-        self.assertArrayEqual(data, [3])
+        _shared_utils.assert_array_equal(data, [3])
 
 
-class Test_lazy_masked(tests.IrisTest):
-    def setUp(self):
+class Test_lazy_masked:
+    @pytest.fixture(autouse=True)
+    def _setup(self):
         masked_data = ma.masked_less([1, 2, 3, 4, 5], 3)
         self.cube = Cube(as_lazy_data(masked_data))
         self.cube.add_dim_coord(DimCoord([6, 7, 8, 9, 10], long_name="foo"), 0)
 
     def test_lazy_ma(self):
         lazy_data = MIN.lazy_aggregate(self.cube.lazy_data(), axis=0)
-        self.assertTrue(is_lazy_data(lazy_data))
-        self.assertArrayEqual(lazy_data.compute(), [3])
+        assert is_lazy_data(lazy_data)
+        _shared_utils.assert_array_equal(lazy_data.compute(), [3])
 
 
-class Test_aggregate_shape(tests.IrisTest):
+class Test_aggregate_shape:
     def test(self):
         shape = ()
         kwargs = dict()
-        self.assertTupleEqual(MIN.aggregate_shape(**kwargs), shape)
+        assert MIN.aggregate_shape(**kwargs) == shape
         kwargs = dict(wibble="wobble")
-        self.assertTupleEqual(MIN.aggregate_shape(**kwargs), shape)
-
-
-if __name__ == "__main__":
-    tests.main()
+        assert MIN.aggregate_shape(**kwargs) == shape
