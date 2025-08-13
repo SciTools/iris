5b324fb1d945ae4a293ac0f56ca042bfe19b583e
Elias Sadek <elias.sadek@metoffice.gov.uk>
2025-08-13 15:16:29 +0100

    edited test__shared_utils top thing

--- lib/iris/tests/unit/tests/test__shared_utils.py
+++ lib/iris/tests/unit/tests/test__shared_utils.py
@@ -2,7 +2,7 @@
 #
 # This file is part of Iris and is released under the BSD license.
 # See LICENSE in the root of the repository for full licensing details.
-"""Unit tests for the :mod:`iris.tests._shared_utils`."""
+"""Unit tests for :mod:`iris.tests._shared_utils`."""
 
 from abc import ABCMeta, abstractmethod
 
