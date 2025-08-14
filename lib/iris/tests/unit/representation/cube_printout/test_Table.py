fd1f359a674f1247fcd57c328dece46e685310fa
Martin Yeo <martin.yeo@metoffice.gov.uk>
2025-08-14 17:19:27 +0100

    Convert test_Table to PyTest.

--- lib/iris/tests/unit/representation/cube_printout/test_Table.py
+++ lib/iris/tests/unit/representation/cube_printout/test_Table.py
@@ -4,14 +4,16 @@
 # See LICENSE in the root of the repository for full licensing details.
 """Unit tests for :class:`iris._representation.cube_printout.Table`."""
 
+import pytest
+
 from iris._representation.cube_printout import Table
-import iris.tests as tests
 
 
-class TestTable(tests.IrisTest):
+class TestTable:
     # Note: this is just barely an independent definition, not *strictly* part
     # of CubePrinter, but effectively more-or-less so.
-    def setUp(self):
+    @pytest.fixture(autouse=True)
+    def _setup(self):
         table = Table()
         table.add_row(["one", "b", "three"], aligns=["left", "right", "left"])
         table.add_row(["a", "two", "c"], aligns=["right", "left", "right"])
@@ -19,27 +21,25 @@ class TestTable(tests.IrisTest):
 
     def test_empty(self):
         table = Table()
-        self.assertIsNone(table.n_columns)
-        self.assertEqual(len(table.rows), 0)
-        self.assertIsNone(table.col_widths)
+        assert table.n_columns is None
+        assert len(table.rows) == 0
+        assert table.col_widths is None
         # Check other methods : should be ok but do nothing.
         table.set_min_column_widths()  # Ok but does nothing.
-        self.assertIsNone(table.col_widths)
-        self.assertEqual(table.formatted_as_strings(), [])
-        self.assertEqual(str(table), "")
+        assert table.col_widths is None
+        assert table.formatted_as_strings() == []
+        assert str(table) == ""
 
     def test_basic_content(self):
         # Mirror the above 'empty' tests on a small basic table.
         table = self.simple_table
-        self.assertEqual(table.n_columns, 3)
-        self.assertEqual(len(table.rows), 2)
-        self.assertIsNone(table.col_widths)
+        assert table.n_columns == 3
+        assert len(table.rows) == 2
+        assert table.col_widths is None
         table.set_min_column_widths()  # Ok but does nothing.
-        self.assertEqual(table.col_widths, [3, 3, 5])
-        self.assertEqual(
-            table.formatted_as_strings(), ["one   b three", "  a two     c"]
-        )
-        self.assertEqual(str(table), "one   b three\n  a two     c")
+        assert table.col_widths == [3, 3, 5]
+        assert table.formatted_as_strings() == ["one   b three", "  a two     c"]
+        assert str(table) == "one   b three\n  a two     c"
 
     def test_copy(self):
         table = self.simple_table
@@ -48,40 +48,40 @@ class TestTable(tests.IrisTest):
         table.col_widths = [10, 15, 12]
         # Make the copy
         table2 = table.copy()
-        self.assertIsNot(table2, table)
-        self.assertNotEqual(table2, table)  # Note: equality is not implemented
+        assert table2 is not table
+        assert table2 != table  # Note: equality is not implemented
         # Check the parts match the original.
-        self.assertEqual(len(table2.rows), len(table.rows))
+        assert len(table2.rows) == len(table.rows)
         for row2, row in zip(table2.rows, table.rows):
-            self.assertEqual(row2.cols, row.cols)
-            self.assertEqual(row2.aligns, row.aligns)
-            self.assertEqual(row2.i_col_unlimited, row.i_col_unlimited)
+            assert row2.cols == row.cols
+            assert row2.aligns == row.aligns
+            assert row2.i_col_unlimited == row.i_col_unlimited
 
     def test_add_row(self):
         table = Table()
-        self.assertEqual(table.n_columns, None)
+        assert table.n_columns is None
         # Add one row.
         table.add_row(["one", "two", "three"], aligns=["left", "left", "left"])
-        self.assertEqual(len(table.rows), 1)
-        self.assertEqual(table.n_columns, 3)
-        self.assertIsNone(table.rows[0].i_col_unlimited)
+        assert len(table.rows) == 1
+        assert table.n_columns == 3
+        assert table.rows[0].i_col_unlimited is None
         # Second row ok.
         table.add_row(
             ["x", "y", "z"],
             aligns=["right", "right", "right"],
             i_col_unlimited=199,
         )
-        self.assertEqual(len(table.rows), 2)
-        self.assertEqual(table.rows[-1].i_col_unlimited, 199)
+        assert len(table.rows) == 2
+        assert table.rows[-1].i_col_unlimited == 199
 
         # Fails with bad number of columns
         regex = "columns.*!=.*existing"
-        with self.assertRaisesRegex(ValueError, regex):
+        with pytest.raises(ValueError, match=regex):
             table.add_row(["1", "2"], ["left", "right"])
 
         # Fails with bad number of aligns
         regex = "aligns.*!=.*col"
-        with self.assertRaisesRegex(ValueError, regex):
+        with pytest.raises(ValueError, match=regex):
             table.add_row(["1", "2", "3"], ["left", "left", "left", "left"])
 
     def test_formatted_as_strings(self):
@@ -92,17 +92,17 @@ class TestTable(tests.IrisTest):
         table.add_row(["123", "2", "3"], aligns)
 
         # Check that printing calculates default column widths, and result..
-        self.assertEqual(table.col_widths, None)
+        assert table.col_widths is None
         result = table.formatted_as_strings()
-        self.assertEqual(result, ["1   266 32", "123   2 3"])
-        self.assertEqual(table.col_widths, [3, 3, 2])
+        assert result == ["1   266 32", "123   2 3"]
+        assert table.col_widths == [3, 3, 2]
 
     def test_fail_bad_alignments(self):
         # Invalid 'aligns' content : only detected when printed
         table = Table()
         table.add_row(["1", "2", "3"], ["left", "right", "BAD"])
         regex = 'Unknown alignment "BAD"'
-        with self.assertRaisesRegex(ValueError, regex):
+        with pytest.raises(ValueError, match=regex):
             str(table)
 
     def test_table_set_width(self):
@@ -114,15 +114,14 @@ class TestTable(tests.IrisTest):
         table.add_row(["a", "b", "c"], aligns)
         table.add_row(["12345", "12345", "12345"], aligns)
         result = table.formatted_as_strings()
-        self.assertEqual(table.col_widths, [3, 3, 2])
-        self.assertEqual(
-            result,
-            [
-                "333 333 22",
-                "a     b c",
-                "12345 12345 12345",  # These are exceeding the given widths.
-            ],
-        )
+        assert table.col_widths == [3, 3, 2]
+
+        # These are exceeding the given widths.
+        assert result == [
+            "333 333 22",
+            "a     b c",
+            "12345 12345 12345",
+        ]
 
     def test_unlimited_column(self):
         table = Table()
@@ -131,16 +130,14 @@ class TestTable(tests.IrisTest):
         table.add_row(["abcd", "any-longer-stuff", "this"], aligns, i_col_unlimited=1)
         table.add_row(["12", "x", "yy"], aligns)
         result = table.formatted_as_strings()
-        self.assertEqual(
-            result,
-            [
-                "a    beee c",
-                "abcd any-longer-stuff this",
-                # NOTE: the widths-calc is ignoring cols 1-2, but
-                # entry#0 *is* extending the width of col#0
-                "12      x yy",
-            ],
-        )
+
+        # NOTE: the widths-calc is ignoring cols 1-2, but
+        # entry#0 *is* extending the width of col#0
+        assert result == [
+            "a    beee c",
+            "abcd any-longer-stuff this",
+            "12      x yy",
+        ]
 
     def test_str(self):
         # Check that str returns the formatted_as_strings() output.
@@ -150,8 +147,4 @@ class TestTable(tests.IrisTest):
         table.add_row(["1", "2", "3"], aligns=aligns)
         expected = "\n".join(table.formatted_as_strings())
         result = str(table)
-        self.assertEqual(result, expected)
-
-
-if __name__ == "__main__":
-    tests.main()
+        assert result == expected
