6208f4ca447cea34d0fa0958fd6f8aa00b9f23a7
Martin Yeo <martin.yeo@metoffice.gov.uk>
2025-08-20 15:56:48 +0100

    Protect util from elementwise NOT equality.

--- lib/iris/util.py
+++ lib/iris/util.py
@@ -233,7 +233,7 @@ def describe_diff(cube_a, cube_b, output_file=None):
     else:
         common_keys = set(cube_a.attributes).intersection(cube_b.attributes)
         for key in common_keys:
-            if np.any(cube_a.attributes[key] != cube_b.attributes[key]):
+            if hexdigest(cube_a.attributes[key]) != hexdigest(cube_b.attributes[key]):
                 output_file.write(
                     '"%s" cube_a attribute value "%s" is not '
                     "compatible with cube_b "
