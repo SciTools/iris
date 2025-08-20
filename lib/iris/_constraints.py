a036073f06f547bf51e9565b42cac6c2a1190c45
Martin Yeo <martin.yeo@metoffice.gov.uk>
2025-08-20 12:49:16 +0100

    Fix dumb Constraint eq mistake.

--- lib/iris/_constraints.py
+++ lib/iris/_constraints.py
@@ -532,9 +532,11 @@ class AttributeConstraint(Constraint):
         super().__init__(cube_func=self._cube_func)
 
     def __eq__(self, other):
-        hex_self = {(k, hexdigest(v)) for k, v in self._attributes.items()}
-        hex_other = {(k, hexdigest(v)) for k, v in other._attributes.items()}
-        eq = isinstance(other, AttributeConstraint) and hex_self == hex_other
+        eq = isinstance(other, AttributeConstraint)
+        if eq:
+            hex_self = {(k, hexdigest(v)) for k, v in self._attributes.items()}
+            hex_other = {(k, hexdigest(v)) for k, v in other._attributes.items()}
+            eq = hex_self == hex_other
         return eq
 
     def __hash__(self):
