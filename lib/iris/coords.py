Fix guess_bounds precision for circular coords at numpy v2

lib/iris/coords.py
@@ -2402,8 +2402,9 @@ class Coord(_DimensionalMetadata):
                 points = np.empty(self.shape[0] + 2)
                 points[1:-1] = self.points
                 direction = 1 if self.points[-1] > self.points[0] else -1
-                points[0] = self.points[-1] - (self.units.modulus * direction)
-                points[-1] = self.points[0] + (self.units.modulus * direction)
+                modulus = np.float64(self.units.modulus)
+                points[0] = self.points[-1] - (modulus * direction)
+                points[-1] = self.points[0] + (modulus * direction)
                 diffs = np.diff(points)
             else:
                 diffs = np.diff(self.points)