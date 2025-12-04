06f20fb137631f32c87e2029337dc3de5181300c
Elias Sadek <elias.sadek@metoffice.gov.uk>
2025-12-04 11:45:40 +0000

    converted build and add aux

--- lib/iris/tests/unit/fileformats/nc_load_rules/helpers/test_build_and_add_auxiliary_coordinate.py
+++ lib/iris/tests/unit/fileformats/nc_load_rules/helpers/test_build_and_add_auxiliary_coordinate.py
@@ -7,12 +7,7 @@ build_auxilliary_coordinate`.
 
 """
 
-# import iris tests first so that some things can be initialised before
-# importing anything else
-import iris.tests as tests  # isort:skip
-
 import contextlib
-from unittest import mock
 
 import numpy as np
 import pytest
@@ -24,9 +19,10 @@ from iris.exceptions import CannotAddError
 from iris.fileformats._nc_load_rules.helpers import build_and_add_auxiliary_coordinate
 from iris.fileformats.cf import CFVariable
 from iris.loading import LOAD_PROBLEMS
+from iris.tests.unit.fileformats.nc_load_rules.helpers import MockerMixin
 
 
-class TestBoundsVertexDim(tests.IrisTest):
+class TestBoundsVertexDim(MockerMixin):
     # Lookup for various tests (which change the dimension order).
     dim_names_lens = {
         "foo": 2,
@@ -37,19 +33,20 @@ class TestBoundsVertexDim(tests.IrisTest):
         "y": 3,
     }
 
-    def setUp(self):
+    @pytest.fixture(autouse=True)
+    def _setup(self, mocker):
         # Create coordinate cf variables and pyke engine.
         dimension_names = ("foo", "bar")
-        points, cf_data = self._make_array_and_cf_data(dimension_names)
+        points, cf_data = self._make_array_and_cf_data(mocker, dimension_names)
 
-        self.engine = mock.Mock(
-            cube=mock.Mock(),
-            cf_var=mock.Mock(dimensions=("foo", "bar"), cf_data=cf_data),
+        self.engine = mocker.Mock(
+            cube=mocker.Mock(),
+            cf_var=mocker.Mock(dimensions=("foo", "bar"), cf_data=cf_data),
             filename="DUMMY",
             cube_parts=dict(coordinates=[]),
         )
 
-        self.cf_coord_var = mock.Mock(
+        self.cf_coord_var = mocker.Mock(
             spec=CFVariable,
             dimensions=dimension_names,
             cf_name="wibble",
@@ -65,7 +62,7 @@ class TestBoundsVertexDim(tests.IrisTest):
         )
 
         expected_bounds, _ = self._make_array_and_cf_data(
-            dimension_names=("foo", "bar", "nv")
+            mocker, dimension_names=("foo", "bar", "nv")
         )
         self.expected_coord = AuxCoord(
             self.cf_coord_var[:],
@@ -83,7 +80,7 @@ class TestBoundsVertexDim(tests.IrisTest):
                     return var[keys]
             raise RuntimeError()
 
-        self.patch(
+        mocker.patch(
             "iris.fileformats.netcdf.NetCDFDataProxy.__getitem__",
             new=patched__getitem__,
         )
@@ -95,16 +92,16 @@ class TestBoundsVertexDim(tests.IrisTest):
             # Return the 'cf_bounds_var' created by the current test.
             return (self.cf_bounds_var, False)
 
-        self.patch(
+        mocker.patch(
             "iris.fileformats._nc_load_rules.helpers.get_cf_bounds_var",
             new=_get_per_test_bounds_var,
         )
 
     @classmethod
-    def _make_array_and_cf_data(cls, dimension_names, rollaxis=False):
+    def _make_array_and_cf_data(cls, mocker, dimension_names, rollaxis=False):
         shape = tuple(cls.dim_names_lens[name] for name in dimension_names)
-        cf_data = mock.MagicMock(_FillValue=None, spec=[])
-        cf_data.chunking = mock.MagicMock(return_value=shape)
+        cf_data = mocker.MagicMock(_FillValue=None, spec=[])
+        cf_data.chunking = mocker.MagicMock(return_value=shape)
         data = np.arange(np.prod(shape), dtype=float)
         if rollaxis:
             shape = shape[1:] + (shape[0],)
@@ -114,13 +111,13 @@ class TestBoundsVertexDim(tests.IrisTest):
             data = data.reshape(shape)
         return data, cf_data
 
-    def _make_cf_bounds_var(self, dimension_names, rollaxis=False):
+    def _make_cf_bounds_var(self, mocker, dimension_names, rollaxis=False):
         # Create the bounds cf variable.
         bounds, cf_data = self._make_array_and_cf_data(
-            dimension_names, rollaxis=rollaxis
+            mocker, dimension_names, rollaxis=rollaxis
         )
         bounds *= 1000  # Convert to metres.
-        cf_bounds_var = mock.Mock(
+        cf_bounds_var = self.mocker.Mock(
             spec=CFVariable,
             dimensions=dimension_names,
             cf_name="wibble_bnds",
@@ -137,7 +134,7 @@ class TestBoundsVertexDim(tests.IrisTest):
 
     def _check_case(self, dimension_names, rollaxis=False):
         self.cf_bounds_var = self._make_cf_bounds_var(
-            dimension_names, rollaxis=rollaxis
+            self.mocker, dimension_names, rollaxis=rollaxis
         )
 
         # Asserts must lie within context manager because of deferred loading.
@@ -148,7 +145,7 @@ class TestBoundsVertexDim(tests.IrisTest):
 
         # Test that engine.cube_parts container is correctly populated.
         expected_list = [(self.expected_coord, self.cf_coord_var.cf_name)]
-        self.assertEqual(self.engine.cube_parts["coordinates"], expected_list)
+        assert self.engine.cube_parts["coordinates"] == expected_list
 
     def test_fastest_varying_vertex_dim__normalise_bounds(self):
         # The usual order.
@@ -165,21 +162,22 @@ class TestBoundsVertexDim(tests.IrisTest):
         self._check_case(dimension_names=("x", "y", "nv"))
 
 
-class TestDtype(tests.IrisTest):
-    def setUp(self):
+class TestDtype(MockerMixin):
+    @pytest.fixture(autouse=True)
+    def _setup(self, mocker):
         # Create coordinate cf variables and pyke engine.
         points = np.arange(6).reshape(2, 3)
-        cf_data = mock.MagicMock(_FillValue=None)
-        cf_data.chunking = mock.MagicMock(return_value=points.shape)
+        cf_data = mocker.MagicMock(_FillValue=None)
+        cf_data.chunking = mocker.MagicMock(return_value=points.shape)
 
-        self.engine = mock.Mock(
-            cube=mock.Mock(),
-            cf_var=mock.Mock(dimensions=("foo", "bar")),
+        self.engine = mocker.Mock(
+            cube=mocker.Mock(),
+            cf_var=mocker.Mock(dimensions=("foo", "bar")),
             filename="DUMMY",
             cube_parts=dict(coordinates=[]),
         )
 
-        self.cf_coord_var = mock.Mock(
+        self.cf_coord_var = mocker.Mock(
             spec=CFVariable,
             dimensions=("foo", "bar"),
             cf_name="wibble",
@@ -202,13 +200,13 @@ class TestDtype(tests.IrisTest):
             raise RuntimeError()
 
         # Fix for deferred load, *AND* avoid loading small variable data in real arrays.
-        with mock.patch(
+        self.mocker.patch(
             "iris.fileformats.netcdf.NetCDFDataProxy.__getitem__",
             new=patched__getitem__,
-        ):
-            # While loading, "turn off" loading small variables as real data.
-            with mock.patch("iris.fileformats.netcdf.loader._LAZYVAR_MIN_BYTES", 0):
-                yield
+        )
+        # While loading, "turn off" loading small variables as real data.
+        self.mocker.patch("iris.fileformats.netcdf.loader._LAZYVAR_MIN_BYTES", 0)
+        yield
 
     def test_scale_factor_add_offset_int(self):
         self.cf_coord_var.scale_factor = 3
@@ -218,7 +216,7 @@ class TestDtype(tests.IrisTest):
             build_and_add_auxiliary_coordinate(self.engine, self.cf_coord_var)
 
         coord, _ = self.engine.cube_parts["coordinates"][0]
-        self.assertEqual(coord.dtype.kind, "i")
+        assert coord.dtype.kind == "i"
 
     def test_scale_factor_float(self):
         self.cf_coord_var.scale_factor = 3.0
@@ -227,7 +225,7 @@ class TestDtype(tests.IrisTest):
             build_and_add_auxiliary_coordinate(self.engine, self.cf_coord_var)
 
         coord, _ = self.engine.cube_parts["coordinates"][0]
-        self.assertEqual(coord.dtype.kind, "f")
+        assert coord.dtype.kind == "f"
 
     def test_add_offset_float(self):
         self.cf_coord_var.add_offset = 5.0
@@ -236,28 +234,29 @@ class TestDtype(tests.IrisTest):
             build_and_add_auxiliary_coordinate(self.engine, self.cf_coord_var)
 
         coord, _ = self.engine.cube_parts["coordinates"][0]
-        self.assertEqual(coord.dtype.kind, "f")
+        assert coord.dtype.kind == "f"
 
 
-class TestCoordConstruction(tests.IrisTest):
-    def setUp(self):
+class TestCoordConstruction:
+    @pytest.fixture(autouse=True)
+    def _setup(self, mocker):
         # Create dummy pyke engine.
-        self.engine = mock.Mock(
-            cube=mock.Mock(),
-            cf_var=mock.Mock(dimensions=("foo", "bar")),
+        self.engine = mocker.Mock(
+            cube=mocker.Mock(),
+            cf_var=mocker.Mock(dimensions=("foo", "bar")),
             filename="DUMMY",
             cube_parts=dict(coordinates=[]),
         )
 
         points = np.arange(6)
         units = "days since 1970-01-01"
-        self.cf_coord_var = mock.Mock(
+        self.cf_coord_var = mocker.Mock(
             spec=CFVariable,
             dimensions=("foo",),
             scale_factor=1,
             add_offset=0,
             cf_name="wibble",
-            cf_data=mock.MagicMock(chunking=mock.Mock(return_value=None), spec=[]),
+            cf_data=mocker.MagicMock(chunking=mocker.Mock(return_value=None), spec=[]),
             filename=self.engine.filename,
             standard_name=None,
             long_name="wibble",
@@ -270,13 +269,13 @@ class TestCoordConstruction(tests.IrisTest):
         )
 
         bounds = np.arange(12).reshape(6, 2)
-        cf_data = mock.MagicMock(chunking=mock.Mock(return_value=None))
+        cf_data = mocker.MagicMock(chunking=mocker.Mock(return_value=None))
         # we want to mock the absence of flag attributes to helpers.get_attr_units
         # see https://docs.python.org/3/library/unittest.mock.html#deleting-attributes
         del cf_data.flag_values
         del cf_data.flag_masks
         del cf_data.flag_meanings
-        self.cf_bounds_var = mock.Mock(
+        self.cf_bounds_var = mocker.Mock(
             spec=CFVariable,
             dimensions=("x", "nv"),
             scale_factor=1,
@@ -300,7 +299,7 @@ class TestCoordConstruction(tests.IrisTest):
                     return var[keys]
             raise RuntimeError()
 
-        self.patch(
+        mocker.patch(
             "iris.fileformats.netcdf.NetCDFDataProxy.__getitem__",
             new=patched__getitem__,
         )
@@ -312,7 +311,7 @@ class TestCoordConstruction(tests.IrisTest):
         def get_cf_bounds_var(coord_var):
             return self.cf_bounds_var, self.use_climatology_bounds
 
-        self.patch(
+        mocker.patch(
             "iris.fileformats._nc_load_rules.helpers.get_cf_bounds_var",
             new=get_cf_bounds_var,
         )
@@ -368,7 +367,7 @@ class TestCoordConstruction(tests.IrisTest):
         # Test that expected coord is built and added to cube.
         self.engine.cube.add_aux_coord.assert_called_with(expected_coord, [0])
 
-    def test_bad_coord_system(self):
+    def test_bad_coord_system(self, mocker):
         coord_system = RotatedGeogCS(
             grid_north_pole_latitude=45.0, grid_north_pole_longitude=45.0
         )
@@ -382,19 +381,16 @@ class TestCoordConstruction(tests.IrisTest):
             else:
                 self._metadata_manager.coord_system = value
 
-        with mock.patch.object(
+        mocker.patch.object(
             AuxCoord,
             "coord_system",
             new=property(AuxCoord.coord_system.fget, mock_setter),
-        ):
-            build_and_add_auxiliary_coordinate(
-                self.engine, self.cf_coord_var, coord_system=coord_system
-            )
-            load_problem = LOAD_PROBLEMS.problems[-1]
-            self.assertIn(
-                "test_bad_coord_system",
-                "".join(load_problem.stack_trace.format()),
-            )
+        )
+        build_and_add_auxiliary_coordinate(
+            self.engine, self.cf_coord_var, coord_system=coord_system
+        )
+        load_problem = LOAD_PROBLEMS.problems[-1]
+        assert "test_bad_coord_system" in "".join(load_problem.stack_trace.format())
 
     def test_not_added(self):
         # Confirm that the coord will be skipped if a CannotAddError is raised
@@ -418,7 +414,7 @@ class TestCoordConstruction(tests.IrisTest):
             m.setattr(self.engine, "cube", "foo")
             n_problems = len(LOAD_PROBLEMS.problems)
             build_and_add_auxiliary_coordinate(self.engine, self.cf_coord_var)
-            self.assertTrue(len(LOAD_PROBLEMS.problems) > n_problems)
+            assert len(LOAD_PROBLEMS.problems) > n_problems
 
         assert self.engine.cube_parts["coordinates"] == []
 
@@ -433,7 +429,3 @@ class TestCoordConstruction(tests.IrisTest):
             assert destination.identifier == self.engine.cf_var.cf_name
 
         assert self.engine.cube_parts["coordinates"] == []
-
-
-if __name__ == "__main__":
-    tests.main()
