3f8a33bf437d1a90611c008019510fa0ac1b13f4
Elias <110238618+ESadek-MO@users.noreply.github.com>
2025-12-05 17:05:04 +0000

    Converted integration/root tests to pytest  (#6840)
    * converted test_climatology
    
    * fixed climatology tmp_path
    
    * converted test_cube
    
    * converted test_Datums
    
    * converted test_ff
    
    * ruff test_mask_cube
    
    * ruff test_netcdf__
    
    * converted test_new_axis
    
    * converted test_PartialDateTime
    
    * converted test_pickle
    
    * converted test_pp
    
    * converted test_pp_constrained
    
    * converted test_regrid_equ
    
    * mostly converted test_regridding
    
    * converted test_subset
    
    * converted test_traj
    
    * review comments (minus one)
    
    * fixed cml asserts
    
    * refreshed cmls

--- lib/iris/tests/integration/test_cube.py
+++ lib/iris/tests/integration/test_cube.py
@@ -4,30 +4,24 @@
 # See LICENSE in the root of the repository for full licensing details.
 """Integration tests for :class:`iris.cube.Cube`."""
 
-# Import iris.tests first so that some things can be initialised before
-# importing anything else.
-import iris.tests as tests  # isort:skip
-
-from unittest import mock
-
 import numpy as np
 
 import iris
 from iris._lazy_data import as_lazy_data, is_lazy_data
 from iris.analysis import MEAN
 from iris.cube import Cube
+from iris.tests import _shared_utils
 
 
-class Test_aggregated_by(tests.IrisTest):
-    @tests.skip_data
-    def test_agg_by_aux_coord(self):
-        problem_test_file = tests.get_data_path(
+class Test_aggregated_by:
+    @_shared_utils.skip_data
+    def test_agg_by_aux_coord(self, mocker):
+        problem_test_file = _shared_utils.get_data_path(
             ("NetCDF", "testing", "small_theta_colpex.nc")
         )
         # While loading, "turn off" loading small variables as real data.
-        with mock.patch("iris.fileformats.netcdf.loader._LAZYVAR_MIN_BYTES", 0):
-            cube = iris.load_cube(problem_test_file, "air_potential_temperature")
-
+        mocker.patch("iris.fileformats.netcdf.loader._LAZYVAR_MIN_BYTES", 0)
+        cube = iris.load_cube(problem_test_file, "air_potential_temperature")
         # Test aggregating by aux coord, notably the `forecast_period` aux
         # coord on `cube`, whose `_points` attribute is a lazy array.
         # This test then ensures that aggregating using `points` instead is
@@ -38,29 +32,25 @@ class Test_aggregated_by(tests.IrisTest):
         # triggered the load of the coordinate's data.
         forecast_period_coord = cube.coord("forecast_period")
 
-        self.assertTrue(is_lazy_data(forecast_period_coord.core_points()))
+        assert is_lazy_data(forecast_period_coord.core_points())
 
         # Now confirm we can aggregate along this coord.
         res_cube = cube.aggregated_by("forecast_period", MEAN)
         res_cell_methods = res_cube.cell_methods[0]
-        self.assertEqual(res_cell_methods.coord_names, ("forecast_period",))
-        self.assertEqual(res_cell_methods.method, "mean")
+        assert res_cell_methods.coord_names == ("forecast_period",)
+        assert res_cell_methods.method == "mean"
 
 
-class TestDataFillValue(tests.IrisTest):
+class TestDataFillValue:
     def test_real(self):
         data = np.ma.masked_array([1, 2, 3], [0, 1, 0], fill_value=10)
         cube = Cube(data)
         cube.data.fill_value = 20
-        self.assertEqual(cube.data.fill_value, 20)
+        assert cube.data.fill_value == 20
 
     def test_lazy(self):
         data = np.ma.masked_array([1, 2, 3], [0, 1, 0], fill_value=10)
         data = as_lazy_data(data)
         cube = Cube(data)
         cube.data.fill_value = 20
-        self.assertEqual(cube.data.fill_value, 20)
-
-
-if __name__ == "__main__":
-    tests.main()
+        assert cube.data.fill_value == 20
--- lib/iris/tests/integration/test_pp_constrained_load_cubes.py
+++ lib/iris/tests/integration/test_pp_constrained_load_cubes.py
@@ -4,44 +4,37 @@
 # See LICENSE in the root of the repository for full licensing details.
 """Integration tests for :func:`iris.fileformats.rules.load_cubes`."""
 
-# Import iris.tests first so that some things can be initialised before
-# importing anything else.
-import iris.tests as tests  # isort:skip
-
 import iris
 from iris.fileformats import pp
 from iris.fileformats.pp_load_rules import convert
 from iris.fileformats.rules import load_cubes
+from iris.tests import _shared_utils
 
 
-class Test(tests.IrisTest):
-    @tests.skip_data
+class Test:
+    @_shared_utils.skip_data
     def test_pp_with_stash_constraint(self):
-        filenames = [tests.get_data_path(("PP", "globClim1", "dec_subset.pp"))]
+        filenames = [_shared_utils.get_data_path(("PP", "globClim1", "dec_subset.pp"))]
         stcon = iris.AttributeConstraint(STASH="m01s00i004")
         pp_constraints = pp._convert_constraints(stcon)
         pp_loader = iris.fileformats.rules.Loader(pp.load, {}, convert)
         cubes = list(load_cubes(filenames, None, pp_loader, pp_constraints))
-        self.assertEqual(len(cubes), 38)
+        assert len(cubes) == 38
 
-    @tests.skip_data
+    @_shared_utils.skip_data
     def test_pp_with_stash_constraints(self):
-        filenames = [tests.get_data_path(("PP", "globClim1", "dec_subset.pp"))]
+        filenames = [_shared_utils.get_data_path(("PP", "globClim1", "dec_subset.pp"))]
         stcon1 = iris.AttributeConstraint(STASH="m01s00i004")
         stcon2 = iris.AttributeConstraint(STASH="m01s00i010")
         pp_constraints = pp._convert_constraints([stcon1, stcon2])
         pp_loader = iris.fileformats.rules.Loader(pp.load, {}, convert)
         cubes = list(load_cubes(filenames, None, pp_loader, pp_constraints))
-        self.assertEqual(len(cubes), 76)
+        assert len(cubes) == 76
 
-    @tests.skip_data
+    @_shared_utils.skip_data
     def test_pp_no_constraint(self):
-        filenames = [tests.get_data_path(("PP", "globClim1", "dec_subset.pp"))]
+        filenames = [_shared_utils.get_data_path(("PP", "globClim1", "dec_subset.pp"))]
         pp_constraints = pp._convert_constraints(None)
         pp_loader = iris.fileformats.rules.Loader(pp.load, {}, convert)
         cubes = list(load_cubes(filenames, None, pp_loader, pp_constraints))
-        self.assertEqual(len(cubes), 152)
-
-
-if __name__ == "__main__":
-    tests.main()
+        assert len(cubes) == 152
