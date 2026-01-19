38dd5bbe943fd2bf84fa3d91569f987a562d669c
Elias Sadek <elias.sadek@metoffice.gov.uk>
2026-01-19 11:06:14 +0000

    actioned test_ArakawaC

--- lib/iris/tests/unit/fileformats/ff/test_ArakawaC.py
+++ lib/iris/tests/unit/fileformats/ff/test_ArakawaC.py
@@ -4,22 +4,19 @@
 # See LICENSE in the root of the repository for full licensing details.
 """Unit tests for :class:`iris.fileformat.ff.ArakawaC`."""
 
-# Import iris.tests first so that some things can be initialised before
-# importing anything else.
-import iris.tests as tests  # isort:skip
-
 import numpy as np
 
 from iris.fileformats._ff import ArakawaC
+from iris.tests import _shared_utils
 
 
-class Test__x_vectors(tests.IrisTest):
+class Test__x_vectors:
     def _test(self, column, horiz_grid_type, xp, xu):
         reals = np.arange(6) + 100
         grid = ArakawaC(column, None, reals, horiz_grid_type)
         result_xp, result_xu = grid._x_vectors()
-        self.assertArrayEqual(result_xp, xp)
-        self.assertArrayEqual(result_xu, xu)
+        _shared_utils.assert_array_equal(result_xp, xp)
+        _shared_utils.assert_array_equal(result_xu, xu)
 
     def test_none(self):
         self._test(column=None, horiz_grid_type=None, xp=None, xu=None)
@@ -49,12 +46,12 @@ class Test__x_vectors(tests.IrisTest):
         )
 
 
-class Test_regular_x(tests.IrisTest):
+class Test_regular_x:
     def _test(self, subgrid, bzx, bdx):
         grid = ArakawaC(None, None, [4.0, None, None, -5.0, None, None], None)
         result_bzx, result_bdx = grid.regular_x(subgrid)
-        self.assertEqual(result_bzx, bzx)
-        self.assertEqual(result_bdx, bdx)
+        assert result_bzx == bzx
+        assert result_bdx == bdx
 
     def test_theta_subgrid(self):
         self._test(1, -9.0, 4.0)
@@ -63,13 +60,13 @@ class Test_regular_x(tests.IrisTest):
         self._test(11, -7.0, 4.0)
 
 
-class Test_regular_y(tests.IrisTest):
+class Test_regular_y:
     def _test(self, v_offset, subgrid, bzy, bdy):
         grid = ArakawaC(None, None, [None, 4.0, 45.0, None, None, None], None)
         grid._v_offset = v_offset
         result_bzy, result_bdy = grid.regular_y(subgrid)
-        self.assertEqual(result_bzy, bzy)
-        self.assertEqual(result_bdy, bdy)
+        assert result_bzy == bzy
+        assert result_bdy == bdy
 
     def test_theta_subgrid_NewDynamics(self):
         self._test(0.5, 1, 41.0, 4.0)
@@ -82,7 +79,3 @@ class Test_regular_y(tests.IrisTest):
 
     def test_v_subgrid_ENDGame(self):
         self._test(-0.5, 11, 39.0, 4.0)
-
-
-if __name__ == "__main__":
-    tests.main()
