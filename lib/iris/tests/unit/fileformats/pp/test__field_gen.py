dc4cd76f9e9a6d7d7136bfd5422792f702d31f61
Elias <110238618+ESadek-MO@users.noreply.github.com>
2026-02-06 10:12:29 +0000

    Converted fileformats/pp to pytest (#6904)
    * converted __convert_constraints
    
    * converted __create_field_data
    
    * converted __data_bytes...
    
    * test__field_gen
    
    * test__interpret
    
    * test_as_fields
    
    * test_load
    
    * test_PPData
    
    * test_PPField
    
    * test_save
    
    * test_save_fields
    
    * test_save_pairs
    
    * removed redundant import
    
    * PT fixes
    
    * fixed mocker patch cm

--- lib/iris/tests/unit/fileformats/pp/test__field_gen.py
+++ lib/iris/tests/unit/fileformats/pp/test__field_gen.py
@@ -4,75 +4,70 @@
 # See LICENSE in the root of the repository for full licensing details.
 """Unit tests for the `iris.fileformats.pp._field_gen` function."""
 
-# Import iris.tests first so that some things can be initialised before
-# importing anything else.
-import iris.tests as tests  # isort:skip
-
 import contextlib
 import io
-from unittest import mock
-import warnings
 
 import numpy as np
+import pytest
 
 import iris.fileformats.pp as pp
 
 
-class Test(tests.IrisTest):
-    @contextlib.contextmanager
-    def mock_for_field_gen(self, fields):
-        side_effect_fields = list(fields)[:]
+class Test:
+    @pytest.fixture
+    def mock_for_field_gen(self, mocker):
+        @contextlib.contextmanager
+        def _mock_for_field_gen(fields):
+            side_effect_fields = list(fields)[:]
 
-        def make_pp_field_override(*args):
-            # Iterates over the fields passed to this context manager,
-            # until there are no more, upon which the np.fromfile
-            # returns an empty list and the while loop in load() is
-            # broken.
-            result = side_effect_fields.pop(0)
-            if not side_effect_fields:
-                np.fromfile.return_value = []
-            return result
+            def make_pp_field_override(*args):
+                # Iterates over the fields passed to this context manager,
+                # until there are no more, upon which the np.fromfile
+                # returns an empty list and the while loop in load() is
+                # broken.
+                result = side_effect_fields.pop(0)
+                if not side_effect_fields:
+                    np.fromfile.return_value = []
+                return result
 
-        open_func = "builtins.open"
-        with (
-            mock.patch("numpy.fromfile", return_value=[0]),
-            mock.patch(open_func),
-            mock.patch("struct.unpack_from", return_value=[4]),
-            mock.patch(
+            open_func = "builtins.open"
+            mocker.patch("numpy.fromfile", return_value=[0])
+            mocker.patch(open_func)
+            mocker.patch("struct.unpack_from", return_value=[4])
+            mocker.patch(
                 "iris.fileformats.pp.make_pp_field",
                 side_effect=make_pp_field_override,
-            ),
-        ):
+            )
             yield
 
-    def gen_fields(self, fields):
-        with self.mock_for_field_gen(fields):
+        return _mock_for_field_gen
+
+    def gen_fields(self, fields, mock_for_field_gen):
+        with mock_for_field_gen(fields):
             return list(pp._field_gen("mocked", "mocked"))
 
-    def test_lblrec_invalid(self):
-        pp_field = mock.Mock(lblrec=2, lbext=0)
-        with warnings.catch_warnings(record=True) as warn:
-            warnings.simplefilter("always")
-            self.gen_fields([pp_field])
-        self.assertEqual(len(warn), 1)
+    def test_lblrec_invalid(self, mocker, mock_for_field_gen):
+        pp_field = mocker.Mock(lblrec=2, lbext=0)
         wmsg = (
             "LBLREC has a different value to the .* the header in the "
             r"file \(8 and 4\)\. Skipping .*"
         )
-        self.assertRegex(str(warn[0].message), wmsg)
+        with pytest.warns(UserWarning, match=wmsg) as warn:
+            self.gen_fields([pp_field], mock_for_field_gen)
+        assert len(warn) == 1
 
-    def test_read_headers_call(self):
+    def test_read_headers_call(self, mocker, mock_for_field_gen):
         # Checks that the two calls to np.fromfile are called in the
         # expected way.
-        pp_field = mock.Mock(lblrec=1, lbext=0, lbuser=[0])
-        with self.mock_for_field_gen([pp_field]):
-            open_fh = mock.MagicMock(spec=io.RawIOBase)
+        pp_field = mocker.Mock(lblrec=1, lbext=0, lbuser=[0])
+        with mock_for_field_gen([pp_field]):
+            open_fh = mocker.MagicMock(spec=io.RawIOBase)
             open.return_value = open_fh
             next(pp._field_gen("mocked", read_data_bytes=False))
             with open_fh as open_fh_ctx:
                 calls = [
-                    mock.call(open_fh_ctx, count=45, dtype=">i4"),
-                    mock.call(open_fh_ctx, count=19, dtype=">f4"),
+                    mocker.call(open_fh_ctx, count=45, dtype=">i4"),
+                    mocker.call(open_fh_ctx, count=19, dtype=">f4"),
                 ]
             np.fromfile.assert_has_calls(calls)
         with open_fh as open_fh_ctx:
@@ -82,33 +77,30 @@ class Test(tests.IrisTest):
                 4,
                 np.dtype(">f4"),
             )
-        self.assertEqual(pp_field.data, expected_deferred_bytes)
+        assert pp_field.data == expected_deferred_bytes
 
-    def test_read_data_call(self):
+    def test_read_data_call(self, mocker, mock_for_field_gen):
         # Checks that data is read if read_data is True.
-        pp_field = mock.Mock(lblrec=1, lbext=0, lbuser=[0])
-        with self.mock_for_field_gen([pp_field]):
-            open_fh = mock.MagicMock(spec=io.RawIOBase)
+        pp_field = mocker.Mock(lblrec=1, lbext=0, lbuser=[0])
+        with mock_for_field_gen([pp_field]):
+            open_fh = mocker.MagicMock(spec=io.RawIOBase)
             open.return_value = open_fh
             next(pp._field_gen("mocked", read_data_bytes=True))
         with open_fh as open_fh_ctx:
             expected_loaded_bytes = pp.LoadedArrayBytes(
                 open_fh_ctx.read(), np.dtype(">f4")
             )
-        self.assertEqual(pp_field.data, expected_loaded_bytes)
+        assert pp_field.data == expected_loaded_bytes
 
-    def test_invalid_header_release(self):
+    def test_invalid_header_release(self, tmp_path):
         # Check that an unknown LBREL value just results in a warning
         # and the end of the file iteration instead of raising an error.
-        with self.temp_filename() as temp_path:
-            np.zeros(65, dtype="i4").tofile(temp_path)
-            generator = pp._field_gen(temp_path, False)
-            with mock.patch("warnings.warn") as warn:
-                with self.assertRaises(StopIteration):
-                    next(generator)
-            self.assertEqual(warn.call_count, 1)
-            self.assertIn("header release number", warn.call_args[0][0])
-
-
-if __name__ == "__main__":
-    tests.main()
+        temp_path = tmp_path / "temp"
+        np.zeros(65, dtype="i4").tofile(temp_path)
+        generator = pp._field_gen(temp_path, False)
+        with pytest.warns(
+            pp._WarnComboIgnoringLoad, match="header release number"
+        ) as warn:
+            with pytest.raises(StopIteration):
+                next(generator)
+        assert len(warn) == 1
--- lib/iris/tests/unit/fileformats/pp/test__interpret_field.py
+++ lib/iris/tests/unit/fileformats/pp/test__interpret_field.py
@@ -4,36 +4,34 @@
 # See LICENSE in the root of the repository for full licensing details.
 """Unit tests for the `iris.fileformats.pp._interpret_field` function."""
 
-# Import iris.tests first so that some things can be initialised before
-# importing anything else.
-import iris.tests as tests  # isort:skip
-
 from copy import deepcopy
-from unittest import mock
 
 import numpy as np
+import pytest
 
 import iris.fileformats.pp as pp
+from iris.warnings import IrisLoadWarning
 
 
-class Test__interpret_fields__land_packed_fields(tests.IrisTest):
-    def setUp(self):
+class Test__interpret_fields__land_packed_fields:
+    @pytest.fixture(autouse=True)
+    def _setup(self, mocker):
         return_value = ("dummy", 0, 0, np.dtype("f4"))
-        core_data = mock.MagicMock(return_value=return_value)
+        core_data = mocker.MagicMock(return_value=return_value)
         # A field packed using a land/sea mask.
-        self.pp_field = mock.Mock(
+        self.pp_field = mocker.Mock(
             lblrec=1,
             lbext=0,
             lbuser=[0] * 7,
             lbrow=0,
             lbnpt=0,
             raw_lbpack=21,
-            lbpack=mock.Mock(n1=0, n2=2, n3=1),
+            lbpack=mocker.Mock(n1=0, n2=2, n3=1),
             core_data=core_data,
         )
         # The field specifying the land/seamask.
         lbuser = [None, None, None, 30, None, None, 1]  # m01s00i030
-        self.land_mask_field = mock.Mock(
+        self.land_mask_field = mocker.Mock(
             lblrec=1,
             lbext=0,
             lbuser=lbuser,
@@ -47,33 +45,29 @@ class Test__interpret_fields__land_packed_fields(tests.IrisTest):
         # Checks the fix_lbrow_lbnpt is applied to fields which are not
         # deferred.
         f1, mask = self.pp_field, self.land_mask_field
-        self.assertEqual(f1.lbrow, 0)
-        self.assertEqual(f1.lbnpt, 0)
+        assert f1.lbrow == 0
+        assert f1.lbnpt == 0
         list(pp._interpret_fields([mask, f1]))
-        self.assertEqual(f1.lbrow, 3)
-        self.assertEqual(f1.lbnpt, 4)
+        assert f1.lbrow == 3
+        assert f1.lbnpt == 4
         # Check the data's shape has been updated too.
-        self.assertEqual(f1.data.shape, (3, 4))
+        assert f1.data.shape == (3, 4)
 
     def test_fix_lbrow_lbnpt_no_mask_available(self):
         # Check a warning is issued when loading a land masked field
         # without a land mask.
-        with mock.patch("warnings.warn") as warn:
+        with pytest.warns(
+            IrisLoadWarning,
+            match="Landmask compressed fields existed without a landmask",
+        ) as warn:
             list(pp._interpret_fields([self.pp_field]))
-        self.assertEqual(warn.call_count, 1)
-        warn_msg = warn.call_args[0][0]
-        self.assertTrue(
-            warn_msg.startswith(
-                "Landmask compressed fields existed without a landmask"
-            ),
-            "Unexpected warning message: {!r}".format(warn_msg),
-        )
+        assert len(warn) == 1
 
     def test_deferred_mask_field(self):
         # Check that the order of the load is yielded last if the mask
         # hasn't yet been seen.
         result = list(pp._interpret_fields([self.pp_field, self.land_mask_field]))
-        self.assertEqual(result, [self.land_mask_field, self.pp_field])
+        assert result == [self.land_mask_field, self.pp_field]
 
     def test_not_deferred_mask_field(self):
         # Check that the order of the load is unchanged if a land mask
@@ -81,15 +75,11 @@ class Test__interpret_fields__land_packed_fields(tests.IrisTest):
         f1, mask = self.pp_field, self.land_mask_field
         mask2 = deepcopy(mask)
         result = list(pp._interpret_fields([mask, f1, mask2]))
-        self.assertEqual(result, [mask, f1, mask2])
+        assert result == [mask, f1, mask2]
 
     def test_deferred_fix_lbrow_lbnpt(self):
         # Check the fix is also applied to fields which are deferred.
         f1, mask = self.pp_field, self.land_mask_field
         list(pp._interpret_fields([f1, mask]))
-        self.assertEqual(f1.lbrow, 3)
-        self.assertEqual(f1.lbnpt, 4)
-
-
-if __name__ == "__main__":
-    tests.main()
+        assert f1.lbrow == 3
+        assert f1.lbnpt == 4
