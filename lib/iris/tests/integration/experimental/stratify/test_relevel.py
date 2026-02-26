6afa3c9c04522c17a2fdfe1ea2fa666147daa3da
Elias Sadek <elias.sadek@metoffice.gov.uk>
2026-02-26 15:34:19 +0000

    stratify

--- lib/iris/tests/integration/experimental/stratify/test_relevel.py
+++ lib/iris/tests/integration/experimental/stratify/test_relevel.py
@@ -2,19 +2,29 @@
 #
 # This file is part of Iris and is released under the BSD license.
 # See LICENSE in the root of the repository for full licensing details.
-"""Unit tests for the :func:`iris.experimental.stratify.relevel` function."""
+"""Integration tests for the :func:`iris.experimental.stratify.relevel` function."""
+
+from functools import partial
 
 import numpy as np
 import pytest
 
 from iris.coords import AuxCoord, DimCoord
-from iris.experimental.stratify import relevel
+from iris.tests import _shared_utils
 import iris.tests.stock as stock
 
+try:
+    import stratify
 
+    from iris.experimental.stratify import relevel
+except ImportError:
+    stratify = None
+
+
+@_shared_utils.skip_stratify
 class Test:
     @pytest.fixture(autouse=True)
-    def _setup(self, mocker):
+    def _setup(self):
         cube = stock.simple_3d()[:, :1, :1]
         #: The data from which to get the levels.
         self.src_levels = cube.copy()
@@ -24,10 +34,6 @@ class Test:
         self.cube *= 10
         self.coord = self.src_levels.coord("wibble")
         self.axes = (self.coord, self.coord.name(), None, 0)
-        self.patch_interpolate = mocker.patch(
-            "iris.experimental.stratify.stratify.interpolate"
-        )
-        self.patch_interpolate.return_value = np.ones((3, 1, 1))
 
     def test_broadcast_fail_src_levels(self):
         emsg = "Cannot broadcast the cube and src_levels"
@@ -44,9 +50,25 @@ class Test:
     def test_standard_input(self):
         for axis in self.axes:
             result = relevel(self.cube, self.src_levels, [-1, 0, 5.5], axis=axis)
+            _shared_utils.assert_array_equal(
+                result.data.flatten(), np.array([np.nan, 0, 55])
+            )
             expected = DimCoord([-1, 0, 5.5], units=1, long_name="thingness")
             assert expected == result.coord("thingness")
-            self.patch_interpolate.assert_called()
+
+    def test_non_monotonic(self):
+        for axis in self.axes:
+            result = relevel(self.cube, self.src_levels, [2, 3, 2], axis=axis)
+            _shared_utils.assert_array_equal(
+                result.data.flatten(), np.array([20, 30, np.nan])
+            )
+            expected = AuxCoord([2, 3, 2], units=1, long_name="thingness")
+            assert result.coord("thingness") == expected
+
+    def test_static_level(self):
+        for axis in self.axes:
+            result = relevel(self.cube, self.src_levels, [2, 2], axis=axis)
+            _shared_utils.assert_array_equal(result.data.flatten(), np.array([20, 20]))
 
     def test_coord_input(self):
         source = AuxCoord(self.src_levels.data)
@@ -58,20 +80,37 @@ class Test:
         for axis in self.axes:
             result = relevel(self.cube, source, [0, 12, 13], axis=axis)
             assert result.shape == (3, 1, 1)
-        self.patch_interpolate.assert_called()
+            _shared_utils.assert_array_equal(result.data.flatten(), [0, 120, np.nan])
 
-    def test_custom_interpolator(self, mocker):
-        mock_interpolate = mocker.Mock()
-        mock_interpolate.return_value = np.ones((3, 1, 1))
-
-        interpolator = mock_interpolate
+    def test_custom_interpolator(self):
+        interpolator = partial(stratify.interpolate, interpolation="nearest")
 
         for axis in self.axes:
-            _ = relevel(
+            result = relevel(
                 self.cube,
                 self.src_levels,
                 [-1, 0, 6.5],
                 axis=axis,
                 interpolator=interpolator,
             )
-            mock_interpolate.assert_called()
+            _shared_utils.assert_array_equal(
+                result.data.flatten(), np.array([np.nan, 0, 120])
+            )
+
+    def test_multi_dim_target_levels(self, request):
+        interpolator = partial(
+            stratify.interpolate,
+            interpolation="linear",
+            extrapolation="linear",
+        )
+
+        for axis in self.axes:
+            result = relevel(
+                self.cube,
+                self.src_levels,
+                self.src_levels.data,
+                axis=axis,
+                interpolator=interpolator,
+            )
+            _shared_utils.assert_array_equal(result.data.flatten(), np.array([0, 120]))
+            _shared_utils.assert_CML(request, result)
