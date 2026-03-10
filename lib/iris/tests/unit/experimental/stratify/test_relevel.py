b2722790a5e9ea0013932a3d6e704adbaa024752
Elias Sadek <elias.sadek@metoffice.gov.uk>
2026-03-10 15:07:30 +0000

    stratify and extras

--- lib/iris/tests/unit/experimental/stratify/test_relevel.py
+++ lib/iris/tests/unit/experimental/stratify/test_relevel.py
@@ -4,12 +4,28 @@
 # See LICENSE in the root of the repository for full licensing details.
 """Unit tests for the :func:`iris.experimental.stratify.relevel` function."""
 
+import sys
+import types
+
 import numpy as np
 import pytest
 
 from iris.coords import AuxCoord, DimCoord
-from iris.experimental.stratify import relevel
 import iris.tests.stock as stock
+
+
+@pytest.fixture(autouse=True, scope="module")
+def fake_stratify():
+    try:
+        import stratify
+    except:
+        fake = types.ModuleType("stratify")
+        fake.interpolate = lambda *a, **k: None
+        sys.modules["stratify"] = fake
+        yield
+    finally:
+        # Remove fake after tests in this module complete
+        sys.modules.pop("stratify", None)
 
 
 class Test:
@@ -30,18 +46,24 @@ class Test:
         self.patch_interpolate.return_value = np.ones((3, 1, 1))
 
     def test_broadcast_fail_src_levels(self):
+        from iris.experimental.stratify import relevel
+
         emsg = "Cannot broadcast the cube and src_levels"
         data = np.arange(60).reshape(3, 4, 5)
         with pytest.raises(ValueError, match=emsg):
             relevel(self.cube, AuxCoord(data), [1, 2, 3])
 
     def test_broadcast_fail_tgt_levels(self):
+        from iris.experimental.stratify import relevel
+
         emsg = "Cannot broadcast the cube and tgt_levels"
         data = np.arange(60).reshape(3, 4, 5)
         with pytest.raises(ValueError, match=emsg):
             relevel(self.cube, self.coord, data)
 
     def test_standard_input(self):
+        from iris.experimental.stratify import relevel
+
         for axis in self.axes:
             result = relevel(self.cube, self.src_levels, [-1, 0, 5.5], axis=axis)
             expected = DimCoord([-1, 0, 5.5], units=1, long_name="thingness")
@@ -49,6 +71,8 @@ class Test:
             self.patch_interpolate.assert_called()
 
     def test_coord_input(self):
+        from iris.experimental.stratify import relevel
+
         source = AuxCoord(self.src_levels.data)
         metadata = self.src_levels.metadata._asdict()
         metadata["coord_system"] = None
@@ -61,6 +85,8 @@ class Test:
         self.patch_interpolate.assert_called()
 
     def test_custom_interpolator(self, mocker):
+        from iris.experimental.stratify import relevel
+
         mock_interpolate = mocker.Mock()
         mock_interpolate.return_value = np.ones((3, 1, 1))
 
