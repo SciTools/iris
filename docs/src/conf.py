bc526784c07ee87c535d66eff04264ca295fd7fe
Martin Yeo <martin.yeo@metoffice.gov.uk>
2026-04-20 14:00:19 +0100

    Revert "Carousel: use img-bottom instead of img-background."
    This reverts commit afb1c6c4316a521057edfae4467def52b65f649a.

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -507,7 +507,7 @@ def generate_carousel(
     cards_by_link = {}
 
     card = r""".. card::
-    :img-bottom: {image}
+    :img-background: {image}
     :link: {link}
     :link-type: ref
     :width: {width}
