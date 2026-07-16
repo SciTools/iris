b6cb460eb113f773860fe5df6fd4a243d025d184
Bouwe Andela <b.andela@esciencecenter.nl>
2026-07-16 18:06:34 +0200

    Fix type hints for cube

--- lib/iris/cube.py
+++ lib/iris/cube.py
@@ -1213,11 +1213,14 @@ class Cube(CFVariableMixin):
         attributes: Mapping | None = None,
         cell_methods: Iterable[CellMethod] | None = None,
         dim_coords_and_dims: Iterable[tuple[DimCoord, int]] | None = None,
-        aux_coords_and_dims: Iterable[tuple[AuxCoord, int | Iterable[int]]]
+        aux_coords_and_dims: Iterable[tuple[AuxCoord, int | Iterable[int] | None]]
         | None = None,
         aux_factories: Iterable[AuxCoordFactory] | None = None,
-        cell_measures_and_dims: Iterable[tuple[CellMeasure, int]] | None = None,
-        ancillary_variables_and_dims: Iterable[tuple[AncillaryVariable, int]]
+        cell_measures_and_dims: Iterable[tuple[CellMeasure, Iterable[int] | int | None]]
+        | None = None,
+        ancillary_variables_and_dims: Iterable[
+            tuple[AncillaryVariable, Iterable[int] | int | None]
+        ]
         | None = None,
         shape: tuple | None = None,
     ):
