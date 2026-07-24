43b82f2588ce360c8b171baa079b06c5e705eaf9
pt331 <144435193+pt331@users.noreply.github.com>
2026-07-24 18:40:58 +0100

    Tests for indexing meshes (#7218)
    * Barebones tests
    
    * assertion fix and allowing for single value index
    
    * fixed tests/stock/mesh topology_dimension
    
    * Update as_mesh to deal with locations not existing
    
    * Copied as_mesh changes from unit tests branch
    
    * Applied equality changes offered by @trexfeathers
    
    * Remove cube_mesh_node from test_subset_indexing_new_mesh test
    
    * updating to full tests - test_subset_indexing_mesh_index_set fails now
    
    * Fixed looking at the wrong part of cube and _MeshIndexSet test

--- lib/iris/mesh/components.py
+++ lib/iris/mesh/components.py
@@ -696,11 +696,16 @@ class _MeshXYMixin(Mesh, ABC):
         result: bool | NotImplementedType = NotImplemented
 
         if isinstance(other, _MeshXYMixin):
-            result = self.metadata == other.metadata
-            if result:
-                result = self.all_coords == other.all_coords
-            if result:
-                result = self.all_connectivities == other.all_connectivities
+            # Using identity speeds up several real-world equality checks
+            #  for _MeshIndexSet and MeshCoord.
+            result = self is other
+
+            if not result:
+                result = self.metadata == other.metadata
+                if result:
+                    result = self.all_coords == other.all_coords
+                if result:
+                    result = self.all_connectivities == other.all_connectivities
 
         return result
 
@@ -3396,6 +3401,18 @@ class _MeshIndexSet(_MeshXYMixin, _DimensionalMetadata):
             attributes=attributes,
         )
 
+    def __eq__(self, other) -> bool | NotImplementedType:
+        result: bool | NotImplementedType = NotImplemented
+
+        if isinstance(other, _MeshIndexSet):
+            result = self.metadata == other.metadata
+            # Don't check coords or connectivities as these are
+            #  fully derived using metadata.
+            if result:
+                result = self.indices == other.indices
+
+        return result
+
     def __getstate__(self) -> tuple[ArrayLike, MeshIndexSetMetadata]:
         return (
             self.indices,
@@ -3453,21 +3470,22 @@ class _MeshIndexSet(_MeshXYMixin, _DimensionalMetadata):
             case "node":
                 # self.indices is a user-supplied index array over the nodes; convert
                 #  it to a fixed-shape boolean membership mask.
-                monotonic, direction = iris.util.monotonic(
-                    self.indices, strict=True, return_direction=True
-                )
-                if not (monotonic and direction == 1):
-                    # TODO: boolean 'mask' array precludes non-monotonic indexing,
-                    #  but is only needed to support connectivity construction, and
-                    #  only causes problems for coordinate construction. Separate
-                    #  logic to allow array of integer indices for coordinate
-                    #  construction.
-                    message = (
-                        "Indexing the nodes on a Mesh currently requires strictly "
-                        "increasing indices. Contact the Iris developers if this "
-                        "causes you problems."
+                if len(self.indices) > 1:  # Single value index is fine
+                    monotonic, direction = iris.util.monotonic(
+                        self.indices, strict=True, return_direction=True
                     )
-                    raise ValueError(message)
+                    if not (monotonic and direction == 1):
+                        # TODO: boolean 'mask' array precludes non-monotonic indexing,
+                        #  but is only needed to support connectivity construction, and
+                        #  only causes problems for coordinate construction. Separate
+                        #  logic to allow array of integer indices for coordinate
+                        #  construction.
+                        message = (
+                            "Indexing the nodes on a Mesh currently requires strictly "
+                            "increasing indices. Contact the Iris developers if this "
+                            "causes you problems."
+                        )
+                        raise ValueError(message)
                 indices = self.indices
                 al = da if _lazy.is_lazy_data(indices) else np
                 node_mask = al.zeros(n_original_nodes, dtype=bool)
@@ -3611,6 +3629,18 @@ class _MeshIndexSet(_MeshXYMixin, _DimensionalMetadata):
         kwargs: Kwargs = {"mesh_id": id(self.mesh), "frozen": True}
         coord_man = self.mesh._coord_manager.indexed(*indices, **kwargs)
         conn_man = self.mesh._connectivity_manager.indexed(*indices, **kwargs)
+        has_edges = hasattr(conn_man, "edge_node")
+        has_faces = hasattr(conn_man, "face_node")
+        if has_faces:
+            topology_dimension = 2
+        elif has_edges:
+            topology_dimension = 1
+        else:
+            message = (
+                f"Cannot create a MeshXY from a MeshIndexSet with no edge or face "
+                f"connectivities. This is a *{self.location}* MeshIndexSet."
+            )
+            raise NotImplementedError(message)
 
         def _coords_and_axes(
             location: Literal["node", "edge", "face"],
@@ -3619,11 +3649,11 @@ class _MeshIndexSet(_MeshXYMixin, _DimensionalMetadata):
             return [(getattr(coords, f"{location}_{axis}"), axis) for axis in self.AXES]
 
         return MeshXY(
-            topology_dimension=self.topology_dimension,
+            topology_dimension=topology_dimension,
             node_coords_and_axes=_coords_and_axes("node"),
             connectivities=[conn for conn in conn_man.all_members if conn is not None],
-            edge_coords_and_axes=_coords_and_axes("edge"),
-            face_coords_and_axes=_coords_and_axes("face"),
+            edge_coords_and_axes=_coords_and_axes("edge") if has_edges else None,
+            face_coords_and_axes=_coords_and_axes("face") if has_faces else None,
             standard_name=self.standard_name,
             long_name=self.long_name,
             var_name=self.var_name,
--- lib/iris/tests/stock/mesh.py
+++ lib/iris/tests/stock/mesh.py
@@ -81,6 +81,8 @@ def sample_mesh(
     )
     node_y = AuxCoord(1200 + arr.arange(n_nodes), standard_name="latitude")
 
+    topology_dimension = 0
+
     connectivities = []
     if n_edges == 0:
         edge_coords_and_axes = None
@@ -100,6 +102,8 @@ def sample_mesh(
         edge_y = AuxCoord(2200 + arr.arange(n_edges), standard_name="latitude")
         edge_coords_and_axes = [(edge_x, "x"), (edge_y, "y")]
 
+        topology_dimension = 1
+
     if n_faces == 0:
         face_coords_and_axes = None
     else:
@@ -118,8 +122,10 @@ def sample_mesh(
         face_y = AuxCoord(3200 + arr.arange(n_faces), standard_name="latitude")
         face_coords_and_axes = [(face_x, "x"), (face_y, "y")]
 
+        topology_dimension = 2
+
     mesh = MeshXY(
-        topology_dimension=2,
+        topology_dimension=topology_dimension,
         node_coords_and_axes=[(node_x, "x"), (node_y, "y")],
         connectivities=connectivities,
         edge_coords_and_axes=edge_coords_and_axes,
