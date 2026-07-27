6e4a8f1418100243e5e0203149d49afd10dc9d21
Martin Yeo <martin.yeo@metoffice.gov.uk>
2026-07-27 09:35:43 +0100

    Extra experimental note.

--- lib/iris/experimental/mesh_coord_indexing.py
+++ lib/iris/experimental/mesh_coord_indexing.py
@@ -100,7 +100,10 @@ class Options(enum.Enum):
     NEW_MESH = enum.auto()
     """Index the :attr:`~iris.mesh.MeshCoord.mesh` of the ``MeshCoord`` to
     produce a new :class:`~iris.mesh.MeshXY` instance, then return a new
-    :class:`~iris.mesh.MeshCoord` instance based on that new mesh.
+    :class:`~iris.mesh.MeshCoord` instance based on that new mesh. Note: while
+    we continue to experiment with :class:`iris.mesh.components._MeshIndexSet`,
+    the properties of the new :class:`~iris.mesh.MeshXY` instance are liable to
+    change.
     """
 
     MESH_INDEX_SET = enum.auto()
