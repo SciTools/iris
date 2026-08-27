ef8d6174e068209147fcae1a5b94f3d1d230b77f
Martin Yeo <martin.yeo@metoffice.gov.uk>
2026-08-27 17:57:02 +0100

    Full CFGroup test coverage.

--- lib/iris/tests/unit/fileformats/cf/test_CFGroup.py
+++ lib/iris/tests/unit/fileformats/cf/test_CFGroup.py
@@ -4,58 +4,80 @@
 # See LICENSE in the root of the repository for full licensing details.
 """Unit tests for the :class:`iris.fileformats.cf.CFGroup` class."""
 
-from copy import deepcopy
-from typing import TYPE_CHECKING, NamedTuple
+from typing import Callable
 from unittest.mock import MagicMock
 
 import pytest
 
 from iris.fileformats import cf
 
-
-class MockVariables(NamedTuple):
-    formula_var: MagicMock
-    anc_var: MagicMock
-    aux_var: MagicMock
-    bound_var: MagicMock
-    climat_var: MagicMock
-    coord_var: MagicMock
-    data_var: MagicMock
-    grid_var: MagicMock
-    label_var: MagicMock
-    measure_var: MagicMock
-    ugrid_aux_var: MagicMock
-    ugrid_conn_var: MagicMock
-    ugrid_mesh_var: MagicMock
-
-
-@pytest.fixture
-def mock_variables(mocker) -> MockVariables:
-    return MockVariables(
-        mocker.MagicMock(spec=cf._CFFormulaTermsVariable, cf_name="formula_var"),
-        mocker.MagicMock(spec=cf.CFAncillaryDataVariable, cf_name="anc_var"),
-        mocker.MagicMock(spec=cf.CFAuxiliaryCoordinateVariable, cf_name="aux_var"),
-        mocker.MagicMock(spec=cf.CFBoundaryVariable, cf_name="bound_var"),
-        mocker.MagicMock(spec=cf.CFClimatologyVariable, cf_name="climat_var"),
-        mocker.MagicMock(spec=cf.CFCoordinateVariable, cf_name="coord_var"),
-        mocker.MagicMock(spec=cf.CFDataVariable, cf_name="data_var"),
-        mocker.MagicMock(spec=cf.CFGridMappingVariable, cf_name="grid_var"),
-        mocker.MagicMock(spec=cf.CFLabelVariable, cf_name="label_var"),
-        mocker.MagicMock(spec=cf.CFMeasureVariable, cf_name="measure_var"),
-        mocker.MagicMock(
-            spec=cf.CFUGridAuxiliaryCoordinateVariable, cf_name="ugrid_aux_var"
-        ),
-        mocker.MagicMock(spec=cf.CFUGridConnectivityVariable, cf_name="ugrid_conn_var"),
-        mocker.MagicMock(spec=cf.CFUGridMeshVariable, cf_name="ugrid_mesh_var"),
-    )
+VariableMap: dict[type[cf.CFVariable], Callable] = {
+    cf._CFFormulaTermsVariable: cf.CFGroup.formula_terms,
+    cf.CFAncillaryDataVariable: cf.CFGroup.ancillary_variables,
+    cf.CFAuxiliaryCoordinateVariable: cf.CFGroup.auxiliary_coordinates,
+    cf.CFBoundaryVariable: cf.CFGroup.bounds,
+    cf.CFClimatologyVariable: cf.CFGroup.climatology,
+    cf.CFCoordinateVariable: cf.CFGroup.coordinates,
+    cf.CFDataVariable: cf.CFGroup.data_variables,
+    cf.CFGridMappingVariable: cf.CFGroup.grid_mappings,
+    cf.CFLabelVariable: cf.CFGroup.labels,
+    cf.CFMeasureVariable: cf.CFGroup.cell_measures,
+    cf.CFUGridAuxiliaryCoordinateVariable: cf.CFGroup.ugrid_coords,
+    cf.CFUGridConnectivityVariable: cf.CFGroup.connectivities,
+    cf.CFUGridMeshVariable: cf.CFGroup.meshes,
+}
 
 
-@pytest.fixture
-def mock_variables_extra(mock_variables) -> MockVariables:
-    result = deepcopy(mock_variables)
-    for var in result:
-        var.cf_name = f"{var.cf_name}_2"
-    return result
+def get_mocked_var(class_: type[cf.CFVariable], mocker) -> MagicMock:
+    cf_name = f"{class_.__name__}_var"
+    mocked = mocker.MagicMock(spec=class_, cf_name=cf_name)
+    return mocked
+
+
+@pytest.fixture(
+    params=[
+        (class_, scenario)
+        for class_ in VariableMap
+        for scenario in ("single", "duplicates", "multiple")
+    ],
+    ids=lambda value: f"{value[0].__name__}-{value[1]}",
+)
+def mock_variables(
+    request, mocker
+) -> tuple[type[cf.CFVariable], tuple[MagicMock, ...]]:
+    class_, scenario = request.param
+    mocked = get_mocked_var(class_, mocker)
+    variables = [mocked]
+
+    if scenario == "duplicates":
+        variables.append(get_mocked_var(class_, mocker))
+    elif scenario == "multiple":
+        mocked_2 = get_mocked_var(class_, mocker)
+        mocked_2.cf_name = f"{mocked.cf_name}_2"
+        variables.append(mocked_2)
+
+    return class_, tuple(variables)
+
+
+@pytest.fixture(params=["single", "duplicates", "multiple"])
+def mock_variables_all(request, mocker) -> tuple[MagicMock, ...]:
+    scenario = request.param
+    all_variables = []
+
+    for class_ in VariableMap:
+        mocked = get_mocked_var(class_, mocker)
+        variables = [mocked]
+
+        if scenario == "duplicates":
+            variables.append(get_mocked_var(class_, mocker))
+        elif scenario == "multiple":
+            mocked_2 = get_mocked_var(class_, mocker)
+            mocked_2.cf_name = f"{mocked.cf_name}_2"
+            variables.append(mocked_2)
+
+        all_variables.extend(variables)
+
+    return tuple(all_variables)
 
 
 @pytest.fixture
@@ -63,89 +85,102 @@ def cf_group() -> cf.CFGroup:
     return cf.CFGroup()
 
 
-def test_non_data_names(cf_group, mock_variables, mock_variables_extra):
-    data_var = mock_variables.data_var
-    aux_var = mock_variables.aux_var
-    coord_var = mock_variables.coord_var
-    coord_var2 = mock_variables_extra.coord_var
-    duplicate_name_var = mock_variables_extra.aux_var
-    duplicate_name_var.cf_name = aux_var.cf_name
-
-    for var in (
-        data_var,
-        aux_var,
-        coord_var,
-        coord_var2,
-        duplicate_name_var,
-    ):
-        cf_group[var.cf_name] = var
-
-    expected_names = [var.cf_name for var in (aux_var, coord_var, coord_var2)]
-    expected = set(expected_names)
-    assert cf_group.non_data_variable_names == expected
+@pytest.fixture
+def cf_group_populated(cf_group, mock_variables_all) -> cf.CFGroup:
+    for mocked in mock_variables_all:
+        cf_group[mocked.cf_name] = mocked
+    return cf_group
 
 
-# TODO: unit tests for existing functionality pre 2021-03-11.
+class TestProperties:
+    def test_common(self, cf_group, mock_variables):
+        class_, variables = mock_variables
+        for mocked in variables:
+            cf_group[mocked.cf_name] = mocked
+
+        property_ = VariableMap[class_]
+        result = property_.fget(cf_group)
+        expected_names = {var.cf_name for var in variables}
+        assert len(result) == len(expected_names)
+        for expected_name in expected_names:
+            assert expected_name in result
+
+    def test_non_data_names(self, cf_group_populated, mock_variables_all):
+        expected = {
+            mocked.cf_name
+            for mocked in mock_variables_all
+            if not isinstance(mocked, (cf.CFDataVariable, cf._CFFormulaTermsVariable))
+        }
+        assert cf_group_populated.non_data_variable_names == expected
 
 
-class TestUgrid:
-    """Separate class to test UGRID functionality."""
-
+class TestReturns:
     @pytest.fixture(autouse=True)
-    def _setup(self, cf_group, mock_variables):
-        self.cf_group = cf_group
-        self.variables = mock_variables
+    def _setup(self, cf_group_populated, mock_variables_all):
+        self.variables = mock_variables_all
+        self.cf_group = cf_group_populated
 
-    def test_inherited(self):
-        coord_var = self.variables.coord_var
-        self.cf_group[coord_var.cf_name] = coord_var
-        assert self.cf_group.coordinates[coord_var.cf_name] == coord_var
+    def test_keys(self):
+        expected_names = {mocked.cf_name for mocked in self.variables}
+        assert set(self.cf_group.keys()) == expected_names
 
-    def test_connectivities(self):
-        conn_var = self.variables.ugrid_conn_var
-        self.cf_group[conn_var.cf_name] = conn_var
-        assert self.cf_group.connectivities[conn_var.cf_name] == conn_var
+    def test_len(self):
+        expected_names = {mocked.cf_name for mocked in self.variables}
+        assert len(self.cf_group) == len(expected_names)
 
-    def test_ugrid_coords(self):
-        coord_var = self.variables.ugrid_aux_var
-        self.cf_group[coord_var.cf_name] = coord_var
-        assert self.cf_group.ugrid_coords[coord_var.cf_name] == coord_var
+    def test_iter(self):
+        expected_names = {mocked.cf_name for mocked in self.variables}
+        actual_names = set()
+        for name in self.cf_group:
+            actual_names.add(name)
 
-    def test_meshes(self):
-        mesh_var = self.variables.ugrid_mesh_var
-        self.cf_group[mesh_var.cf_name] = mesh_var
-        assert self.cf_group.meshes[mesh_var.cf_name] == mesh_var
+        assert actual_names == expected_names
 
-    def test_non_data_names(self, mock_variables_extra):
-        data_var = self.variables.data_var
-        coord_var = self.variables.coord_var
-        conn_var = self.variables.ugrid_conn_var
-        ugrid_coord_var = self.variables.ugrid_aux_var
-        mesh_var = self.variables.ugrid_mesh_var
-        mesh_var2 = mock_variables_extra.ugrid_mesh_var
-        duplicate_name_var = mock_variables_extra.coord_var
-        duplicate_name_var.cf_name = coord_var.cf_name
+    def test_getitem(self):
+        expected_by_name = {}
+        for mocked in self.variables:
+            expected_by_name[mocked.cf_name] = mocked
 
-        for var in (
-            data_var,
-            coord_var,
-            conn_var,
-            ugrid_coord_var,
-            mesh_var,
-            mesh_var2,
-            duplicate_name_var,
-        ):
-            self.cf_group[var.cf_name] = var
+        for expected_name, expected_variable in expected_by_name.items():
+            assert self.cf_group[expected_name] is expected_variable
 
-        expected_names = [
-            var.cf_name
-            for var in (
-                coord_var,
-                conn_var,
-                ugrid_coord_var,
-                mesh_var,
-                mesh_var2,
-            )
-        ]
-        expected = set(expected_names)
-        assert self.cf_group.non_data_variable_names == expected
+        with pytest.raises(KeyError, match="Cannot get unknown CF-netCDF variable"):
+            _ = self.cf_group["unknown_name"]
+
+    def test_repr(self):
+        self.cf_group.global_attributes["global_attr"] = "value"
+        self.cf_group.promoted["promoted_var"] = self.variables[0]
+
+        expected_names = {mocked.cf_name for mocked in self.variables}
+        expected = (
+            "<CFGroup of "
+            f"variables:{len(expected_names)}, "
+            "global_attributes:1, "
+            "promoted:1>"
+        )
+        assert repr(self.cf_group) == expected
+
+
+class TestMutations:
+    def test_setitem(self, cf_group, mock_variables_all):
+        mocked = mock_variables_all[0]
+
+        cf_group[mocked.cf_name] = mocked
+        assert cf_group[mocked.cf_name] is mocked
+
+        with pytest.raises(TypeError, match="Attempted to add an invalid"):
+            cf_group[mocked.cf_name] = object()
+
+        with pytest.raises(ValueError, match="Mismatch between key name"):
+            cf_group[f"{mocked.cf_name}_mismatch"] = mocked
+
+    def test_delitem(self, cf_group_populated, mock_variables_all):
+        expected_names = {mocked.cf_name for mocked in mock_variables_all}
+        name_to_delete = next(iter(expected_names))
+
+        del cf_group_populated[name_to_delete]
+        assert name_to_delete not in cf_group_populated
+        assert len(cf_group_populated) == len(expected_names) - 1
+
+        with pytest.raises(KeyError, match="Cannot delete unknown CF-netcdf"):
+            del cf_group_populated["unknown_name"]
