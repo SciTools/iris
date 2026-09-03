6a1a99c8996a68c3a1c963a7907f910013fe072a
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2026-09-03 02:29:00 +0100

    Sort tests in test_CFVariable into classes based on method-under-test.

--- lib/iris/tests/unit/fileformats/cf/test_CFVariable.py
+++ lib/iris/tests/unit/fileformats/cf/test_CFVariable.py
@@ -52,167 +52,166 @@ def nc_vars(mocker):
     return tuple(make_nc_var(mocker) for _ in range(3))
 
 
-def test_init_records_filename_from_group(nc_var):
-    cf_var = CFVariableSub("foo", nc_var)
+class TestInit:
+    def test_records_filename_from_group(self, nc_var):
+        cf_var = CFVariableSub("foo", nc_var)
 
-    assert cf_var.filename == "/tmp/file.nc"
-    assert cf_var.cf_name == "foo"
-    assert cf_var.cf_data is nc_var
-    assert cf_var.cf_group is None
-    assert cf_var.cf_terms_by_root == {}
-    assert cf_var._to_be_promoted is False
+        assert cf_var.filename == "/tmp/file.nc"
+        assert cf_var.cf_name == "foo"
+        assert cf_var.cf_data is nc_var
+        assert cf_var.cf_group is None
+        assert cf_var.cf_terms_by_root == {}
+        assert cf_var._to_be_promoted is False
 
+    def test_falls_back_to_unknown_filename_without_group(self, nc_var_without_group):
+        cf_var = CFVariableSub("foo", nc_var_without_group)
 
-def test_init_falls_back_to_unknown_filename_without_group(nc_var_without_group):
-    cf_var = CFVariableSub("foo", nc_var_without_group)
-
-    assert cf_var.filename == "<unknown_filename>"
-
-
-def test_identify_common_handles_defaults_and_target_selection():
-    variables = {"a": object(), "b": object()}
-
-    ignore, target = CFVariableSub._identify_common(variables, None, None)
-    assert ignore == []
-    assert target is variables
-
-    ignore, target = CFVariableSub._identify_common(variables, ["a"], "b")
-    assert ignore == ["a"]
-    assert target == {"b": variables["b"]}
+        assert cf_var.filename == "<unknown_filename>"
 
 
-def test_identify_common_raises_for_unknown_target():
-    with pytest.raises(ValueError, match="Cannot identify unknown target"):
-        CFVariableSub._identify_common({"a": object()}, None, "missing")
+class TestIdentifyCommon:
+    def test_handles_defaults_and_target_selection(self):
+        variables = {"a": object(), "b": object()}
+
+        ignore, target = CFVariableSub._identify_common(variables, None, None)
+        assert ignore == []
+        assert target is variables
+
+        ignore, target = CFVariableSub._identify_common(variables, ["a"], "b")
+        assert ignore == ["a"]
+        assert target == {"b": variables["b"]}
+
+    def test_raises_for_unknown_target(self):
+        with pytest.raises(ValueError, match="Cannot identify unknown target"):
+            CFVariableSub._identify_common({"a": object()}, None, "missing")
+
+    def test_raises_for_invalid_target_type(self):
+        with pytest.raises(TypeError, match="Expect a target CF-netCDF variable name"):
+            CFVariableSub._identify_common({"a": object()}, None, object())
 
 
-def test_identify_common_raises_for_invalid_target_type():
-    with pytest.raises(TypeError, match="Expect a target CF-netCDF variable name"):
-        CFVariableSub._identify_common({"a": object()}, None, object())
+class TestSpans:
+    def test_scalar_dimension_always_true(self, mocker, nc_var):
+        nc_var.dimensions = (cf._NCZARR_SCALAR_DIMENSION,)
+        cf_var = CFVariableSub("scalar", nc_var)
+
+        other = mocker.MagicMock()
+        other.dimensions = ("time",)
+
+        assert cf_var.spans(other)
+
+    def test_is_subset_check(self, mocker, nc_vars):
+        lhs_nc_var, other_nc_var, _ = nc_vars
+        lhs_nc_var.dimensions = ("time",)
+        lhs = CFVariableSub("lhs", lhs_nc_var)
+
+        rhs = mocker.MagicMock()
+        rhs.dimensions = ("time", "lat")
+
+        assert lhs.spans(rhs)
+
+        other_nc_var.dimensions = ("height",)
+        other = CFVariableSub("other", other_nc_var)
+        assert not other.spans(rhs)
 
 
-def test_spans_scalar_dimension_always_true(mocker, nc_var):
-    nc_var.dimensions = (cf._NCZARR_SCALAR_DIMENSION,)
-    cf_var = CFVariableSub("scalar", nc_var)
+class TestComparisonAndRepresentation:
+    def test_equality_inequality_and_hash_by_name(self, nc_vars):
+        first, second, third = nc_vars
+        one = CFVariableSub("same", first)
+        two = CFVariableSub("same", second)
+        other = CFVariableSub("different", third)
 
-    other = mocker.MagicMock()
-    other.dimensions = ("time",)
+        assert one == two
+        assert one != other
+        assert hash(one) == hash(two)
+        assert hash(one) != hash(other)
 
-    assert cf_var.spans(other)
+    def test_repr_contains_class_name_name_and_data_repr(self, nc_var):
+        cf_var = CFVariableSub("foo", nc_var)
 
-
-def test_spans_is_subset_check(mocker, nc_vars):
-    lhs_nc_var, other_nc_var, _ = nc_vars
-    lhs_nc_var.dimensions = ("time",)
-    lhs = CFVariableSub("lhs", lhs_nc_var)
-
-    rhs = mocker.MagicMock()
-    rhs.dimensions = ("time", "lat")
-
-    assert lhs.spans(rhs)
-
-    other_nc_var.dimensions = ("height",)
-    other = CFVariableSub("other", other_nc_var)
-    assert not other.spans(rhs)
+        assert repr(cf_var) == f"CFVariableSub('foo', {nc_var!r})"
 
 
-def test_equality_inequality_and_hash_by_name(nc_vars):
-    first, second, third = nc_vars
-    one = CFVariableSub("same", first)
-    two = CFVariableSub("same", second)
-    other = CFVariableSub("different", third)
+class TestAttributeAccess:
+    def test_cached(self, nc_var):
+        # Make sure attribute access to the underlying netCDF4.Variable
+        # is cached.
+        name = "foo"
+        cf_var = CFVariableSub(name, nc_var)
+        assert nc_var.ncattrs.call_count == 1
 
-    assert one == two
-    assert one != other
-    assert hash(one) == hash(two)
-    assert hash(one) != hash(other)
+        # Accessing a netCDF attribute should result in no further calls
+        # to nc_var.ncattrs() and the creation of an attribute on the
+        # cf_var.
+        # NB. Can't use hasattr() because that triggers the attribute
+        # to be created!
+        assert "coordinates" not in cf_var.__dict__
+        _ = cf_var.coordinates
+        assert nc_var.ncattrs.call_count == 1
+        assert "coordinates" in cf_var.__dict__
 
+        # Trying again results in no change.
+        _ = cf_var.coordinates
+        assert nc_var.ncattrs.call_count == 1
+        assert "coordinates" in cf_var.__dict__
 
-def test_cached(nc_var):
-    # Make sure attribute access to the underlying netCDF4.Variable
-    # is cached.
-    name = "foo"
-    cf_var = CFVariableSub(name, nc_var)
-    assert nc_var.ncattrs.call_count == 1
+        # Trying another attribute results in just a new attribute.
+        assert "standard_name" not in cf_var.__dict__
+        _ = cf_var.standard_name
+        assert nc_var.ncattrs.call_count == 1
+        assert "standard_name" in cf_var.__dict__
 
-    # Accessing a netCDF attribute should result in no further calls
-    # to nc_var.ncattrs() and the creation of an attribute on the
-    # cf_var.
-    # NB. Can't use hasattr() because that triggers the attribute
-    # to be created!
-    assert "coordinates" not in cf_var.__dict__
-    _ = cf_var.coordinates
-    assert nc_var.ncattrs.call_count == 1
-    assert "coordinates" in cf_var.__dict__
+    def test_getattr_non_ncattr_value_is_cached_but_not_marked_used(self, nc_var):
+        nc_var.not_an_ncattr = 42
+        cf_var = CFVariableSub("foo", nc_var)
 
-    # Trying again results in no change.
-    _ = cf_var.coordinates
-    assert nc_var.ncattrs.call_count == 1
-    assert "coordinates" in cf_var.__dict__
+        assert cf_var.not_an_ncattr == 42
+        assert "not_an_ncattr" in cf_var.__dict__
+        assert "not_an_ncattr" not in cf_var.cf_attrs()
 
-    # Trying another attribute results in just a new attribute.
-    assert "standard_name" not in cf_var.__dict__
-    _ = cf_var.standard_name
-    assert nc_var.ncattrs.call_count == 1
-    assert "standard_name" in cf_var.__dict__
+    def test_getitem_and_len_delegate_to_underlying_variable(self, nc_var):
+        cf_var = CFVariableSub("foo", nc_var)
 
+        assert len(cf_var) == 4
+        assert cf_var[0] == "payload"
+        nc_var.__len__.assert_called_once_with()
+        nc_var.__getitem__.assert_called_once_with(0)
 
-def test_getattr_non_ncattr_value_is_cached_but_not_marked_used(nc_var):
-    nc_var.not_an_ncattr = 42
-    cf_var = CFVariableSub("foo", nc_var)
+    def test_cf_attrs_access_helpers_and_reset(self, nc_var):
+        cf_var = CFVariableSub("foo", nc_var)
 
-    assert cf_var.not_an_ncattr == 42
-    assert "not_an_ncattr" in cf_var.__dict__
-    assert "not_an_ncattr" not in cf_var.cf_attrs()
+        assert cf_var.cf_attrs() == (
+            ("_FillValue", -999),
+            ("coordinates", "x y"),
+            ("standard_name", "air_temperature"),
+        )
+        assert cf_var.cf_attrs_ignored() == (("_FillValue", -999),)
+        assert cf_var.cf_attrs_used() == (("_FillValue", -999),)
+        assert cf_var.cf_attrs_unused() == (
+            ("coordinates", "x y"),
+            ("standard_name", "air_temperature"),
+        )
 
+        _ = cf_var.coordinates
+        assert cf_var.cf_attrs_used() == (("_FillValue", -999), ("coordinates", "x y"))
 
-def test_getitem_and_len_delegate_to_underlying_variable(nc_var):
-    cf_var = CFVariableSub("foo", nc_var)
-
-    assert len(cf_var) == 4
-    assert cf_var[0] == "payload"
-    nc_var.__len__.assert_called_once_with()
-    nc_var.__getitem__.assert_called_once_with(0)
-
-
-def test_repr_contains_class_name_name_and_data_repr(nc_var):
-    cf_var = CFVariableSub("foo", nc_var)
-
-    assert repr(cf_var) == f"CFVariableSub('foo', {nc_var!r})"
-
-
-def test_cf_attrs_access_helpers_and_reset(nc_var):
-    cf_var = CFVariableSub("foo", nc_var)
-
-    assert cf_var.cf_attrs() == (
-        ("_FillValue", -999),
-        ("coordinates", "x y"),
-        ("standard_name", "air_temperature"),
-    )
-    assert cf_var.cf_attrs_ignored() == (("_FillValue", -999),)
-    assert cf_var.cf_attrs_used() == (("_FillValue", -999),)
-    assert cf_var.cf_attrs_unused() == (
-        ("coordinates", "x y"),
-        ("standard_name", "air_temperature"),
-    )
-
-    _ = cf_var.coordinates
-    assert cf_var.cf_attrs_used() == (("_FillValue", -999), ("coordinates", "x y"))
-
-    cf_var.cf_attrs_reset()
-    assert cf_var.cf_attrs_used() == (("_FillValue", -999),)
+        cf_var.cf_attrs_reset()
+        assert cf_var.cf_attrs_used() == (("_FillValue", -999),)
 
 
-def test_formula_term_registration_and_presence(nc_var):
-    cf_var = CFVariableSub("foo", nc_var)
+class TestFormulaTerms:
+    def test_registration_and_presence(self, nc_var):
+        cf_var = CFVariableSub("foo", nc_var)
 
-    assert not cf_var.has_formula_terms()
-    cf_var.add_formula_term("root", "a")
-    assert cf_var.has_formula_terms()
-    assert cf_var.cf_terms_by_root == {"root": "a"}
+        assert not cf_var.has_formula_terms()
+        cf_var.add_formula_term("root", "a")
+        assert cf_var.has_formula_terms()
+        assert cf_var.cf_terms_by_root == {"root": "a"}
 
 
-def test_identify_subclass_stub_returns_none(nc_var):
-    cf_var = CFVariableSub("foo", nc_var)
+class TestIdentify:
+    def test_subclass_stub_returns_none(self, nc_var):
+        cf_var = CFVariableSub("foo", nc_var)
 
-    assert cf_var.identify({}) is None
+        assert cf_var.identify({}) is None
