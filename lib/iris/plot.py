64db794a2767873f3e6461bef70adec5cbeb38a9
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2026-09-24 15:52:02 +0100

    Reduce equality tolerance in path testing.

--- lib/iris/plot.py
+++ lib/iris/plot.py
@@ -755,7 +755,7 @@ def _shift_plot_sections(u_object, u, v):
     proj_x, proj_y, _ = tgt_proj.transform_points(src_crs, u, v).T
 
     # Wrap proj_x into range -180..+180, as that's what the following logic needs.
-    # N.B. before Cartopy>0.26, this was true anyway, but has since changed.
+    # N.B. upto cartopy v0.26, this was true anyway, but has since changed.
     # see : github.com/SciTools/cartopy/issues/2747
     proj_x = (proj_x + 540.0) % 360.0 - 180.0
 
