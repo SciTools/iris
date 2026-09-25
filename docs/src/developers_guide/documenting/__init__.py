0aea4e95827956187f4120267e87950bf4783c24
stephen.worsley <stephen.worsley@metoffice.gov.uk>
2026-09-25 14:37:16 +0100

    ruff fix

--- docs/src/developers_guide/documenting/__init__.py
+++ docs/src/developers_guide/documenting/__init__.py
@@ -2,5 +2,4 @@
 #
 # This file is part of Iris and is released under the BSD license.
 # See LICENSE in the root of the repository for full licensing details.
-
-# noqa: D104
+"""Docstring examples."""
